8366da29c96fd52343de168ac199e22888d6730a
Bill Swanson <bill.swanson@advizorsolutions.com>
2010-06-18 12:49:13 -0500

    Added a project to develop the UI.

---
+++ Noise.UI/NoiseUiModule.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Practices.Composite.Modularity;
+using Microsoft.Practices.Unity;
+
+namespace Noise.UI {
+	public class NoiseUiModule : IModule {
+		private readonly IUnityContainer    mContainer;
+
+		public NoiseUiModule( IUnityContainer container ) {
+			mContainer = container;
+		}
+
+		public void Initialize() {
+			
+		}
+	}
+}
