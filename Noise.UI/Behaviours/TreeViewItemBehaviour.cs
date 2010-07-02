2f43c828a7aacb13c0c9f8313e31ba36383d3261
Bill Swanson <bswanson58@gmail.com>
2010-07-02 12:24:56 -0500

    Added a behavior to bring a selected tree view item into view.

---
+++ Noise.UI/Behaviours/TreeViewItemBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Noise.UI.Behaviours {
+	public class TreeViewItemBehaviour {
+		public static bool GetIsBroughtIntoViewWhenSelected( TreeViewItem treeViewItem ) {
+			return (bool)treeViewItem.GetValue( IsBroughtIntoViewWhenSelectedProperty );
+		}
+
+		public static void SetIsBroughtIntoViewWhenSelected( TreeViewItem treeViewItem, bool value ) {
+			treeViewItem.SetValue( IsBroughtIntoViewWhenSelectedProperty, value );
+		}
+
+		public static readonly DependencyProperty IsBroughtIntoViewWhenSelectedProperty =
+			DependencyProperty.RegisterAttached(
+				"IsBroughtIntoViewWhenSelected",
+				typeof( bool ),
+				typeof( TreeViewItemBehaviour ),
+				new UIPropertyMetadata( false, OnIsBroughtIntoViewWhenSelectedChanged ) );
+
+		static void OnIsBroughtIntoViewWhenSelectedChanged( DependencyObject depObj, DependencyPropertyChangedEventArgs e ) {
+			var item = depObj as TreeViewItem;
+
+			if(( item != null ) &&
+			   ( e.NewValue is bool )) {
+				if( (bool)e.NewValue ) {
+					item.Selected += OnItemSelected;
+				}
+				else {
+					item.Selected -= OnItemSelected;
+				}
+			}
+		}
+
+
+		static void OnItemSelected( object sender, RoutedEventArgs e ) {
+			var item = e.OriginalSource as TreeViewItem;
+
+			if( item != null ) {
+				item.BringIntoView();
+			}
+		}
+	}
+}
