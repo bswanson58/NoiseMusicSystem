f76bfcb5af02e1004211dbe8ebfe443f2e2138f3
Bill Swanson <bswanson58@gmail.com>
2010-07-09 08:40:45 -0500

    Added a toolbar to the application.

---
+++ Noise.UI/ViewModels/ToolbarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Composite.Events;
+using Microsoft.Practices.Unity;
+using Noise.Infrastructure.Support;
+
+namespace Noise.UI.ViewModels {
+	public class ToolbarViewModel : ViewModelBase {
+		private IUnityContainer		mContainer;
+		private IEventAggregator	mEvents;
+
+		[Dependency]
+		public IUnityContainer Container {
+			get { return( mContainer ); }
+			set {
+				mContainer = value;
+
+				mEvents = mContainer.Resolve<IEventAggregator>();
+			}
+		}
+	}
+}
