8b1d4d6786a3f5bf00421be6e3ddaebd3816daab
Bill Swanson <bswanson58@gmail.com>
2010-07-13 09:27:25 -0500

    Added a value converter to clean html tags from the artist biography.

---
+++ Noise.UI/ValueConverters/HtmlTextConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+
+namespace Noise.UI.ValueConverters {
+	public class HtmlTextConverter : IValueConverter {
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture ) {
+			var retValue = "";
+
+			if( value is string ) {
+				var input = (string)value;
+				var regEx = new Regex( @"<(.|\n)*?>", RegexOptions.IgnoreCase );
+
+				retValue = regEx.Replace( input, String.Empty );
+
+			}
+			return( retValue );
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture ) {
+			throw new NotImplementedException();
+		}
+	}
+}
