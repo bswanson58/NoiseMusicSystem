54ed5ca2c3a789f836a3e84b0df04435ff7d09e4
Bill Swanson <bswanson58@gmail.com>
2010-07-21 08:39:39 -0500

    Added date only converter to added date in track view.

---
+++ Noise.UI/ValueConverters/DateOnlyConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Noise.UI.ValueConverters {
+	public class DateOnlyConverter : IValueConverter {
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture ) {
+			var retValue = "";
+
+			if( value is DateTime ) {
+				var date = (DateTime)value;
+
+				retValue = date.ToShortDateString();
+			}
+
+			return( retValue );
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture ) {
+			throw new NotImplementedException();
+		}
+	}
+}
