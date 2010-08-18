f226109cfd87e6627e745f33e6c4f663463c1c16
Bill Swanson <bswanson58@gmail.com>
2010-08-18 11:30:06 -0500

    Added an application handler for unhandled exceptions.

--- Noise.Desktop/App.xaml.cs
+++ Noise.Desktop/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Noise.Infrastructure.Interfaces;
 
 namespace Noise.Desktop {
 	/// <summary>
@@ -10,8 +11,19 @@ namespace Noise.Desktop {
 		protected override void OnStartup( StartupEventArgs e ) {
 			base.OnStartup( e );
 
+			DispatcherUnhandledException += App_DispatcherUnhandledException;
+
 			mBootstrapper = new Bootstrapper();
 			mBootstrapper.Run();
+		}
+
+		void App_DispatcherUnhandledException( object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e ) {
+			var	log = mBootstrapper.Container.Resolve<ILog>();
+
+			log.LogException( "Application unhandled exception:", e.Exception );
+			e.Handled = true;
+
+			Shutdown( -1 );
 		}
 
 		protected override void OnExit( ExitEventArgs e ) {
