c2ba13c7a7ffa04fc68a66718ae4fbac77775697
Bill Swanson <bswanson58@gmail.com>
2010-10-12 12:01:26 -0500

    Added database classes for play lists and genres.

---
+++ Noise.Infrastructure/Dto/DbGenre.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace Noise.Infrastructure.Dto {
+	public class DbGenre : DbBase, IUserSettings {
+		public string			Name { get; set; }
+		public string			Description { get; set; }
+		public Int16			Rating { get; set; }
+		public bool				IsFavorite { get; set; }
+
+		public DbGenre() {
+			Name = "";
+			Description = "";
+		}
+
+		public string Genre {
+			get{ return( Name ); }
+			set{ }
+		}
+
+		public bool IsUserRating {
+			get { return( true ); }
+		}
+
+		[Export("PersistenceType")]
+		public static Type PersistenceType {
+			get{ return( typeof( DbGenre )); }
+		}
+	}
+}
---
+++ Noise.Infrastructure/Dto/DbPlayList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace Noise.Infrastructure.Dto {
+	public class DbPlayList : DbBase, IUserSettings {
+		public string			Name { get; set; }
+		public string			Description { get; set; }
+		public long[]			TrackIds { get; set; }
+		public Int16			Rating { get; set; }
+		public string			Genre { get; set; }
+		public bool				IsFavorite { get; set; }
+
+		public DbPlayList() {
+			Name = "";
+			Description = "";
+
+			TrackIds = new long[0];
+		}
+
+		public bool IsUserRating {
+			get { return( true ); }
+		}
+
+		[Export("PersistenceType")]
+		public static Type PersistenceType {
+			get{ return( typeof( DbPlayList )); }
+		}
+	}
+}
