324512f3e7053a2417d550a43bbe330495e23856
Bill Swanson <bswanson58@gmail.com>
2010-10-29 08:34:44 -0500

    Added a bare library additions view to the explorer view.

---
+++ Noise.UI/ViewModels/LibraryAdditionsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using Microsoft.Practices.Composite.Events;
+using Microsoft.Practices.Unity;
+using Noise.Infrastructure;
+using Noise.Infrastructure.Interfaces;
+
+namespace Noise.UI.ViewModels {
+	public class LibraryAdditionsViewModel {
+		private IUnityContainer				mContainer;
+		private IEventAggregator			mEvents;
+		private INoiseManager				mNoiseManager;
+		private readonly BackgroundWorker	mBackgroundWorker;
+
+		public LibraryAdditionsViewModel() {
+			
+		}
+
+		[Dependency]
+		public IUnityContainer Container {
+			get { return( mContainer ); }
+			set {
+				mContainer = value;
+
+				mEvents = mContainer.Resolve<IEventAggregator>();
+				mNoiseManager = mContainer.Resolve<INoiseManager>();
+			}
+		}
+	}
+}
