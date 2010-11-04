3e84586593a2c44f8dd5bab9700cfcb13b5eb65c
Bill Swanson <bswanson58@gmail.com>
2010-11-04 14:17:05 -0500

    Cleanup of Unity configuration.

--- Noise.Service/ServiceMain.cs
+++ Noise.Service/ServiceMain.cs
@@ -11,36 +11,14 @@ using Noise.Service.LibraryService;
 
 namespace Noise.Service {
 	static class ServiceMain {
+		private static IUnityContainer	mContainer;
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		static void Main( string[] args ) {
-			var	container = new UnityContainer();
-			var catalog = new ModuleCatalog();
+			InitializeUnity();
 
-	        ILoggerFacade	loggerFacade = new TextLogger();
-
-			catalog.AddModule( typeof( Core.NoiseCoreModule ));
-			container.RegisterInstance<IModuleCatalog>( catalog );
-            container.AddNewExtension<UnityBootstrapperExtension>();
-
-            container.RegisterInstance( loggerFacade );
-			container.RegisterType<IServiceLocator, UnityServiceLocatorAdapter>( new ContainerControlledLifetimeManager());
-			container.RegisterType<IModuleInitializer, ModuleInitializer>( new ContainerControlledLifetimeManager());
-            container.RegisterType<IModuleManager, ModuleManager>( new ContainerControlledLifetimeManager());
-            container.RegisterType<IEventAggregator, EventAggregator>( new ContainerControlledLifetimeManager());
-
-            ExceptionExtensions.RegisterFrameworkExceptionType( typeof( ActivationException));
-            ExceptionExtensions.RegisterFrameworkExceptionType( typeof( ResolutionFailedException));
-            ExceptionExtensions.RegisterFrameworkExceptionType( typeof(Microsoft.Practices.ObjectBuilder2.BuildFailedException));
-
-            ServiceLocator.SetLocatorProvider( container.Resolve<IServiceLocator>);
-
-            var manager = container.Resolve<IModuleManager>();
-
-            manager.Run();
-
-			using( var implementation = new LibraryServiceImpl( container ) ) {
+			using( var implementation = new LibraryServiceImpl( mContainer ) ) {
 				// if started from console, file explorer, etc, run as console app.
 				if( Environment.UserInteractive ) {
 					ConsoleServiceHarness.Run( args, implementation );
@@ -49,6 +27,39 @@ namespace Noise.Service {
 				// otherwise run as a windows service
 				else {
 					ServiceBase.Run( new WindowsServiceHarness( implementation ));
+				}
+			}
+		}
+
+		private static void InitializeUnity() {
+			try {
+				mContainer = new UnityContainer();
+
+				mContainer.RegisterInstance<ILoggerFacade>( new TextLogger());
+
+				var catalog = new ModuleCatalog();
+
+				catalog.AddModule( typeof( Core.NoiseCoreModule ));
+				mContainer.RegisterInstance<IModuleCatalog>( catalog );
+
+				mContainer.RegisterType<IServiceLocator, UnityServiceLocatorAdapter>( new ContainerControlledLifetimeManager());
+				mContainer.RegisterType<IModuleInitializer, ModuleInitializer>( new ContainerControlledLifetimeManager());
+				mContainer.RegisterType<IModuleManager, ModuleManager>( new ContainerControlledLifetimeManager());
+				mContainer.RegisterType<IEventAggregator, EventAggregator>( new ContainerControlledLifetimeManager());
+
+				ExceptionExtensions.RegisterFrameworkExceptionType( typeof( ActivationException));
+				ExceptionExtensions.RegisterFrameworkExceptionType( typeof( ResolutionFailedException));
+				ExceptionExtensions.RegisterFrameworkExceptionType( typeof(Microsoft.Practices.ObjectBuilder2.BuildFailedException));
+
+				ServiceLocator.SetLocatorProvider( mContainer.Resolve<IServiceLocator>);
+
+				mContainer.Resolve<IModuleManager>().Run();
+			}
+			catch( Exception ex ) {
+				if( mContainer != null ) {
+					var logger = mContainer.Resolve<ILoggerFacade>();
+
+					logger.Log( String.Format( "Failed to initialize Unity: {0}", ex.Message ), Category.Exception, Priority.High );
 				}
 			}
 		}
