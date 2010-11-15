e7becea8f4fa243b621cae975a1edcdaca3e93b1
Bill Swanson <bswanson58@gmail.com>
2010-11-15 08:03:00 -0600

    Added a separate assembly to contain UI resources and definitions.

---
+++ Noise.UI.Style/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "Noise.UI.Style" )]
+[assembly: AssemblyDescription( "User Interface styles and resources used by the Noise Music System" )]
+[assembly: AssemblyProduct( "Noise Music System" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "605aeb70-a6cb-4dea-be97-5aca060262a4" )]
