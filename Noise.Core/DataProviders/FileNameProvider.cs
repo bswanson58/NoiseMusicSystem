21fdd45c139160690088c2d068a85db4fab05777
Bill Swanson <bswanson58@gmail.com>
2010-11-22 12:44:31 -0600

    Added parsing of album names to determine a published year.

--- Noise.Core/DataProviders/FileNameProvider.cs
+++ Noise.Core/DataProviders/FileNameProvider.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.Practices.Unity;
 using Noise.Core.Database;
 using Noise.Core.FileStore;
+using Noise.Infrastructure;
 using Noise.Infrastructure.Dto;
 using Noise.Infrastructure.Interfaces;
 
@@ -13,11 +15,14 @@ namespace Noise.Core.DataProviders {
 		private readonly IDatabaseManager	mDatabaseManager;
 		private readonly ILog				mLog;
 		private long						mFolderId;
+		private readonly Regex				mDatePattern;
 		private List<StorageFile>			mFolderFiles;
 
 		public FileNameProvider( IUnityContainer container ) {
 			mDatabaseManager = container.Resolve<IDatabaseManager>();
 			mLog = container.Resolve<ILog>();
+
+			mDatePattern = new Regex( "(?<month>0?[1-9]|1[012]) [- .] (?<day>0?[1-9]|[12][0-9]|3[01]) [- .] (?<year>[0-9]{2,})", RegexOptions.IgnorePatternWhitespace );
 		}
 
 		public IMetaDataProvider GetProvider( StorageFile forFile ) {
@@ -25,7 +30,7 @@ namespace Noise.Core.DataProviders {
 				BuildFolderFiles( forFile.ParentFolder );
 			}
 
-			return( new NameProvider( forFile, mFolderFiles ));
+			return( new NameProvider( forFile, mFolderFiles, mDatePattern ));
 		}
 
 		private void BuildFolderFiles( long parentId ) {
@@ -51,10 +56,12 @@ namespace Noise.Core.DataProviders {
 	internal class NameProvider : IMetaDataProvider {
 		private readonly StorageFile		mFile;
 		private readonly IList<StorageFile>	mFolderFiles;
+		private readonly Regex				mDatePattern;
 
-		public NameProvider( StorageFile file, IList<StorageFile> folderFiles ) {
+		public NameProvider( StorageFile file, IList<StorageFile> folderFiles, Regex datePattern ) {
 			mFolderFiles = folderFiles;
 			mFile = file;
+			mDatePattern = datePattern;
 		}
 
 		public string Artist {
@@ -84,6 +91,23 @@ namespace Noise.Core.DataProviders {
 
 				if( listTrack != null ) {
 					track.TrackNumber = (UInt16)( mFolderFiles.IndexOf( listTrack ) + 1 );
+				}
+			}
+
+			if( track.PublishedYear == Constants.cUnknownYear ) {
+				var	match = mDatePattern.Match( album.Name );
+
+				if( match.Success ) {
+					var year = Convert.ToUInt16( match.Groups["year"].Captures[0].Value );
+
+					if( year < 30 ) {
+						year += 2000;
+					}
+					else if( year < 100 ) {
+						year += 1900;
+					}
+
+					track.PublishedYear = year;
 				}
 			}
 		}
