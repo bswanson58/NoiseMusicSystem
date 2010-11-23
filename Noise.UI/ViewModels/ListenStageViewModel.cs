669c2cab3f24adbd49c27524647324edda1dd66f
Bill Swanson <bswanson58@gmail.com>
2010-11-23 14:55:12 -0600

    Added a view / view model for the listening view.

---
+++ Noise.UI/ViewModels/ListenStageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Prism.Events;
+using Microsoft.Practices.Unity;
+using Noise.Infrastructure.Interfaces;
+using Noise.Infrastructure.Support;
+
+namespace Noise.UI.ViewModels {
+	public class ListenStageViewModel : ViewModelBase {
+		private	IUnityContainer		mContainer;
+		private IEventAggregator	mEvents;
+		private INoiseManager		mNoiseManager;
+
+		[Dependency]
+		public IUnityContainer Container {
+			get { return( mContainer ); }
+			set {
+				mContainer = value;
+
+				mEvents = mContainer.Resolve<IEventAggregator>();
+				mNoiseManager = mContainer.Resolve<INoiseManager>();
+			}
+		}
+
+	}
+}
