a3a5e612bbef65234c4a96d5fb2745e99cba226b
Bill Swanson <bswanson58@gmail.com>
2010-12-02 09:15:48 -0600

    Changes the small player vu meter colors again. Exposed the peak threshold and color properties of the vu meter control.

--- Noise.UI/Controls/VuMeter.xaml.cs
+++ Noise.UI/Controls/VuMeter.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace Noise.UI.Controls {
 	/// <summary>
@@ -9,16 +10,28 @@ namespace Noise.UI.Controls {
 			InitializeComponent();
 		}
 
-        public static DependencyProperty LeftLevelProperty = DependencyProperty.Register( "LeftLevel", typeof( double ), typeof( VuMeter ), new PropertyMetadata( null ));
+        public static DependencyProperty LeftLevelProperty = DependencyProperty.Register( "LeftLevel", typeof( double ), typeof( VuMeter ), new PropertyMetadata( 0.0 ));
         public double LeftLevel {
             get { return (double)GetValue( LeftLevelProperty ); }
             set { SetValue( LeftLevelProperty, value ); }
         }
 
-        public static DependencyProperty RightLevelProperty = DependencyProperty.Register( "RightLevel", typeof( double ), typeof( VuMeter ), new PropertyMetadata( null ));
+        public static DependencyProperty RightLevelProperty = DependencyProperty.Register( "RightLevel", typeof( double ), typeof( VuMeter ), new PropertyMetadata( 0.0 ));
         public double RightLevel {
             get { return (double)GetValue( RightLevelProperty ); }
             set { SetValue( RightLevelProperty, value ); }
+        }
+
+        public static DependencyProperty PeakThresholdProperty = DependencyProperty.Register( "PeakThreshold", typeof( double ), typeof( VuMeter ), new PropertyMetadata( 0.9 ));
+        public double PeakThreshold {
+            get { return (double)GetValue( PeakThresholdProperty ); }
+            set { SetValue( PeakThresholdProperty, value ); }
+        }
+
+        public static DependencyProperty PeakColorProperty = DependencyProperty.Register( "PeakColor", typeof( Brush ), typeof( VuMeter ), new PropertyMetadata( Brushes.Red ));
+        public Brush PeakColor {
+            get { return (Brush)GetValue( PeakColorProperty ); }
+            set { SetValue( PeakColorProperty, value ); }
         }
 	}
 }
