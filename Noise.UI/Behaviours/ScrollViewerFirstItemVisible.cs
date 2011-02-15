0da9c3aabf5687ea5adec75fab6004fa9f035c87
Bill Swanson <bswanson58@gmail.com>
2011-02-15 15:17:42 -0600

    Fixed scrolling in the artist's track view.

---
+++ Noise.UI/Behaviours/ScrollViewerFirstItemVisible.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Noise.UI.Behaviours {
+	public class ScrollViewerFirstItemVisible {
+		public static object GetChangeTrigger( ScrollViewer scroller ) {
+			return( scroller.GetValue( ChangeTriggerProperty ));
+		}
+
+		public static void SetChangeTrigger( ScrollViewer scroller, object value ) {
+			scroller.SetValue( ChangeTriggerProperty, value );
+		}
+
+		public static readonly DependencyProperty ChangeTriggerProperty =
+			DependencyProperty.RegisterAttached(
+				"ChangeTrigger",
+				typeof( object ),
+				typeof( ScrollViewerFirstItemVisible ),
+				new UIPropertyMetadata( null, OnChangeTrigger ) );
+
+		static void OnChangeTrigger( DependencyObject depObj, DependencyPropertyChangedEventArgs args ) {
+			var scroller = depObj as ScrollViewer;
+
+			if( scroller != null ) {
+				scroller.ScrollToHome();
+			}
+		}
+
+	}
+}
