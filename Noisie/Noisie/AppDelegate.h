bc876a8bd99e1cfed4bc39d050389ec9ecb8408b
Bill Swanson <bswanson58@gmail.com>
2011-11-13 15:32:45 -0600

    Added RestKit to the project.

--- Noisie/Noisie/AppDelegate.h
+++ Noisie/Noisie/AppDelegate.h
@@ -3,7 +3,7 @@
 //  Noisie
 //
 //  Created by William Swanson on 11/13/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Secret Squirrel Products. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
