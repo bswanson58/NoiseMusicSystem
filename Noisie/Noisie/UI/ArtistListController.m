57ebe0d36f86ed7c8825a63184d8506befbe86ed
Bill Swanson <bswanson58@gmail.com>
2011-11-29 16:45:43 -0600

    Added a section ribbon list to the artist list view.

--- Noisie/Noisie/UI/ArtistListController.m
+++ Noisie/Noisie/UI/ArtistListController.m
@@ -14,7 +14,8 @@
 
 @interface ArtistListController ()
 
-@property (nonatomic, retain)   NSMutableArray  *mArtistList;
+@property (nonatomic, retain)   NSMutableDictionary *mArtistSections;
+@property (nonatomic, retain)   NSMutableArray      *mSectionList;
 
 - (void) onServerConnected:(NSNotification *) notification;
 - (void) onArtistListUpdate:(NSNotification *) notification;
@@ -24,7 +25,8 @@
 @implementation ArtistListController
 
 @synthesize uiArtistList;
-@synthesize mArtistList;
+@synthesize mArtistSections;
+@synthesize mSectionList;
 @synthesize artistListCell;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
@@ -38,7 +40,8 @@
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
     
-    self.mArtistList = nil;
+    self.mArtistSections = nil;
+    self.mSectionList = nil;
     self.uiArtistList = nil;
     self.artistListCell = nil;
 
@@ -46,7 +49,7 @@
 }
 
 - (void) onServerConnected:(NSNotification *)notification {
-    if( [self.mArtistList count] > 0 ) {
+    if( [self.mSectionList count] > 0 ) {
         [[NSNotificationCenter defaultCenter] postNotificationName:EventArtistListRequest object:nil];
     }
 }
@@ -54,24 +57,116 @@
 - (void) onArtistListUpdate:(NSNotification *)notification {
     ArtistListResult    *result = [notification object];
     
-    [self.mArtistList removeAllObjects];
-    [self.mArtistList addObjectsFromArray:result.Artists];
+    [self.mArtistSections removeAllObjects];
+    [self.mSectionList removeAllObjects];
+    
+    for ( RoArtist *artist in result.Artists ) {
+        NSString        *section = [artist.Name substringToIndex:1];
+        NSMutableArray  *sectionList = [self.mArtistSections objectForKey:section];
+        
+        if( sectionList == nil ) {
+            sectionList = [[[NSMutableArray alloc] init] autorelease];
+            
+            [self.mArtistSections setObject:sectionList forKey:section];
+            [self.mSectionList addObject:section];
+        }
+        
+        [sectionList addObject:artist];
+    }
 
     NSSortDescriptor *orderDescriptor = [[[NSSortDescriptor alloc] initWithKey:@"Name" ascending:YES] autorelease];
-    [self.mArtistList sortUsingDescriptors:[NSArray arrayWithObject:orderDescriptor]];
+    NSArray          *descriptorArray = [NSArray arrayWithObject:orderDescriptor];
+    
+    for( NSMutableArray *sectionList in self.mArtistSections.objectEnumerator ) {
+        [sectionList sortUsingDescriptors:descriptorArray];
+    }
+    
+    [self.mSectionList sortUsingComparator:^NSComparisonResult(id obj1, id obj2) { return [((NSString *)obj1) compare:((NSString *)obj2) options:NSNumericSearch]; }];
 
     [self.uiArtistList reloadData];
 }
 
 - (void) setSearchFocus:(RoSearchResultItem *)item {
-    for (RoArtist *artist in self.mArtistList) {
-        if([artist.DbId isEqualToNumber:item.ArtistId]) {
-            [[NSNotificationCenter defaultCenter] postNotificationName:EventArtistSelected object:artist];
+    bool    found = false;
+    
+    for( NSMutableArray *sectionList in self.mArtistSections.objectEnumerator ) {
+        for( RoArtist *artist in sectionList ) {
+            if([artist.DbId isEqualToNumber:item.ArtistId]) {
+                [[NSNotificationCenter defaultCenter] postNotificationName:EventArtistSelected object:artist];
             
+                found = true;
+                break;
+            }
+        }
+        
+        if( found ) {
             break;
         }
     }
 }
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return([self.mSectionList count]);
+}
+
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
+    return( self.mSectionList );
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    NSString        *sectionKey = [self.mSectionList objectAtIndex:section];
+    NSMutableArray  *sectionList = [self.mArtistSections objectForKey:sectionKey];
+    
+    return([sectionList count]);
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"ArtistListCell";
+    
+    ArtistListCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        [[NSBundle mainBundle] loadNibNamed:cellIdentifier owner:self options:nil];
+        
+        cell = self.artistListCell;
+        self.artistListCell = nil;
+    }
+    
+    // Configure the cell...
+    NSString        *sectionKey = [self.mSectionList objectAtIndex:indexPath.section];
+    NSMutableArray  *sectionList = [self.mArtistSections objectForKey:sectionKey];
+    
+    [cell setArtist:[sectionList objectAtIndex:[indexPath row]]];
+  
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString        *sectionKey = [self.mSectionList objectAtIndex:indexPath.section];
+    NSMutableArray  *sectionList = [self.mArtistSections objectForKey:sectionKey];
+    RoArtist        *artist = [sectionList objectAtIndex:[indexPath row]];
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:EventArtistSelected object:artist];
+}
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+ {
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
 
 #pragma mark - View lifecycle
 
@@ -79,11 +174,12 @@
     [super viewDidLoad];
     
     self.navigationItem.title = @"Library - Artists";
-    self.mArtistList = [[[NSMutableArray alloc] init] autorelease];
+    self.mArtistSections = [[[NSMutableDictionary alloc] init] autorelease];
+    self.mSectionList = [[[NSMutableArray alloc] init] autorelease];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onServerConnected:) name:EventServerConnected object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onArtistListUpdate:) name:EventArtistListUpdate object:nil];
-
+    
     [[NSNotificationCenter defaultCenter] postNotificationName:EventArtistListRequest object:nil];
     
     // Uncomment the following line to preserve selection between presentations.
@@ -125,56 +221,5 @@
     // Return YES for supported orientations
 	return YES;
 }
-
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    // Return the number of rows in the section.
-    return([self.mArtistList count]);
-}
-
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *cellIdentifier = @"ArtistListCell";
-    
-    ArtistListCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    if (cell == nil) {
-        [[NSBundle mainBundle] loadNibNamed:cellIdentifier owner:self options:nil];
-        
-        cell = self.artistListCell;
-        self.artistListCell = nil;
-    }
-    
-    // Configure the cell...
-    [cell setArtist:[self.mArtistList objectAtIndex:[indexPath row]]];
-  
-    return cell;
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    RoArtist    *artist = [self.mArtistList objectAtIndex:[indexPath row]];
-    
-    [[NSNotificationCenter defaultCenter] postNotificationName:EventArtistSelected object:artist];
-}
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
- {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
 
 @end
