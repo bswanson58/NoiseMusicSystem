8e8c625cf44e1ac84b5f6963f9d145a0deec1555
Bill Swanson <bswanson58@gmail.com>
2012-01-06 11:30:06 -0600

    Added to DecadeTagBuilderTests.

--- Noise.Core.Tests/BackgroundTasks/DecadeTagBuilderTests.cs
+++ Noise.Core.Tests/BackgroundTasks/DecadeTagBuilderTests.cs
@@ -4,6 +4,7 @@ using Moq;
 using NUnit.Framework;
 using Noise.Core.BackgroundTasks;
 using Noise.Core.Support;
+using Noise.Infrastructure;
 using Noise.Infrastructure.Dto;
 using Noise.Infrastructure.Interfaces;
 
@@ -23,69 +24,135 @@ namespace Noise.Core.Tests.BackgroundTasks {
 			private Mock<ITagAssociationProvider>	mTagAssociationProvider;
 			private Mock<ITagManager>				mTagManager;
 			private Mock<ITimestampProvider>		mTimestampProvider;
+			private Mock<IDatabaseShell>			mDatabaseShell;
 
-			protected void CreateMocks() {
+			private void CreateMocks() {
 				mArtistProvider = new Mock<IArtistProvider>();
 				mAlbumProvider = new Mock<IAlbumProvider>();
 				mLifecycleManager = new Mock<ILifecycleManager>();
 				mTagAssociationProvider = new Mock<ITagAssociationProvider>();
 				mTagManager = new Mock<ITagManager>();
 				mTimestampProvider = new Mock<ITimestampProvider>();
-
-				var	decadeTagList = new List<DbDecadeTag> { new DbDecadeTag( "One" ) { StartYear = 10, EndYear = 19 },
-															new DbDecadeTag( "Two" ) { StartYear = 20, EndYear = 29 },
-															new DbDecadeTag( "3rd" ) { StartYear = 30, EndYear = 39 }};
-
+				mDatabaseShell = new Mock<IDatabaseShell>();
 			}
 
-			protected DecadeTagBuilder CreateSut() {
+			private DecadeTagBuilder CreateSut() {
 				return( new DecadeTagBuilder( mLifecycleManager.Object, mTagAssociationProvider.Object, mTimestampProvider.Object,
 											  mArtistProvider.Object, mAlbumProvider.Object, mTagManager.Object ));
 			}
 
+			private void SetupTimestampProvider( long withTime ) {
+				mTimestampProvider.Setup( x => x.GetTimestamp( It.IsAny<string>())).Returns( withTime );
+			}
+
+			private void SetupArtistProvider( DbArtist artist ) {
+				var artists = new List<DbArtist> { artist };
+				mArtistProvider.Setup( m => m.GetArtistList()).Returns( new DataProviderList<DbArtist>( mDatabaseShell.Object, artists ));
+				mArtistProvider.Setup( m => m.GetArtist( It.IsAny<long>())).Returns( artist );
+			}
+
+			private void SetupAlbumProvider( IEnumerable<DbAlbum> albumList ) {
+				mAlbumProvider.Setup( m => m.GetAlbumList( It.IsAny<long>())).Returns( new DataProviderList<DbAlbum>( mDatabaseShell.Object, albumList ));
+			}
+
+			private void SetupTagProvider( IEnumerable<DbTagAssociation> tagList ) {
+				mTagAssociationProvider.Setup( p => p.GetArtistTagList( It.IsAny<long>(), It.IsAny<eTagGroup>()))
+					.Returns( new DataProviderList<DbTagAssociation>( mDatabaseShell.Object, tagList ));
+			} 
+			
 			[Test]
 			public void RemovesAllPreviousTags() {
 				CreateMocks();
 
-				var nowTicks = DateTime.Now.Ticks - 10; // in the past.
-				var databaseShell = new Mock<IDatabaseShell>();
-
-				mTimestampProvider.Setup( x => x.GetTimestamp( It.IsAny<string>())).Returns( nowTicks );
+				SetupTimestampProvider( DateTime.Now.Ticks - 10 ); // in the past.
 
 				var artist = new DbArtist();
-				var artists = new List<DbArtist> { artist };
-				mArtistProvider.Setup( m => m.GetArtistList()).Returns( new DataProviderList<DbArtist>( databaseShell.Object, artists ));
-				mArtistProvider.Setup( m => m.GetArtist( It.IsAny<long>())).Returns( artist );
+				SetupArtistProvider( artist );
 
-				var albumList = new DataProviderList<DbAlbum>( databaseShell.Object, new List<DbAlbum>());
-				mAlbumProvider.Setup( m => m.GetAlbumList( It.IsAny<long>())).Returns( albumList );
+				SetupAlbumProvider( new List<DbAlbum>());
 
 				var association = new DbTagAssociation( eTagGroup.Decade, 1, 1, 1 );
-				var associationList = new List<DbTagAssociation> { association };
-				var providerList = new DataProviderList<DbTagAssociation>( databaseShell.Object, associationList );
+				SetupTagProvider( new List<DbTagAssociation> { association });
 
-				mTagAssociationProvider.Setup( p => p.GetArtistTagList( It.IsAny<long>(), It.IsAny<eTagGroup>())).Returns( providerList );
 				mTagAssociationProvider.Setup( p => p.RemoveAssociation( It.Is<long>( l => l == association.DbId ))).Verifiable();
 
 				var sut = CreateSut();
 				sut.Initialize();
 				sut.ExecuteTask();
+
 				mTagAssociationProvider.Verify();
 			}
 
 			[Test]
 			public void AddsNewTag() {
-				
+				CreateMocks();
+
+				SetupTimestampProvider( DateTime.Now.Ticks - 10 ); // in the past.
+
+				var artist = new DbArtist();
+				SetupArtistProvider( artist );
+
+				var album = new DbAlbum { PublishedYear = 23 };
+				SetupAlbumProvider( new List<DbAlbum>{ album });
+
+				SetupTagProvider( new List<DbTagAssociation>());
+
+				var decadeOne = new DbDecadeTag( "one" ) { StartYear = 10, EndYear = 19 };
+				var decadeTwo = new DbDecadeTag( "Two" ) { StartYear = 20, EndYear = 29 };
+				var decadeThr = new DbDecadeTag( "3rd" ) { StartYear = 30, EndYear = 39 };
+				var	decadeTagList = new List<DbDecadeTag> { decadeTwo, decadeThr, decadeOne };
+				mTagManager.Setup( m => m.DecadeTagList ).Returns( decadeTagList );
+
+				mTagAssociationProvider.Setup( m => m.AddAssociation( It.Is<DbTagAssociation>( item => item.TagId == decadeTwo.DbId ))).Verifiable();
+
+				var sut = CreateSut();
+				sut.Initialize();
+				sut.ExecuteTask();
+
+				mTagAssociationProvider.Verify();
 			}
 
 			[Test]
 			public void DoesNotAddUnTaggedYear() {
-				
+				CreateMocks();
+
+				SetupTimestampProvider( DateTime.Now.Ticks - 10 ); // in the past.
+
+				var artist = new DbArtist();
+				SetupArtistProvider( artist );
+
+				var album = new DbAlbum { PublishedYear = Constants.cUnknownYear };
+				SetupAlbumProvider( new List<DbAlbum>{ album });
+
+				SetupTagProvider( new List<DbTagAssociation>());
+
+				var decadeOne = new DbDecadeTag( "one" ) { StartYear = 10, EndYear = 99 };
+				var	decadeTagList = new List<DbDecadeTag> { decadeOne };
+				mTagManager.Setup( m => m.DecadeTagList ).Returns( decadeTagList );
+
+				var sut = CreateSut();
+				sut.Initialize();
+				sut.ExecuteTask();
+
+				mTagAssociationProvider.Verify( m => m.AddAssociation( It.IsAny<DbTagAssociation>()), Times.Never());
 			}
 
 			[Test]
 			public void DoesNotRescanUnchangedArtist() {
-				
+				CreateMocks();
+
+				SetupTimestampProvider(( DateTime.Now + new TimeSpan( 1, 0 , 0 )).Ticks ); // one hour in the future
+
+				var artist = new DbArtist();
+				SetupArtistProvider( artist );
+
+				SetupTagProvider( new List<DbTagAssociation>());
+
+				var sut = CreateSut();
+				sut.Initialize();
+				sut.ExecuteTask();
+
+				mAlbumProvider.Verify( m => m.GetAlbumList( It.IsAny<long>()), Times.Never());
 			}
 		}
 	}
