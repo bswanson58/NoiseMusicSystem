2a2d2cdc8e85b27549750552e68ca6b5ed292ba7
Bill Swanson <bswanson58@gmail.com>
2012-01-13 13:02:25 -0600

    Added integration tests for the GenreProvider class.

---
+++ Noise.Core.IntegrationTests/Database/GenreProviderTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Noise.Core.Database;
+using Noise.Infrastructure.Dto;
+using Noise.Infrastructure.Interfaces;
+
+namespace Noise.Core.IntegrationTests.Database {
+	[TestFixture]
+	public class GenreProviderTests : BaseDatabaseProviderTests {
+
+		private IGenreProvider CreateSut() {
+			return( new GenreProvider( mDatabaseManager ));
+		}
+
+		[Test]
+		public void CanAddGenre() {
+			var genre = new DbGenre();
+
+			var sut = CreateSut();
+
+			sut.AddGenre( genre );
+		}
+
+		[Test]
+		[ExpectedException( typeof( ArgumentNullException ))]
+		public void CannotAddNullGenre() {
+			var sut = CreateSut();
+
+			sut.AddGenre( null );
+		}
+
+		[Test]
+		public void CanGetGenreList() {
+			var genre = new DbGenre();
+
+			var sut = CreateSut();
+			sut.AddGenre( genre );
+
+			using( var list = sut.GetGenreList()) {
+				list.List.Should().HaveCount( 1 );
+			}
+		}
+	}
+}
