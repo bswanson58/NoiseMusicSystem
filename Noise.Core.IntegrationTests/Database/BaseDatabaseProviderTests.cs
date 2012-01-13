36747a8c50a875bdec8147cf0a85777c9cc6b6ac
Bill Swanson <bswanson58@gmail.com>
2012-01-13 12:35:11 -0600

    Added integration tests for the ExpiringContentProvider class.

---
+++ Noise.Core.IntegrationTests/Database/BaseDatabaseProviderTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Microsoft.Practices.Prism.Events;
+using Moq;
+using NUnit.Framework;
+using Noise.AppSupport;
+using Noise.EloqueraDatabase;
+using Noise.EloqueraDatabase.BlobStore;
+using Noise.EloqueraDatabase.Database;
+using Noise.Infrastructure;
+using Noise.Infrastructure.Configuration;
+using Noise.Infrastructure.Interfaces;
+
+namespace Noise.Core.IntegrationTests.Database {
+	public class BaseDatabaseProviderTests {
+		protected Mock<ILog>						mDummyLog;
+		protected IIoc								mIocProvider;
+		protected DatabaseConfiguration				mDatabaseConfiguration;
+		protected IBlobStorageResolver				mBlobResolver;
+		protected IDatabaseFactory					mDatabaseFactory;
+		protected IDatabaseManager					mDatabaseManager;
+		protected Mock<IEventAggregator>			mEventAggregator;
+
+		[SetUp]
+		public virtual void Setup() {
+			mDummyLog = new Mock<ILog>();
+			NoiseLogger.Current = mDummyLog.Object;
+				
+			mEventAggregator = new Mock<IEventAggregator> { DefaultValue = DefaultValue.Mock };
+
+			mDatabaseConfiguration = new DatabaseConfiguration { DatabaseName = "Integration Test Database" };
+
+			mIocProvider = new IocProvider();
+			mBlobResolver = new BlobStorageResolver();
+			mDatabaseFactory = new EloqueraDatabaseFactory( mBlobResolver, mEventAggregator.Object, mIocProvider, mDatabaseConfiguration );
+			mDatabaseManager = new DatabaseManager( mDatabaseFactory );
+
+			if( mDatabaseManager.Initialize()) {
+				using( var database = mDatabaseManager.CreateDatabase()) {
+					database.Database.OpenWithCreateDatabase();
+				}
+			}
+		}
+
+		[TearDown]
+		public virtual void Teardown() {
+			mDatabaseManager.ReservedDatabaseCount.Should().Be( 0 );
+
+			using( var database = mDatabaseManager.CreateDatabase()) {
+				database.Database.DeleteDatabase();
+			}
+		}
+	}
+}
---
+++ Noise.Core.IntegrationTests/Database/ExpiringContentProviderTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Noise.Core.Database;
+using Noise.Infrastructure.Dto;
+using Noise.Infrastructure.Interfaces;
+
+namespace Noise.Core.IntegrationTests.Database {
+	[TestFixture]
+	public class ExpiringContentProviderTests : BaseDatabaseProviderTests {
+
+		private IExpiringContentProvider CreateSut() {
+			return( new ExpiringContentProvider( mDatabaseManager ));
+		}
+
+		[Test]
+		public void CanGetContentList() {
+			var dbArtwork1 = new DbArtwork( 1, ContentType.AlbumCover );
+			var dbArtwork2 = new DbArtwork( 2, ContentType.AlbumCover );
+			var artworkProvider = new ArtworkProvider( mDatabaseManager );
+
+			artworkProvider.AddArtwork( dbArtwork1 );
+			artworkProvider.AddArtwork( dbArtwork2 );
+
+			var sut = CreateSut();
+
+			using( var list = sut.GetContentList( 1, ContentType.AlbumCover )) {
+				list.List.Should().HaveCount( 1 );
+			}
+		}
+
+		[Test]
+		public void CanGetAlbumContentList() {
+			var album = new DbAlbum();
+			var dbArtwork1 = new DbArtwork( 1, ContentType.AlbumCover ) { Album = album.DbId };
+			var dbArtwork2 = new DbArtwork( 2, ContentType.AlbumArtwork) { Album = album.DbId + 1 };
+			var artworkProvider = new ArtworkProvider( mDatabaseManager );
+
+			artworkProvider.AddArtwork( dbArtwork1 );
+			artworkProvider.AddArtwork( dbArtwork2 );
+			
+			var sut = CreateSut();
+
+			using( var list = sut.GetAlbumContentList( album.DbId )) {
+				list.List.Should().HaveCount( 1 );
+			}
+		}
+	}
+}
