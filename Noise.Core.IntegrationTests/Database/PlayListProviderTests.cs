5e22dffa843f89b81c0f2b1ba6bd628ebb4f5d89
Bill Swanson <bswanson58@gmail.com>
2012-02-05 14:27:53 -0600

    Added integration tests for the PlayListProvider class.

---
+++ Noise.Core.IntegrationTests/Database/PlayListProviderTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using Noise.Core.Database;
+using Noise.Infrastructure.Dto;
+using Noise.Infrastructure.Interfaces;
+
+namespace Noise.Core.IntegrationTests.Database {
+	[TestFixture]
+	public class PlayListProviderTests : BaseDatabaseProviderTests {
+
+		private IPlayListProvider CreateSut() {
+			return( new PlayListProvider( mEventAggregator.Object, mDatabaseManager ));
+		}
+
+		[Test]
+		public void CanAddPlayList() {
+			var playList = new DbPlayList();
+			var sut = CreateSut();
+
+			sut.AddPlayList( playList );
+		}
+
+		[Test]
+		[ExpectedException( typeof( ArgumentNullException ))]
+		public void CannotAddNullPlayList() {
+			var sut = CreateSut();
+
+			sut.AddPlayList( null );
+		}
+
+		[Test]
+		public void CanRetrievePlayList() {
+			var playList = new DbPlayList();
+
+			var sut = CreateSut();
+			sut.AddPlayList( playList );
+
+			var retrievedPlayList = sut.GetPlayList( playList.DbId );
+
+			playList.ShouldHave().AllProperties().EqualTo( retrievedPlayList );
+		}
+
+		[Test]
+		public void CanDeletePlayList() {
+			var playList = new DbPlayList();
+
+			var sut = CreateSut();
+			sut.AddPlayList( playList );
+
+			sut.DeletePlayList( playList );
+
+			var retrievedPlayList = sut.GetPlayList( playList.DbId );
+
+			Assert.IsNull( retrievedPlayList );
+		}
+
+		[Test]
+		public void CanRetrieveAllPlayLists() {
+			var playList1 = new DbPlayList( "list one", "description", new List<long> { 0, 1, 2 } );
+			var playList2 = new DbPlayList( "list two", "description", new List<long> { 1, 2, 3 } );
+
+			var sut = CreateSut();
+			sut.AddPlayList( playList1 );
+			sut.AddPlayList( playList2 );
+
+			using( var playLists = sut.GetPlayLists()) {
+				Assert.IsNotNull( playLists );
+				Assert.IsNotNull( playLists.List );
+
+				playLists.List.Should().HaveCount( 2 );
+			}
+		}
+
+		[Test]
+		public void CanRetrievePlayListForUpdate() {
+			var playList = new DbPlayList();
+
+			var sut = CreateSut();
+			sut.AddPlayList( playList );
+
+			using( var updater = sut.GetPlayListForUpdate( playList.DbId )) {
+				Assert.IsNotNull( updater );
+				Assert.IsNotNull( updater.Item );
+
+				playList.ShouldHave().AllProperties().EqualTo( updater.Item );
+			}
+		}
+
+		[Test]
+		public void CanUpdatePlayList() {
+			var playList = new DbPlayList( "list name", "list description", new List<long> { 1, 2, 3, 4 });
+
+			var sut = CreateSut();
+			sut.AddPlayList( playList );
+
+			using( var updater = sut.GetPlayListForUpdate( playList.DbId )) {
+				updater.Item.Name = "new list name";
+
+				updater.Update();
+			}
+
+			var retrievedItem = sut.GetPlayList( playList.DbId );
+
+			playList.Name.Should().NotBe( retrievedItem.Name );
+		}
+	}
+}
