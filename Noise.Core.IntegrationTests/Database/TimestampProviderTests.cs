0b80e764641dcf03a04825396715760221ea62e7
Bill Swanson <bswanson58@gmail.com>
2012-02-14 13:31:46 -0600

    Added tests for the TimestampProvider class.

---
+++ Noise.Core.IntegrationTests/Database/TimestampProviderTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Noise.Core.Database;
+using Noise.Infrastructure.Interfaces;
+
+namespace Noise.Core.IntegrationTests.Database {
+	[TestFixture]
+	public class TimestampProviderTests : BaseDatabaseProviderTests {
+
+		private ITimestampProvider CreateSut() {
+			return( new TimestampProvider( mDatabaseManager ));
+		}
+
+		[Test]
+		public void CanSetNewTimestamp() {
+			var sut = CreateSut();
+
+			sut.SetTimestamp( "component", 1 );
+		}
+
+		[Test]
+		public void CanRetrieveTimestamp() {
+			var sut = CreateSut();
+
+			sut.SetTimestamp( "component1", 17 );
+			var retrievedTimestamp = sut.GetTimestamp( "component1" );
+
+			retrievedTimestamp.Should().Be( 17 );
+		}
+
+		[Test]
+		public void CanUpdateTimestamp() {
+			var sut = CreateSut();
+
+			sut.SetTimestamp( "component1", 17 );
+			sut.SetTimestamp( "component1", 23 );
+			var retrievedTimestamp = sut.GetTimestamp( "component1" );
+
+			retrievedTimestamp.Should().Be( 23 );
+		}
+	}
+}
