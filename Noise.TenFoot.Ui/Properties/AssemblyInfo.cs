862a1794fe134f35d26ae0bea0afdc11641c0a78
Bill Swanson <bswanson58@gmail.com>
2012-03-25 14:30:43 -0500

    Added assemblies for the Ten Foot UI application and user interface.

---
+++ Noise.TenFoot.Ui/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "Noise.TenFoot.Ui" )]
+[assembly: AssemblyDescription( "Media Screen User Interface for the Noise Music System" )]
+[assembly: AssemblyProduct( "Noise Music System" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "ac3543d9-a818-4961-8530-28568d7ee073" )]
