4f0d554b63d890d8ee25a405689ed3072da0d3f2
Bill Swanson <bswanson58@gmail.com>
2012-03-26 11:39:18 -0500

    Added Autofac support to the TenFooter application.

--- Noise.TenFooter/AppBootstrapper.cs
+++ Noise.TenFooter/AppBootstrapper.cs
@@ -1,56 +1,19 @@
-﻿namespace Noise.TenFooter
-{
-	using System;
-	using System.Collections.Generic;
-	using System.ComponentModel.Composition;
-	using System.ComponentModel.Composition.Hosting;
-	using System.ComponentModel.Composition.Primitives;
-	using System.Linq;
-	using Caliburn.Micro;
+﻿using Autofac;
+using Caliburn.Micro.Autofac;
 
-	public class AppBootstrapper : Bootstrapper<IShell>
-	{
-		CompositionContainer container;
+namespace Noise.TenFooter {
+	public class AppBootstrapper : AutofacBootstrapper<ShellViewModel> {
+		protected override void ConfigureBootstrapper() {
+			base.ConfigureBootstrapper();
 
-		/// <summary>
-		/// By default, we are configured to use MEF
-		/// </summary>
-		protected override void Configure() {
-		    var catalog = new AggregateCatalog(
-		        AssemblySource.Instance.Select(x => new AssemblyCatalog(x)).OfType<ComposablePartCatalog>()
-		        );
-
-			container = new CompositionContainer(catalog);
-
-			var batch = new CompositionBatch();
-
-			batch.AddExportedValue<IWindowManager>(new WindowManager());
-			batch.AddExportedValue<IEventAggregator>(new EventAggregator());
-			batch.AddExportedValue(container);
-		    batch.AddExportedValue(catalog);
-
-			container.Compose(batch);
+			//  override namespace naming convention
+			EnforceNamespaceConvention = false;
+			//  change our view model base type
+			ViewModelBaseType = typeof( IShell );
 		}
 
-		protected override object GetInstance(Type serviceType, string key)
-		{
-			string contract = string.IsNullOrEmpty(key) ? AttributedModelServices.GetContractName(serviceType) : key;
-			var exports = container.GetExportedValues<object>(contract);
+		protected override void ConfigureContainer( ContainerBuilder builder ) {
 
-			if (exports.Count() > 0)
-				return exports.First();
-
-			throw new Exception(string.Format("Could not locate any instances of contract {0}.", contract));
-		}
-
-		protected override IEnumerable<object> GetAllInstances(Type serviceType)
-		{
-			return container.GetExportedValues<object>(AttributedModelServices.GetContractName(serviceType));
-		}
-
-		protected override void BuildUp(object instance)
-		{
-			container.SatisfyImportsOnce(instance);
 		}
 	}
 }
