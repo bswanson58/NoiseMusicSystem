2ccc2a829219fba4536535898a43afe77cff7fd2
Bill Swanson <bswanson58@gmail.com>
2012-03-28 10:52:39 -0500

    Moved the generic style for the LoopingListBox into the generic.xaml file.

--- Noise.TenFoot.Ui/Properties/AssemblyInfo.cs
+++ Noise.TenFoot.Ui/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -15,3 +16,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid( "ac3543d9-a818-4961-8530-28568d7ee073" )]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
