c03e4ef7f65066d9be6179fe184ac921482bc10e
Bill Swanson <bswanson58@gmail.com>
2012-03-28 13:15:52 -0500

    Changed the main menu interface to the actual choices.

--- Noise.TenFoot.Ui/ViewModels/HomeViewModel.cs
+++ Noise.TenFoot.Ui/ViewModels/HomeViewModel.cs
@@ -1,24 +1,35 @@
 ﻿using Caliburn.Micro;
 using Noise.TenFoot.Ui.Controls.LoopingListBox;
+using Noise.TenFoot.Ui.Dto;
 using Noise.TenFoot.Ui.Views;
 
 namespace Noise.TenFoot.Ui.ViewModels {
 	public class HomeViewModel : Screen {
-		private readonly BindableCollection<string>	mMenuChoices;
+		private readonly BindableCollection<UiMenuItem>	mMenuChoices;
+		private UiMenuItem								mSelectedMenuItem;
+
 		private LoopingListBox	mListBox;
 
 		public HomeViewModel() {
-			mMenuChoices = new BindableCollection<string> { "Library", "Favorites", "Queue", "What's New" };
+			mMenuChoices = new BindableCollection<UiMenuItem> { new UiMenuItem( eMainMenuCommand.Library, "Library", null ),
+																new UiMenuItem( eMainMenuCommand.Favorites, "Favorites", null ),
+																new UiMenuItem( eMainMenuCommand.Queue, "Queue", null ),
+																new UiMenuItem( eMainMenuCommand.Search, "Search", null )};
 		}
 
-		public object MenuChoices {
+		public BindableCollection<UiMenuItem> MenuList {
 			get{ return( mMenuChoices ); }
+		}
+
+		public UiMenuItem SelectedMenuList {
+			get{ return( mSelectedMenuItem ); }
+			set{ mSelectedMenuItem = value; }
 		}
 
 		protected override void OnViewAttached( object view, object context ) {
 			base.OnViewAttached( view, context );
 
-			mListBox = (view as HomeView).listBox;
+			mListBox = (view as HomeView).MenuList;
 		}
 
 		public void ScrollUp() {
