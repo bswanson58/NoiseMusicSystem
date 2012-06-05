086eb1c051df7e83fa303b9e2bd3ad42a48d40c0
Bill Swanson <bswanson58@gmail.com>
2012-06-05 10:17:53 -0500

    Added the singleton design pattern.

---
+++ ReusableBits.Tests/Patterns/SingletonBaseTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using NUnit.Framework;
+using ReusableBits.Patterns;
+
+namespace ReusableBits.Tests.Patterns {
+	public interface ITestClass {
+		void	SingletonMethod();
+	}
+
+	public class TestClass : ITestClass {
+		public static int	CallCount { get; private set; }
+		
+		public void SingletonMethod() {
+			CallCount++;
+		}
+
+		public static void ResetCallCount() {
+			CallCount = 0;
+		}
+	}
+
+	public class ReplacementClass : ITestClass {
+		public int	CallCount { get; private set; }
+		
+		public void SingletonMethod() {
+			CallCount++;
+		}
+	}
+
+	public class TestSingleton : SingletonBase<ITestClass, TestClass> { }
+
+	internal class TestableSingleton : TestSingleton {
+		public static void ResetDefault() {
+			mDefault = new Lazy<ITestClass>( InternalCreateDefault );
+			Current = null;
+			DefaultCreator = null;
+		}
+	}
+
+	[TestFixture]
+	public class SingletonBaseTests {
+		[SetUp]
+		public void Setup() {
+			TestClass.ResetCallCount();
+			TestableSingleton.ResetDefault();
+		}
+
+		[Test]
+		public void CanUseDefaultSingleton() {
+			TestableSingleton.Current.SingletonMethod();
+	
+			Assert.IsTrue( TestClass.CallCount == 1 );
+		}
+
+		[Test]
+		public void CanReplaceSingleton() {
+			var replacement = new ReplacementClass();
+
+			TestableSingleton.Current = replacement;
+			TestableSingleton.Current.SingletonMethod();
+
+			Assert.IsTrue( replacement.CallCount == 1 );
+			Assert.IsTrue( TestClass.CallCount == 0 );
+		}
+
+		[Test]
+		public void CanReplaceDefaultCreator() {
+			var replacement = new ReplacementClass();
+
+			TestableSingleton.DefaultCreator = () => replacement;
+			TestableSingleton.Current.SingletonMethod();
+
+			Assert.IsTrue( replacement.CallCount == 1 );
+			Assert.IsTrue( TestClass.CallCount == 0 );
+		}
+
+		[Test]
+		public void SingletonIsReused() {
+			TestableSingleton.Current.SingletonMethod();
+			TestableSingleton.Current.SingletonMethod();
+
+			Assert.IsTrue( TestClass.CallCount == 2 );
+		}
+
+		[Test]
+		public void ReplacementSingletonIsReused() {
+			var replacement = new ReplacementClass();
+			TestableSingleton.Current = replacement;
+			
+			TestableSingleton.Current.SingletonMethod();
+			TestableSingleton.Current.SingletonMethod();
+
+			Assert.IsTrue( replacement.CallCount == 2 );
+		}
+	}
+}
---
+++ ReusableBits/Patterns/SingletonBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ReusableBits.Patterns {
+	/// <summary>
+	/// A generic base class that may be used to implement a testable singleton.
+	/// </summary>
+	/// <typeparam name="TInterface">An interface exposed by the singleton</typeparam>
+	/// <typeparam name="TClass">The concrete class implementing the interface</typeparam>
+	public class SingletonBase<TInterface, TClass>
+			where TInterface : class 
+			where TClass : class, TInterface, new() {
+		protected static Lazy<TInterface>		mDefault; 
+		private   static volatile TInterface	mCurrent;
+		private   static Func<TInterface>		mDefaultCreator;
+
+		static SingletonBase() {
+			mDefault = new Lazy<TInterface>( InternalCreateDefault );
+		} 
+
+		public static Func<TInterface> DefaultCreator {
+			get { return( mDefaultCreator ); }
+			set { mDefaultCreator = value; }
+		}
+
+		public static TInterface Current {
+			get { return( mCurrent ?? ( mCurrent = mDefault.Value )); }
+			set { mCurrent = value; }
+		}
+
+		protected static TInterface InternalCreateDefault() {
+			if( mDefaultCreator != null ) {
+				return( mDefaultCreator());
+			}
+
+			return( new TClass());
+		}
+	}
+}
