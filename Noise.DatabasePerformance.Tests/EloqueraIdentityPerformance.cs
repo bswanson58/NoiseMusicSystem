1c6029f989fab503cead230acec3ad719a523aea
Bill Swanson <bswanson58@gmail.com>
2012-06-05 11:21:55 -0500

    Converted all usage of Prism and Unity to use NuGet.

---
+++ Noise.DatabasePerformance.Tests/EloqueraIdentityPerformance.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using Noise.EloqueraDatabase;
+
+namespace Noise.DatabasePerformance.Tests {
+	[TestFixture]
+	public class EloqueraIdentityPerformance {
+		private readonly IUnityContainer	mContainer;
+
+		public EloqueraIdentityPerformance() {
+			mContainer = new UnityContainer();
+
+			var databaseModule = new EloqueraDatabaseModule( mContainer );
+		}
+
+		private void CreateDatabaseFactory() {
+			
+		}
+	}
+}
