fb35f204fc9ba2a5a9f3184e5a30d2a32261411c
Bill Swanson <bswanson58@gmail.com>
2012-06-19 09:28:01 -0500

    Added the DatabaseIdentityProvider class.

---
+++ Noise.Infrastructure/Support/DatabaseIdentityProvider.cs
@@ -0,0 +1,8 @@
+﻿using ReusableBits.Interfaces;
+using ReusableBits.Patterns;
+using ReusableBits.Support;
+
+namespace Noise.Infrastructure.Support {
+	public class DatabaseIdentityProvider : SingletonBase<IIdentityProvider, IdentityProvider> {
+	}
+}
