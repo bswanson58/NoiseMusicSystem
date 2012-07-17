daf17f4c7843290a3b94619edb7bb5f9da21960d
Bill Swanson <bswanson58@gmail.com>
2012-07-17 14:04:49 -0500

    Added a Unity extension class to complete Caliburn.Micro event subscription when objects are resolved.

---
+++ ReusableBits.Mvvm/CaliburnSupport/UnityEventSubscriber.cs
@@ -0,0 +1,33 @@
+﻿using Caliburn.Micro;
+using Microsoft.Practices.ObjectBuilder2;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.ObjectBuilder;
+
+namespace ReusableBits.Mvvm.CaliburnSupport {
+	public class UnityEventSubscriber : UnityContainerExtension {
+		private readonly IEventAggregator	mEventAggregator;
+
+		public UnityEventSubscriber( IEventAggregator eventAggregator ) {
+			mEventAggregator = eventAggregator;
+		}
+
+		protected override void Initialize() {
+			Context.Strategies.Add( new EventSubscriberStrategy( mEventAggregator ), UnityBuildStage.PostInitialization );
+		}
+
+		private class EventSubscriberStrategy : BuilderStrategy {
+			private readonly IEventAggregator	mEventAggregator;
+
+			public EventSubscriberStrategy( IEventAggregator eventAggregator ) {
+				mEventAggregator = eventAggregator;
+			}
+
+			public override void PostBuildUp( IBuilderContext context ) {
+				if(( context.Existing != null ) &&
+				   ( context.Existing is IHandle )) {
+					mEventAggregator.Subscribe( context.Existing );
+				}
+			}
+		}
+	}
+}
