adfe8423a7030e3f9b1322eea89f20f461dad8e1
Bill Swanson <bswanson58@gmail.com>
2012-07-23 13:53:03 -0500

    Added tests for the BasePipelineStep class.

---
+++ Noise.Core.Tests/FileProcessor/BasePipelineStepTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Noise.Core.FileProcessor;
+
+namespace Noise.Core.Tests.FileProcessor {
+	[TestFixture]
+	public class BasePipelineStepTests {
+		internal class TestPipelineStep : BasePipelineStep {
+			public TestPipelineStep() :
+				base( ePipelineStep.Completed ) { }
+
+			public override void ProcessStep( PipelineContext context ) { }
+		}
+
+		[Test]
+		public void CanAppendNextStep() {
+			var sut = new TestPipelineStep();
+			var nextStep = new TestPipelineStep();
+			var context = new PipelineContext( null, null, null, null );
+
+			sut.AppendStep( nextStep );
+
+			var result = sut.Process( context );
+
+			result.Should().Be( nextStep );
+		}
+
+		[Test]
+		public void AppendedStepIsLastInChain() {
+			var sut = new TestPipelineStep();
+			var step2 = new TestPipelineStep();
+			var step3 = new TestPipelineStep();
+			var context = new PipelineContext( null, null, null, null );
+
+			sut.AppendStep( step2 );
+			sut.AppendStep( step3 );
+
+			var result = sut.Process( context );
+			result = result.Process( context );
+
+			result.Should().Be( step3 );
+		}
+	}
+}
