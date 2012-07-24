2c3e7eb0574f3c231f7e14d15c891ab0747dea40
Bill Swanson <bswanson58@gmail.com>
2012-07-24 11:05:00 -0500

    Added tests for the UpdateArtworkPipelineStep class.

---
+++ Noise.Core.Tests/FileProcessor/UpdateArtworkPipelineStepTests.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Noise.Core.FileProcessor;
+using Noise.Infrastructure;
+using Noise.Infrastructure.Dto;
+using Noise.Infrastructure.Interfaces;
+using ReusableBits.TestSupport.Mocking;
+
+namespace Noise.Core.Tests.FileProcessor {
+	[TestFixture]
+	public class UpdateArtworkPipelineStepTests {
+		internal class TestableUpdateArtworkPipelineStep : Testable<UpdateArtworkPipelineStep> { }
+
+		private DatabaseChangeSummary	mSummary;
+		private StorageFile				mStorageFile;
+		private DbArtist				mArtist;
+		private DbAlbum					mAlbum;
+
+		[SetUp]
+		public void Setup() {
+			mSummary = new DatabaseChangeSummary();
+			mStorageFile = new StorageFile();
+			mArtist = new DbArtist();
+			mAlbum = new DbAlbum();
+
+			NoiseLogger.Current = new Mock<ILog>().Object;
+		}
+
+		[Test]
+		public void UpdateStepRequiresArtist() {
+			var testable = new TestableUpdateArtworkPipelineStep();
+			var sut = testable.ClassUnderTest;
+			var context = new PipelineContext( null, null, mStorageFile, mSummary );
+
+			sut.ProcessStep( context );
+			testable.Mock<IArtworkProvider>().Verify( m => m.AddArtwork( It.IsAny<DbArtwork>(), It.IsAny<string>()), Times.Never());
+		}
+
+		[Test]
+		public void UpdateStepRequiresAlbum() {
+			var testable = new TestableUpdateArtworkPipelineStep();
+			var sut = testable.ClassUnderTest;
+			var context = new PipelineContext( null, null, mStorageFile, mSummary );
+
+			sut.ProcessStep( context );
+			testable.Mock<IArtworkProvider>().Verify( m => m.AddArtwork( It.IsAny<DbArtwork>(), It.IsAny<string>()), Times.Never());
+		}
+
+		[Test]
+		public void UpdateStepAddsArtwork() {
+			var testable = new TestableUpdateArtworkPipelineStep();
+			var sut = testable.ClassUnderTest;
+			var context = new PipelineContext( null, null, mStorageFile, mSummary ) { Artist = mArtist, Album = mAlbum };
+
+			testable.Mock<IStorageFileProvider>().Setup( m => m.GetFileForUpdate( mStorageFile.DbId )).Returns( new Mock<IDataUpdateShell<StorageFile>>().Object );
+			testable.Mock<IArtistProvider>().Setup( m => m.GetArtistForUpdate( mArtist.DbId )).Returns( new Mock<IDataUpdateShell<DbArtist>>().Object );
+
+			sut.ProcessStep( context );
+
+			testable.Mock<IArtworkProvider>().Verify( m => m.AddArtwork( It.IsAny<DbArtwork>(), It.IsAny<string>()), Times.Once());
+		}
+
+		[Test]
+		public void UpdateStepSetsArtist() {
+			var testable = new TestableUpdateArtworkPipelineStep();
+			var sut = testable.ClassUnderTest;
+			var context = new PipelineContext( null, null, mStorageFile, mSummary ) { Artist = mArtist, Album = mAlbum };
+			DbArtwork	artwork = null;
+
+			testable.Mock<IStorageFileProvider>().Setup( m => m.GetFileForUpdate( mStorageFile.DbId )).Returns( new Mock<IDataUpdateShell<StorageFile>>().Object );
+			testable.Mock<IArtistProvider>().Setup( m => m.GetArtistForUpdate( mArtist.DbId )).Returns( new Mock<IDataUpdateShell<DbArtist>>().Object );
+			testable.Mock<IArtworkProvider>().Setup( m => m.AddArtwork( It.IsAny<DbArtwork>(), It.IsAny<string>()))
+																			.Callback<DbArtwork, string>(( a, p ) => artwork = a  );
+
+			sut.ProcessStep( context );
+
+			artwork.Should().NotBeNull();
+			artwork.Artist.Should().Be( mArtist.DbId );
+		}
+
+
+		[Test]
+		public void UpdateStepSetsAlbum() {
+			var testable = new TestableUpdateArtworkPipelineStep();
+			var sut = testable.ClassUnderTest;
+			var context = new PipelineContext( null, null, mStorageFile, mSummary ) { Artist = mArtist, Album = mAlbum };
+			DbArtwork	artwork = null;
+
+			testable.Mock<IStorageFileProvider>().Setup( m => m.GetFileForUpdate( mStorageFile.DbId )).Returns( new Mock<IDataUpdateShell<StorageFile>>().Object );
+			testable.Mock<IArtistProvider>().Setup( m => m.GetArtistForUpdate( mArtist.DbId )).Returns( new Mock<IDataUpdateShell<DbArtist>>().Object );
+			testable.Mock<IArtworkProvider>().Setup( m => m.AddArtwork( It.IsAny<DbArtwork>(), It.IsAny<string>()))
+																			.Callback<DbArtwork, string>(( a, p ) => artwork = a  );
+
+			sut.ProcessStep( context );
+
+			artwork.Should().NotBeNull();
+			artwork.Album.Should().Be( mAlbum.DbId );
+		}
+
+		[Test]
+		public void UpdateStepSetsArtworkPath() {
+			var testable = new TestableUpdateArtworkPipelineStep();
+			var sut = testable.ClassUnderTest;
+			var storageFile = new StorageFile( "file name", 123, 321, DateTime.Now );
+			var context = new PipelineContext( null, null, storageFile, mSummary ) { Artist = mArtist, Album = mAlbum };
+			DbArtwork	artwork = null;
+
+			
+			testable.Mock<IStorageFileProvider>().Setup( m => m.GetFileForUpdate( storageFile.DbId )).Returns( new Mock<IDataUpdateShell<StorageFile>>().Object );
+			testable.Mock<IArtistProvider>().Setup( m => m.GetArtistForUpdate( mArtist.DbId )).Returns( new Mock<IDataUpdateShell<DbArtist>>().Object );
+			testable.Mock<IArtworkProvider>().Setup( m => m.AddArtwork( It.IsAny<DbArtwork>(), It.IsAny<string>()))
+																			.Callback<DbArtwork, string>(( a, p ) => artwork = a  );
+
+			sut.ProcessStep( context );
+
+			artwork.Should().NotBeNull();
+			artwork.FolderLocation.Should().Be( storageFile.ParentFolder );
+		}
+
+		[Test]
+		public void UpdateStepSetsArtworkName() {
+			var testable = new TestableUpdateArtworkPipelineStep();
+			var sut = testable.ClassUnderTest;
+			const string fileName = "file name";
+			var storageFile = new StorageFile( fileName + ".jpg", 123, 321, DateTime.Now );
+			var context = new PipelineContext( null, null, storageFile, mSummary ) { Artist = mArtist, Album = mAlbum };
+			DbArtwork	artwork = null;
+
+			
+			testable.Mock<IStorageFileProvider>().Setup( m => m.GetFileForUpdate( storageFile.DbId )).Returns( new Mock<IDataUpdateShell<StorageFile>>().Object );
+			testable.Mock<IArtistProvider>().Setup( m => m.GetArtistForUpdate( mArtist.DbId )).Returns( new Mock<IDataUpdateShell<DbArtist>>().Object );
+			testable.Mock<IArtworkProvider>().Setup( m => m.AddArtwork( It.IsAny<DbArtwork>(), It.IsAny<string>()))
+																			.Callback<DbArtwork, string>(( a, p ) => artwork = a  );
+
+			sut.ProcessStep( context );
+
+			artwork.Should().NotBeNull();
+			artwork.Name.Should().Be( fileName );
+		}
+
+		[Test]
+		public void UpdateShouldSetStorageFileMetaDataPointer() {
+			var testable = new TestableUpdateArtworkPipelineStep();
+			var sut = testable.ClassUnderTest;
+			var context = new PipelineContext( null, null, mStorageFile, mSummary ) { Artist = mArtist, Album = mAlbum };
+			var updater = new Mock<IDataUpdateShell<StorageFile>>();
+			DbArtwork	artwork = null;
+
+			updater.Setup( m => m.Item ).Returns( mStorageFile );
+			testable.Mock<IStorageFileProvider>().Setup( m => m.GetFileForUpdate( mStorageFile.DbId )).Returns( updater.Object );
+			testable.Mock<IArtistProvider>().Setup( m => m.GetArtistForUpdate( mArtist.DbId )).Returns( new Mock<IDataUpdateShell<DbArtist>>().Object );
+			testable.Mock<IArtworkProvider>().Setup( m => m.AddArtwork( It.IsAny<DbArtwork>(), It.IsAny<string>()))
+																			.Callback<DbArtwork, string>(( a, p ) => artwork = a  );
+
+			sut.ProcessStep( context );
+
+			mStorageFile.MetaDataPointer.Should().Be( artwork.DbId );
+		}
+
+		[Test]
+		public void UpdateShouldSetStorageFileTypeToMusic() {
+			var testable = new TestableUpdateArtworkPipelineStep();
+			var sut = testable.ClassUnderTest;
+			var context = new PipelineContext( null, null, mStorageFile, mSummary ) { Artist = mArtist, Album = mAlbum };
+			var updater = new Mock<IDataUpdateShell<StorageFile>>();
+
+			updater.Setup( m => m.Item ).Returns( mStorageFile );
+			testable.Mock<IStorageFileProvider>().Setup( m => m.GetFileForUpdate( mStorageFile.DbId )).Returns( updater.Object );
+			testable.Mock<IArtistProvider>().Setup( m => m.GetArtistForUpdate( mArtist.DbId )).Returns( new Mock<IDataUpdateShell<DbArtist>>().Object );
+
+			sut.ProcessStep( context );
+
+			mStorageFile.FileType.Should().Be( eFileType.Picture );
+		}
+
+		[Test]
+		public void UpdateShouldUpdateStorageFile() {
+			var testable = new TestableUpdateArtworkPipelineStep();
+			var sut = testable.ClassUnderTest;
+			var context = new PipelineContext( null, null, mStorageFile, mSummary ) { Artist = mArtist, Album = mAlbum };
+			var updater = new Mock<IDataUpdateShell<StorageFile>>();
+
+			updater.Setup( m => m.Item ).Returns( mStorageFile );
+			testable.Mock<IStorageFileProvider>().Setup( m => m.GetFileForUpdate( mStorageFile.DbId )).Returns( updater.Object );
+			testable.Mock<IArtistProvider>().Setup( m => m.GetArtistForUpdate( mArtist.DbId )).Returns( new Mock<IDataUpdateShell<DbArtist>>().Object );
+
+			sut.ProcessStep( context );
+
+			updater.Verify( m => m.Update(), Times.Once());
+		}
+	}
+}
