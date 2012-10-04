a29967a733e90dbb1f88774e10cdd8b6a146eb2e
Bill Swanson <bswanson58@gmail.com>
2012-10-04 09:58:44 -0500

    Added a TimeSpan non zero Visibility converter.

---
+++ Noise.UI/ValueConverters/TimeSpanVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Noise.UI.ValueConverters {
+	public class TimeSpanVisibilityConverter : IValueConverter {
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture ) {
+			var retValue = Visibility.Hidden;
+
+			if( value is TimeSpan ) {
+				var timeSpan = (TimeSpan)value;
+
+				if( Math.Abs( timeSpan.Ticks ) > 0 ) {
+					retValue = Visibility.Visible;
+				}
+			}
+			return( retValue );
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture ) {
+			return( null );
+		}
+	}
+}
