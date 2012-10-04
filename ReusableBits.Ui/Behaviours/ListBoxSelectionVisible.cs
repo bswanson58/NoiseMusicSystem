422031642685789895b6e6188e6849c990caa7be
Bill Swanson <bswanson58@gmail.com>
2012-10-04 09:30:57 -0500

    Added a behavior to insure that the selected item in a ListBox is visible.

---
+++ ReusableBits.Ui/Behaviours/ListBoxSelectionVisible.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace ReusableBits.Ui.Behaviours {
+	public class ListBoxSelectionVisible : Behavior<ListBox> {
+		/// <summary>
+		///  When Beahvior is attached
+		/// </summary>
+		protected override void OnAttached() {
+			base.OnAttached();
+			AssociatedObject.SelectionChanged += OnSelectionChanged;
+		}
+
+		/// <summary>
+		/// When behavior is detached
+		/// </summary>
+		protected override void OnDetaching() {
+			base.OnDetaching();
+			AssociatedObject.SelectionChanged -= OnSelectionChanged;
+		}
+
+		private void OnSelectionChanged( object sender, SelectionChangedEventArgs e ) {
+			if( sender is ListBox ) {
+				var listBox = ( sender as ListBox );
+
+				if( listBox.SelectedItem != null ) {
+					listBox.Dispatcher.BeginInvoke((Action)( () => {
+							listBox.UpdateLayout();
+
+							if( listBox.SelectedItem != null ) {
+								listBox.ScrollIntoView( listBox.SelectedItem );
+							}
+						} ));
+				}
+			}
+		}
+	}
+}
