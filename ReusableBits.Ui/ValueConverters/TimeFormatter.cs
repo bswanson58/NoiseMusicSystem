6eab823e73575ee5e68d7618602ba03ed5870a8a
Bill Swanson <bswanson58@gmail.com>
2012-10-04 20:49:42 -0500

    Converted the time display to lower case.

--- ReusableBits.Ui/ValueConverters/TimeFormatter.cs
+++ ReusableBits.Ui/ValueConverters/TimeFormatter.cs
@@ -3,7 +3,7 @@
 namespace ReusableBits.Ui.ValueConverters {
 	public class TimeFormatter  : BaseValueConverter<DateTime, string> {
 		protected override string Convert( DateTime value, System.Globalization.CultureInfo culture ) {
-			return( value.ToString( "t" ));
+			return( value.ToString( "t" ).ToLower());
 		}
 	}
 }
