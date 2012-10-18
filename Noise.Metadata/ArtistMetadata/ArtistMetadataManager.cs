1e5398d04b648fe225b9651ca69e44c85278fef3
Bill Swanson <bswanson58@gmail.com>
2012-10-18 14:50:59 -0500

    Beginning of artist metadata handling.

--- Noise.Metadata/ArtistMetadata/ArtistMetadataManager.cs
+++ Noise.Metadata/ArtistMetadata/ArtistMetadataManager.cs
@@ -1,27 +1,56 @@
-﻿using Caliburn.Micro;
+﻿using System.Linq;
+using Caliburn.Micro;
 using Noise.Metadata.Interfaces;
+using Raven.Client;
+using Raven.Client.Linq;
 
 namespace Noise.Metadata.ArtistMetadata {
 	public class ArtistMetadataManager : IArtistMetadataManager {
 		private readonly IEventAggregator	mEventAggregator;
+		private IDocumentStore				mDocumentStore;
 
 		public ArtistMetadataManager( IEventAggregator eventAggregator ) {
 			mEventAggregator = eventAggregator;
 		}
 
-		public void Initialize() {
+		public void Initialize( IDocumentStore documentStore ) {
+			mDocumentStore = documentStore;
 		}
 
 		public void Shutdown() {
+			mDocumentStore = null;
 		}
 
 		public void ArtistMentioned( string artistName ) {
+			GetOrCreateArtistMetadata( artistName );
 		}
 
 		public void ArtistForgotten( string artistName ) {
 		}
 
 		public void ArtistMetadataRequested( string artistName ) {
+			var metadata = GetOrCreateArtistMetadata( artistName );
+		}
+
+		private ArtistMetadataInfo GetOrCreateArtistMetadata( string forArtist ) {
+			ArtistMetadataInfo	retValue = null;
+
+			if( mDocumentStore != null ) {
+				using( var session = mDocumentStore.OpenSession()) {
+					retValue = ( from metadata in session.Query<ArtistMetadataInfo>()
+								 where metadata.ArtistName == forArtist 
+								 select metadata ).FirstOrDefault();
+
+					if( retValue == null ) {
+						retValue = new ArtistMetadataInfo { ArtistName = forArtist };
+
+						session.Store( retValue );
+						session.SaveChanges();
+					}
+				}
+			}
+
+			return( retValue );
 		}
 	}
 }
--- noise.metadata/Interfaces/IArtistMetadataManager.cs
+++ noise.metadata/Interfaces/IArtistMetadataManager.cs
@@ -1,6 +1,8 @@
-﻿namespace Noise.Metadata.Interfaces {
+﻿using Raven.Client;
+
+namespace Noise.Metadata.Interfaces {
 	public interface IArtistMetadataManager {
-		void		Initialize();
+		void		Initialize( IDocumentStore documentStore );
 		void		Shutdown();
 
 		void		ArtistMentioned( string artistName );
