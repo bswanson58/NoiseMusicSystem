39a45367fd0d0d267b1fb799b6b2cba08d9c66c5
Bill Swanson <bswanson58@gmail.com>
2012-11-15 07:35:33 -0600

    Add IMetadataManager interface.

---
+++ Noise.Infrastructure/Interfaces/IMetadataManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Noise.Infrastructure.Dto;
+
+namespace Noise.Infrastructure.Interfaces {
+	public enum eMetadataType {
+		ArtistArtwork,
+		BandMembers,
+		Biography,
+		Genre,
+		SimilarArtists,
+		TopAlbums,
+		WebSite,
+		Unknown
+	}
+
+	public interface IArtistMetadata {
+		string		ArtistName { get; }
+
+		IEnumerable<string> GetMetadataArray( eMetadataType metadataType );
+		string				GetMetadata( eMetadataType metadataType );
+	}
+
+	public interface IArtistDiscography {
+		string					ArtistName { get; }
+		List<DbDiscographyRelease>	Discography { get; } 
+	}
+
+	public interface IMetadataManager {
+		IArtistMetadata		GetArtistMetadata( string forArtist );
+		IArtistDiscography	GetArtistDiscography( string forArtist );
+		Artwork				GetArtistArtwork( string forArtist );
+	}
+}
