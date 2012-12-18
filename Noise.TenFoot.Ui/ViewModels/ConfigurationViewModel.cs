1966429d16e43c1d2439854d3da35e07314c99c0
Bill Swanson <bswanson58@gmail.com>
2012-12-18 13:16:56 -0600

    Clean up of the TenFooter configuration dialog.

--- Noise.TenFoot.Ui/ViewModels/ConfigurationViewModel.cs
+++ Noise.TenFoot.Ui/ViewModels/ConfigurationViewModel.cs
@@ -1,37 +1,10 @@
-﻿using System.Linq;
-using Caliburn.Micro;
-using Noise.Infrastructure;
+﻿using Caliburn.Micro;
 using Noise.Infrastructure.Configuration;
-using Noise.Infrastructure.Interfaces;
-using Noise.UI.Dto;
-using Noise.UI.Support;
 
 namespace Noise.TenFoot.Ui.ViewModels {
 	public class ConfigurationViewModel : Screen {
-		private readonly IEventAggregator		mEventAggregator;
-		private readonly IPlayQueue				mPlayQueue;
-		private readonly IDialogService			mDialogService;
-		private string							mDatabaseName;
-		private	string							mLibraryLocation;
-		private	bool							mAllowInternet;
-		private	bool							mEnableRemote;
-		private	readonly BindableCollection<ExhaustedStrategyItem>	mExhaustedStrategies;
-		private ExhaustedStrategyItem			mCurrentStrategy;
-
-		public ConfigurationViewModel( IEventAggregator eventAggregator,
-									   IPlayQueue playQueue, IDialogService dialogService ) {
-			mEventAggregator = eventAggregator;
-			mPlayQueue = playQueue;
-			mDialogService = dialogService;
-
-			mExhaustedStrategies = new BindableCollection<ExhaustedStrategyItem>{
-												new ExhaustedStrategyItem( ePlayExhaustedStrategy.Stop, "Stop" ),
-												new ExhaustedStrategyItem( ePlayExhaustedStrategy.Replay, "Replay" ),
-												new ExhaustedStrategyItem( ePlayExhaustedStrategy.PlayFavorites, "Play Favorites" ),
-												new ExhaustedStrategyItem( ePlayExhaustedStrategy.PlaySimilar, "Play Similar" )};
-			mCurrentStrategy = ( from strategy in mExhaustedStrategies 
-								 where strategy.Strategy == mPlayQueue.PlayExhaustedStrategy select strategy ).FirstOrDefault();
-		}
+		private	bool	mAllowInternet;
+		private	bool	mEnableRemote;
 
 		protected override void OnActivate() {
 			base.OnActivate();
@@ -57,24 +30,6 @@ namespace Noise.TenFoot.Ui.ViewModels {
 			}
 		}
 
-		public string DatabaseName {
-			get{ return( mDatabaseName ); }
-			set {
-				mDatabaseName = value;
-
-				NotifyOfPropertyChange( () => DatabaseName );
-			}
-		}
-
-		public string LibraryLocation {
-			get{ return( mLibraryLocation ); }
-			set {
-				mLibraryLocation = value;
-
-				NotifyOfPropertyChange( () => LibraryLocation );
-			}
-		}
-
 		public bool AllowInternet {
 			get{ return( mAllowInternet ); }
 			set {
@@ -90,35 +45,6 @@ namespace Noise.TenFoot.Ui.ViewModels {
 				mEnableRemote = value;
 
 				NotifyOfPropertyChange( () => EnableRemote );
-			}
-		}
-
-		public BindableCollection<ExhaustedStrategyItem> ExhaustedStrategyList {
-			get{ return( mExhaustedStrategies ); }
-		}
-
-		public ExhaustedStrategyItem ExhaustedStrategy {
-			get{ return( mCurrentStrategy ); }
-			set {
-				mCurrentStrategy = value;
-				mPlayQueue.SetPlayExhaustedStrategy( mCurrentStrategy.Strategy, Constants.cDatabaseNullOid );
-
-				var configuration = NoiseSystemConfiguration.Current.RetrieveConfiguration<ExplorerConfiguration>( ExplorerConfiguration.SectionName );
-
-				if( configuration != null ) {
-					configuration.PlayExhaustedStrategy = mCurrentStrategy.Strategy;
-					configuration.PlayExhaustedItem = Constants.cDatabaseNullOid;
-
-					NoiseSystemConfiguration.Current.Save( configuration );
-				}
-			}
-		}
-
-		public void BrowseFolder() {
-			string path = LibraryLocation;
-
-			if( mDialogService.SelectFolderDialog( "Select Music Library", ref path ).GetValueOrDefault( false )) {
-				LibraryLocation = path;
 			}
 		}
 
