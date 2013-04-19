657796fff86550e5c078fd3633c7f23a8ab2b92b
BSwanson <bswanson58@gmail.com>
2013-04-19 12:29:13 -0500

    Added a view/view model to support the status bar.

---
+++ noise.ui/ViewModels/StatusViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Noise.UI.ViewModels {
+	public class StatusViewModel {
+		public	string		Version { get; private set; }
+
+		public StatusViewModel() {
+			Version = "Desktop v0.9.0";
+		}
+	}
+}
