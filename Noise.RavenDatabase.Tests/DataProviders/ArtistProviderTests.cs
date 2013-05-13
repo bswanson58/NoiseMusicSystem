e84f564f2531705b6d64371b6ab15dd8c0788435
Bill Swanson <bswanson58@gmail.com>
2013-05-13 13:32:22 -0500

    Added basic RavenDB testing.

---
+++ Noise.RavenDatabase.Tests/DataProviders/ArtistProviderTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using NUnit.Framework;
+using Noise.BaseDatabase.Tests.DataProviders;
+using Noise.Infrastructure;
+using Noise.Infrastructure.Interfaces;
+using Noise.RavenDatabase.DataProviders;
+using Noise.RavenDatabase.Interfaces;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+
+namespace Noise.RavenDatabase.Tests.DataProviders {
+	[TestFixture]
+	public class ArtistProviderTests : BaseArtistProviderTests {
+		public	Mock<ILog>			DummyLog { get; private set; }
+		private IDocumentStore		mDatabase;
+		private Mock<IDbFactory>	mDatabaseFactory;
+
+		[TestFixtureSetUp]
+		public void FixtureSetup() {
+			DummyLog = new Mock<ILog>();
+			NoiseLogger.Current = DummyLog.Object;
+		}
+
+		[SetUp]
+		public void Setup() {
+			mDatabase = new EmbeddableDocumentStore { DataDirectory = "Test Database", RunInMemory = true };
+			// Wait for all operations to complete before each query.
+			mDatabase.Conventions.DefaultQueryingConsistency = ConsistencyOptions.QueryYourWrites;
+			mDatabase.Initialize();
+
+			mDatabaseFactory = new Mock<IDbFactory>();
+			mDatabaseFactory.Setup( o => o.GetLibraryDatabase()).Returns( mDatabase );
+		}
+
+		[TearDown]
+		public void Teardown() {
+			if( mDatabase != null ) {
+				mDatabase.Dispose();
+
+				mDatabaseFactory = null;
+			}
+		}
+
+		protected override IArtistProvider CreateSut() {
+			return( new ArtistProvider( mDatabaseFactory.Object ));
+		}
+	}
+}
