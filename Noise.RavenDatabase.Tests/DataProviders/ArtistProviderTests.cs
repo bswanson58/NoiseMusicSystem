4c9a1ca95eb2cfa64c37f71246395da23d9fec80
Bill Swanson <bswanson58@gmail.com>
2013-05-13 19:47:25 -0500

    Factored out the common test code.

--- Noise.RavenDatabase.Tests/DataProviders/ArtistProviderTests.cs
+++ Noise.RavenDatabase.Tests/DataProviders/ArtistProviderTests.cs
@@ -1,49 +1,31 @@
-﻿using Moq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Noise.BaseDatabase.Tests.DataProviders;
-using Noise.Infrastructure;
 using Noise.Infrastructure.Interfaces;
 using Noise.RavenDatabase.DataProviders;
-using Noise.RavenDatabase.Interfaces;
-using Raven.Client;
-using Raven.Client.Document;
-using Raven.Client.Embedded;
 
 namespace Noise.RavenDatabase.Tests.DataProviders {
 	[TestFixture]
 	public class ArtistProviderTests : BaseArtistProviderTests {
-		public	Mock<ILog>			DummyLog { get; private set; }
-		private IDocumentStore		mDatabase;
-		private Mock<IDbFactory>	mDatabaseFactory;
+		private CommonTestSetup	mCommon;
 
 		[TestFixtureSetUp]
 		public void FixtureSetup() {
-			DummyLog = new Mock<ILog>();
-			NoiseLogger.Current = DummyLog.Object;
+			mCommon = new CommonTestSetup();
+			mCommon.FixtureSetup();
 		}
 
 		[SetUp]
 		public void Setup() {
-			mDatabase = new EmbeddableDocumentStore { DataDirectory = "Test Database", RunInMemory = true };
-			// Wait for all operations to complete before each query.
-			mDatabase.Conventions.DefaultQueryingConsistency = ConsistencyOptions.QueryYourWrites;
-			mDatabase.Initialize();
-
-			mDatabaseFactory = new Mock<IDbFactory>();
-			mDatabaseFactory.Setup( o => o.GetLibraryDatabase()).Returns( mDatabase );
+			mCommon.Setup();
 		}
 
 		[TearDown]
 		public void Teardown() {
-			if( mDatabase != null ) {
-				mDatabase.Dispose();
-
-				mDatabaseFactory = null;
-			}
+			mCommon.Teardown();
 		}
 
 		protected override IArtistProvider CreateSut() {
-			return( new ArtistProvider( mDatabaseFactory.Object ));
+			return( new ArtistProvider( mCommon.DatabaseFactory.Object ));
 		}
 	}
 }
