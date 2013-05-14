063d8cea787f82d095db485e6b34fb916f0d8932
Bill Swanson <bswanson58@gmail.com>
2013-05-13 20:10:54 -0500

    Added the Raven TrackProvider and tests.

---
+++ Noise.RavenDatabase.Tests/DataProviders/TrackProviderTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Noise.BaseDatabase.Tests.DataProviders;
+using Noise.Infrastructure.Interfaces;
+using Noise.RavenDatabase.DataProviders;
+
+namespace Noise.RavenDatabase.Tests.DataProviders {
+	[TestFixture]
+	public class TrackProviderTests : BaseTrackProviderTests {
+		private CommonTestSetup	mCommon;
+
+		[TestFixtureSetUp]
+		public void FixtureSetup() {
+			mCommon = new CommonTestSetup();
+			mCommon.FixtureSetup();
+		}
+
+		[SetUp]
+		public void Setup() {
+			mCommon.Setup();
+		}
+
+		[TearDown]
+		public void Teardown() {
+			mCommon.Teardown();
+		}
+
+		protected override ITrackProvider CreateSut() {
+			return( new TrackProvider( mCommon.DatabaseFactory.Object ));
+		}
+	}
+}
---
+++ Noise.RavenDatabase/DataProviders/TrackProvider.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Noise.Infrastructure.Dto;
+using Noise.Infrastructure.Interfaces;
+using Noise.RavenDatabase.Interfaces;
+using Noise.RavenDatabase.Support;
+
+namespace Noise.RavenDatabase.DataProviders {
+	public class TrackProvider : ITrackProvider {
+		private readonly IDbFactory				mDbFactory;
+		private readonly IRepository<DbTrack>	mDatabase;
+
+		public TrackProvider( IDbFactory databaseFactory ) {
+			mDbFactory = databaseFactory;
+
+			mDatabase = new RavenRepositoryT<DbTrack>( mDbFactory.GetLibraryDatabase(), track => new object[] { track.DbId });
+		}
+
+		public void AddTrack( DbTrack track ) {
+			mDatabase.Add( track );
+		}
+
+		public DbTrack GetTrack( long trackId ) {
+			return( mDatabase.Get( trackId ));
+		}
+
+		public void DeleteTrack( DbTrack track ) {
+			mDatabase.Delete( track );
+		}
+
+		public IDataProviderList<DbTrack> GetTrackList( long albumId ) {
+			return( new RavenDataProviderList<DbTrack>( mDatabase.Find( track => track.Album == albumId )));
+		}
+
+		public IDataProviderList<DbTrack> GetTrackList( DbAlbum forAlbum ) {
+			return( GetTrackList( forAlbum.DbId ));
+		}
+
+		public IDataProviderList<DbTrack> GetTrackListForGenre( long genreId ) {
+			return( new RavenDataProviderList<DbTrack>( mDatabase.Find( track => track.UserGenre == genreId )));
+		}
+
+		public IDataProviderList<DbTrack> GetFavoriteTracks() {
+			return( new RavenDataProviderList<DbTrack>( mDatabase.Find( track => track.IsFavorite )));
+		}
+
+		public IDataProviderList<DbTrack> GetNewlyAddedTracks() {
+//			return( new RavenDataProviderList<DbTrack>( mDatabase.FindAll().Query().OrderBy( track => track.DateAddedTicks ));
+			return( null );
+		}
+
+		public IEnumerable<DbTrack> GetTrackListForPlayList( DbPlayList playList ) {
+			return( playList.TrackIds.Select( GetTrack ).ToList());
+		}
+
+		public IDataUpdateShell<DbTrack> GetTrackForUpdate( long trackId ) {
+			return( new RavenDataUpdateShell<DbTrack>( track => mDatabase.Update( track ), mDatabase.Get( trackId )));
+		}
+
+		public long GetItemCount() {
+			return( mDatabase.Count());
+		}
+	}
+}
