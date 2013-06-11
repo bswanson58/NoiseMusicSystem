c5d402134d0c6a0d63f5bec40fd5f9d6def088a1
BSwanson <bswanson58@gmail.com>
2013-06-11 11:47:29 -0500

    Created a new VU meter for the external player view.

---
+++ Noise.UI/Controls/HorizontalVuMeter.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Noise.UI.Controls {
+	/// <summary>
+	/// Interaction logic for HorizontalVuMeter.xaml
+	/// </summary>
+	public partial class HorizontalVuMeter {
+		public HorizontalVuMeter() {
+			InitializeComponent();
+		}
+
+		public static DependencyProperty LeftLevelProperty = DependencyProperty.Register( "LeftLevel", typeof( double ), typeof( HorizontalVuMeter ),
+																							new PropertyMetadata( 0.0, OnLevelChanged ) );
+		public double LeftLevel {
+			get { return (double)GetValue( LeftLevelProperty ); }
+			set { SetValue( LeftLevelProperty, value ); }
+		}
+
+		public static DependencyProperty RightLevelProperty = DependencyProperty.Register( "RightLevel", typeof( double ), typeof( HorizontalVuMeter ),
+																							new PropertyMetadata( 0.0, OnLevelChanged ) );
+		public double RightLevel {
+			get { return (double)GetValue( RightLevelProperty ); }
+			set { SetValue( RightLevelProperty, value ); }
+		}
+
+		private static void OnLevelChanged( DependencyObject sender, DependencyPropertyChangedEventArgs args ) {
+			if( sender is HorizontalVuMeter ) {
+				var meter = sender as HorizontalVuMeter;
+
+				meter.CombinedLevel = ( meter.LeftLevel + meter.RightLevel ) / 2.0;
+			}
+		}
+
+		public static DependencyProperty CombinedLevelProperty = DependencyProperty.Register( "CombinedLevel", typeof( double ), typeof( HorizontalVuMeter ), new PropertyMetadata( 0.0 ));
+		public double CombinedLevel {
+			get { return (double)GetValue( CombinedLevelProperty ); }
+			set { SetValue( CombinedLevelProperty, value ); }
+		}
+
+		public static DependencyProperty PeakThresholdProperty = DependencyProperty.Register( "PeakThreshold", typeof( double ), typeof( HorizontalVuMeter ), new PropertyMetadata( 0.9 ));
+		public double PeakThreshold {
+			get { return (double)GetValue( PeakThresholdProperty ); }
+			set { SetValue( PeakThresholdProperty, value ); }
+		}
+
+		public static DependencyProperty PeakColorProperty = DependencyProperty.Register( "PeakColor", typeof( Brush ), typeof( HorizontalVuMeter ), new PropertyMetadata( Brushes.Red ));
+		public Brush PeakColor {
+			get { return (Brush)GetValue( PeakColorProperty ); }
+			set { SetValue( PeakColorProperty, value ); }
+		}
+	}
+}
+
--- Noise.UI/Controls/VuMeterBar.cs
+++ Noise.UI/Controls/VuMeterBar.cs
@@ -7,83 +7,83 @@ using Condition = CuttingEdge.Conditions.Condition;
 
 namespace Noise.UI.Controls {
 	public class VuMeterBar : RangeBase {
-        static VuMeterBar() {
-            MinHeightProperty.OverrideMetadata( typeof( VuMeterBar ), new FrameworkPropertyMetadata((double)10 ));
-            MinWidthProperty.OverrideMetadata( typeof( VuMeterBar ), new FrameworkPropertyMetadata((double)10 ));
-            ClipToBoundsProperty.OverrideMetadata( typeof( VuMeterBar ), new FrameworkPropertyMetadata( true ));
+		static VuMeterBar() {
+			MinHeightProperty.OverrideMetadata( typeof( VuMeterBar ), new FrameworkPropertyMetadata((double)1 ));
+			MinWidthProperty.OverrideMetadata( typeof( VuMeterBar ), new FrameworkPropertyMetadata((double)10 ));
+			ClipToBoundsProperty.OverrideMetadata( typeof( VuMeterBar ), new FrameworkPropertyMetadata( true ));
 			ValueProperty.OverrideMetadata( typeof( VuMeterBar ), new FrameworkPropertyMetadata( 0.0, FrameworkPropertyMetadataOptions.AffectsRender, null ));
-        }
+		}
 
-        public static readonly DependencyProperty BlockMarginProperty =
-            DependencyProperty.Register( "BlockMargin", typeof( double ), typeof( VuMeterBar ),
-            new FrameworkPropertyMetadata( 1.0, FrameworkPropertyMetadataOptions.AffectsRender, null, CoerceBlockMargin ));
+		public static readonly DependencyProperty BlockMarginProperty =
+			DependencyProperty.Register( "BlockMargin", typeof( double ), typeof( VuMeterBar ),
+			new FrameworkPropertyMetadata( 1.0, FrameworkPropertyMetadataOptions.AffectsRender, null, CoerceBlockMargin ));
 
-        public double BlockMargin {
-            get { return (double)GetValue(BlockMarginProperty); }
-            set { SetValue(BlockMarginProperty, value); }
-        }
+		public double BlockMargin {
+			get { return (double)GetValue(BlockMarginProperty); }
+			set { SetValue(BlockMarginProperty, value); }
+		}
 
-        public static readonly DependencyProperty BlockSizeProperty =
-            DependencyProperty.Register( "BlockSize", typeof( double ), typeof( VuMeterBar ),
-            new FrameworkPropertyMetadata( 3.0, FrameworkPropertyMetadataOptions.AffectsRender, null, CoerceBlockSize ));
+		public static readonly DependencyProperty BlockSizeProperty =
+			DependencyProperty.Register( "BlockSize", typeof( double ), typeof( VuMeterBar ),
+			new FrameworkPropertyMetadata( 3.0, FrameworkPropertyMetadataOptions.AffectsRender, null, CoerceBlockSize ));
 
-        public double BlockSize {
-            get { return (double)GetValue( BlockSizeProperty ); }
-            set { SetValue( BlockSizeProperty, value ); }
-        }
+		public double BlockSize {
+			get { return (double)GetValue( BlockSizeProperty ); }
+			set { SetValue( BlockSizeProperty, value ); }
+		}
 
-        public static readonly DependencyProperty PeakThresholdProperty =
-            DependencyProperty.Register( "PeakThreshold", typeof( double ), typeof( VuMeterBar ),
-            new FrameworkPropertyMetadata( 1.0, FrameworkPropertyMetadataOptions.AffectsRender, null, CoerceThreshold ));
+		public static readonly DependencyProperty PeakThresholdProperty =
+			DependencyProperty.Register( "PeakThreshold", typeof( double ), typeof( VuMeterBar ),
+			new FrameworkPropertyMetadata( 1.0, FrameworkPropertyMetadataOptions.AffectsRender, null, CoerceThreshold ));
 
-        public double PeakThreshold {
-            get { return (double)GetValue( PeakThresholdProperty ); }
-            set { SetValue( PeakThresholdProperty, value ); }
-        }
+		public double PeakThreshold {
+			get { return (double)GetValue( PeakThresholdProperty ); }
+			set { SetValue( PeakThresholdProperty, value ); }
+		}
 
-        public static readonly DependencyProperty PeakColorProperty =
-            DependencyProperty.Register( "PeakColor", typeof( Brush ), typeof( VuMeterBar ),
-            new FrameworkPropertyMetadata( Brushes.Red, FrameworkPropertyMetadataOptions.AffectsRender, null ));
+		public static readonly DependencyProperty PeakColorProperty =
+			DependencyProperty.Register( "PeakColor", typeof( Brush ), typeof( VuMeterBar ),
+			new FrameworkPropertyMetadata( Brushes.Red, FrameworkPropertyMetadataOptions.AffectsRender, null ));
 
-        public Brush PeakColor {
-            get { return (Brush)GetValue( PeakColorProperty ); }
-            set { SetValue( PeakColorProperty, value ); }
-        }
+		public Brush PeakColor {
+			get { return (Brush)GetValue( PeakColorProperty ); }
+			set { SetValue( PeakColorProperty, value ); }
+		}
 
-        private static int GetThreshold( double value, int blockCount ) {
+		private static int GetThreshold( double value, int blockCount ) {
 			Condition.Requires( blockCount ).IsGreaterThan( 0 );
 			Condition.Requires( value ).IsGreaterOrEqual( 0.0 );
 			Condition.Requires( value ).IsLessOrEqual( 1.0 );
 
-            int blockNumber = Math.Min((int)(value * (blockCount + 1)), blockCount);
+			int blockNumber = Math.Min((int)(value * (blockCount + 1)), blockCount);
 
 			Condition.Ensures( blockNumber ).IsLessOrEqual( blockCount );
 			Condition.Ensures( blockNumber ).IsGreaterOrEqual( 0 );
 
-            return( blockNumber );
-        }
+			return( blockNumber );
+		}
 
-        private static object CoerceBlockSize( DependencyObject element, object value ) {
-            var input = (double)value;
+		private static object CoerceBlockSize( DependencyObject element, object value ) {
+			var input = (double)value;
 
-            if( input < 1.0 ) {
-                return 1.0;
-            }
+			if( input < 1.0 ) {
+				return 1.0;
+			}
 
-            return input;
-        }
+			return input;
+		}
 
-        private static object CoerceBlockMargin( DependencyObject element, object value ) {
-            var input = (double)value;
+		private static object CoerceBlockMargin( DependencyObject element, object value ) {
+			var input = (double)value;
 
-            if(( input < 0 ) || 
+			if(( input < 0 ) || 
 			   ( double.IsNaN( input )) ||
 			   ( double.IsInfinity( input ))) {
-                return( 0.0 );
-            }
+				return( 0.0 );
+			}
 
 			return( input );
-        }
+		}
 
 		private static object CoerceThreshold( DependencyObject elment, object value ) {
 			var input = (double)value;
@@ -96,23 +96,23 @@ namespace Noise.UI.Controls {
 			return( input );
 		}
 
-        protected override void OnRender( DrawingContext drawingContext ) {
-            var	blockCount = (int)( RenderSize.Width / ( BlockSize + BlockMargin ));
+		protected override void OnRender( DrawingContext drawingContext ) {
+			var	blockCount = (int)( RenderSize.Width / ( BlockSize + BlockMargin ));
 
-            for (int i = 0; i < blockCount; i++) {
-                var	rect = GetRect( RenderSize, blockCount, BlockMargin, i );
+			for (int i = 0; i < blockCount; i++) {
+				var	rect = GetRect( RenderSize, blockCount, BlockMargin, i );
 
-                if (!rect.IsEmpty) {
-                    var	threshold = GetThreshold( Value, blockCount );
+				if (!rect.IsEmpty) {
+					var	threshold = GetThreshold( Value, blockCount );
 					var peak = GetThreshold( PeakThreshold, blockCount );
 					var color = i < threshold ? ( i < peak ? Foreground : PeakColor ) : Background;
 
-                    drawingContext.DrawRectangle( color, null, rect );
-                }
-            }
-        }
+					drawingContext.DrawRectangle( color, null, rect );
+				}
+			}
+		}
 
-        private static Rect GetRect( Size targetSize, int blockCount, double blockMargin, int blockNumber ) {
+		private static Rect GetRect( Size targetSize, int blockCount, double blockMargin, int blockNumber ) {
 			Condition.Requires( targetSize.Height ).IsGreaterThan( 0 );
 			Condition.Requires( targetSize.Width ).IsGreaterThan( 0 );
 			Condition.Requires( blockCount ).IsGreaterThan( 0 );
