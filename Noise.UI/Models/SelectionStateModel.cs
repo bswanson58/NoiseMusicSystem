7ec9e2caa3fe4e73ab17e668e1016cef7b04fed4
Bill Swanson <bswanson58@gmail.com>
2013-07-15 11:16:50 -0500

    Moved the Reactive extension to NuGet support.

--- Noise.UI/Models/SelectionStateModel.cs
+++ Noise.UI/Models/SelectionStateModel.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using Caliburn.Micro;
 using Noise.Infrastructure;
 using Noise.Infrastructure.Configuration;
