d82b9abf450e7d71ff5bde0d863ad56735f42c5e
Bill Swanson <bswanson58@gmail.com>
2014-02-22 15:42:02 -0600

    Added a service class and initialized the Noise Core on startup.

---
+++ Noise.Headless/HeadlessService.cs
@@ -0,0 +1,38 @@
+﻿using System.ServiceProcess;
+using Noise.Infrastructure;
+using Noise.Infrastructure.Configuration;
+using Noise.Infrastructure.Interfaces;
+using ReusableBits.Service;
+
+namespace Noise.Headless {
+	[WindowsService("Noise Library Headless Service",
+		DisplayName = "Noise Library Headless Service",
+		Description = "This service operates the Noise Music System library for remote clients.",
+		EventLogSource = "Noise Headless Service",
+		StartMode = ServiceStartMode.Automatic)]
+	public class HeadlessService : BaseService {
+		private readonly INoiseManager		mNoiseManager;
+
+		public HeadlessService( INoiseManager noiseManager ) {
+			mNoiseManager = noiseManager;
+
+			var sysConfig = NoiseSystemConfiguration.Current.RetrieveConfiguration<ExplorerConfiguration>( ExplorerConfiguration.SectionName );
+			if((sysConfig != null) &&
+			   (sysConfig.EnableRemoteAccess == false )) {
+				sysConfig.EnableRemoteAccess = true;
+
+				NoiseSystemConfiguration.Current.Save( sysConfig );
+			}
+		}
+
+		public override void OnStart( string[] args ) {
+			if(!mNoiseManager.Initialize()) {
+				NoiseLogger.Current.LogMessage( "Noise Headless Service could not start.");
+			}
+		}
+
+		public override void OnShutdown() {
+			mNoiseManager.Shutdown();
+		}
+	}
+}
--- Noise.Headless/ServiceMain.cs
+++ Noise.Headless/ServiceMain.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
+using System.Threading;
 using Microsoft.Practices.Unity;
 using Noise.AppSupport;
 using ReusableBits.Service;
 
 namespace Noise.Headless {
+	internal sealed class SingleThreadSynchronizationContext : SynchronizationContext {
+		private readonly BlockingCollection<KeyValuePair<SendOrPostCallback, object>>
+			mQueue = new BlockingCollection<KeyValuePair<SendOrPostCallback, object>>();
+
+		public override void Post( SendOrPostCallback d, object state ) {
+			mQueue.Add( new KeyValuePair<SendOrPostCallback, object>( d, state ));
+		}
+
+		public void RunOnCurrentThread() {
+			KeyValuePair<SendOrPostCallback, object> workItem;
+			while( mQueue.TryTake( out workItem, Timeout.Infinite )) {
+				workItem.Key( workItem.Value );
+			}
+		}
+
+		public void Complete() {
+			mQueue.CompleteAdding();
+		}
+	}
+
 	static class ServiceMain {
 		/// <summary>
 		/// The main entry point for the application.
@@ -14,7 +37,11 @@ namespace Noise.Headless {
 			var iocConfig = new IocConfiguration();
 
 			iocConfig.InitializeIoc( ApplicationUsage.Server );
+			iocConfig.Container.RegisterType<IWindowsService, HeadlessService>();
 
+			var syncCtx = new SingleThreadSynchronizationContext();
+			SynchronizationContext.SetSynchronizationContext( syncCtx );
+	
 			using( var serviceImpl = iocConfig.Container.Resolve<IWindowsService>()) {
 				// if install was a command line flag, then run the installer at runtime.
 				if( args.Contains( "-install", StringComparer.InvariantCultureIgnoreCase )) {
