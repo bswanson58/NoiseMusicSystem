33a38d4d851d610badd021c926224ced273df1d5
Bill Swanson <bswanson58@gmail.com>
2014-04-23 14:49:47 -0500

    Added a DbConfiguration derived class to allow code configuration of the EF database implementation.

---
+++ Noise.EntityFrameworkDatabase/DatabaseManager/NoiseDbConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace Noise.EntityFrameworkDatabase.DatabaseManager {
+	public class NoiseDbConfiguration : DbConfiguration {
+		public NoiseDbConfiguration() {
+			SetProviderServices( "System.Data.SqlClient", System.Data.Entity.SqlServer.SqlProviderServices.Instance );
+		}
+	}
+}
