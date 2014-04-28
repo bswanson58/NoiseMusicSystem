03fcda0216b46a84df3519bba0ea6b52e5f47f29
Bill Swanson <bswanson58@gmail.com>
2014-04-28 14:44:42 -0500

    Insured that the database directory exists during testing.

--- Noise.EntityFrameworkDatabase.Tests/NoiseContextTests.cs
+++ Noise.EntityFrameworkDatabase.Tests/NoiseContextTests.cs
@@ -1,15 +1,24 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using NUnit.Framework;
 using Noise.EntityFrameworkDatabase.DatabaseManager;
 using Noise.Infrastructure.Dto;
 
 namespace Noise.EntityFrameworkDatabase.Tests {
 	[TestFixture]
 	public class NoiseContextTests {
-		private const string	cDatabaseName = @"D:\Noise Testing\Noise Context Test Database";
+		private const string	cDatabasePath = @"D:\Noise Testing";
+		private const string	cDatabaseName = "Noise Context Test Database";
+
+		[SetUp]
+		public void Setup() {
+			if( !Directory.Exists( cDatabasePath )) {
+				Directory.CreateDirectory( cDatabasePath );
+			}
+		}
 
 		[Test]
 		public void CanCreateDatabase() {
-			var context = new NoiseContext( cDatabaseName );
+			var context = new NoiseContext( Path.Combine( cDatabasePath, cDatabaseName ));
 
 			Assert.IsNotNull( context );
 		}
@@ -18,7 +27,7 @@ namespace Noise.EntityFrameworkDatabase.Tests {
 		public void CanAddArtist() {
 			var artist = new DbArtist { Name = "The Rolling Stones" };
 
-			using( var context = new NoiseContext( cDatabaseName )) {
+			using( var context = new NoiseContext( Path.Combine( cDatabasePath, cDatabaseName ))) {
 				context.Artists.Add( artist );
 
 				context.SaveChanges();
