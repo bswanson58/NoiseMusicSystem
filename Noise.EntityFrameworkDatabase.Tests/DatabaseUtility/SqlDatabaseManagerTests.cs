5233c1a538ef0b865a086d4a7510360a869f2594
Bill Swanson <bswanson58@gmail.com>
2014-04-30 15:00:28 -0500

    Added the beginning of a database utility for SQL Server.

---
+++ Noise.EntityFrameworkDatabase.Tests/DatabaseUtility/SqlDatabaseManagerTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Noise.EntityFrameworkDatabase.DatabaseUtility;
+using NUnit.Framework;
+
+namespace Noise.EntityFrameworkDatabase.Tests.DatabaseUtility {
+	[TestFixture]
+	public class SqlDatabaseManagerTests {
+
+		private SqlDatabaseManager CreateSut() {
+			return( new SqlDatabaseManager());
+		}
+
+		[Test]
+		public void CanIterateDatabases() {
+			var sut = CreateSut();
+
+			var databases = sut.GetDatabaseList();
+			databases.Should().NotBeEmpty( "There should be a list of databases" );
+		}
+
+		[Test]
+		public void CanDetachDatabase() {
+			var sut = CreateSut();
+
+			sut.DetachDatabase( "INTEGRATION_TEST_DATABASE_01f85a2ae3a64187881f70cf71e824b2" );
+		}
+	}
+}
---
+++ Noise.EntityFrameworkDatabase/DatabaseUtility/SqlDatabaseManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Noise.EntityFrameworkDatabase.DatabaseUtility {
+	public class SqlDatabaseManager {
+
+		public IEnumerable<string> GetDatabaseList() {
+			var retValue = new List<string>();
+
+			using( var connection = CreateConnection()) {
+				connection.Open();
+
+				using( var command = new SqlCommand( "SELECT [name] FROM sys.databases", connection )) {
+					using( var reader = command.ExecuteReader()) {
+						while( reader.Read() ) {
+							retValue.Add( reader.GetString( 0 ));
+						}
+					}
+				}
+			}
+
+			return( retValue );
+		}
+
+		public void AttachDatabase( string databaseName, string databaseFile ) {
+			using( var connection = CreateConnection()) {
+				string	commandText = string.Format( "exec sys.sp_attach_db {0} '{1}'", databaseName, databaseFile );
+
+				connection.Open();
+
+				using( var command = new SqlCommand( commandText, connection )) {
+					command.ExecuteNonQuery();
+				}
+			}
+		}
+
+		public void DetachDatabase( string databaseName ) {
+			using( var connection = CreateConnection()) {
+				string	commandText = string.Format( "exec sys.sp_detach_db {0}", databaseName );
+
+				connection.Open();
+
+				using( var command = new SqlCommand( commandText, connection )) {
+					command.ExecuteNonQuery();
+				}
+			}
+		}
+
+		public void BackupDatabase( string databaseName, string backupLocation ) {
+			using( var connection = CreateConnection()) {
+				string	commandText = string.Format( "BACKUP DATABASE {0} TO DISK='{1}' WITH FORMAT, MEDIANAME='NoiseBackup', MEDIADESCRIPTION='Media set for {0} database';",
+													databaseName, backupLocation );
+
+				connection.Open();
+
+				using( var command = new SqlCommand( commandText, connection )) {
+					command.ExecuteNonQuery();
+				}
+			}
+		}
+
+		public void RestoreDatabase( string databaseName, string restoreLocation ) {
+			using( var connection = CreateConnection()) {
+				string	commandText = string.Format( "RESTORE DATABASE {0} FROM DISK='{1}' WITH REPLACE, RECOVERY;",
+													databaseName, restoreLocation );
+
+				connection.Open();
+
+				using( var command = new SqlCommand( commandText, connection )) {
+					command.ExecuteNonQuery();
+				}
+			}
+		}
+
+		private SqlConnection CreateConnection() {
+			return( new SqlConnection( @"Server=(localdb)\v11.0;Integrated Security=true;" ));
+		}
+	}
+}
