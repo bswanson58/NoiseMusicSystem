cae8e58f40257fdd8a0edb27215c8bc1cdb431f4
Bill Swanson <bswanson58@gmail.com>
2014-05-07 12:25:09 -0500

    Updated Prism to version 5.0

--- ReusableBits.TestSupport/MockingEventAggregator/AutoMockingEventAggregator.cs
+++ ReusableBits.TestSupport/MockingEventAggregator/AutoMockingEventAggregator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Windows.Threading;
 using Microsoft.Practices.Prism.Events;
+using Microsoft.Practices.Prism.PubSubEvents;
 using Rhino.Mocks;
 
 namespace ReusableBits.TestSupport.MockingEventAggregator {
