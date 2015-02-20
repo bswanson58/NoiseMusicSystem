9f9b2315c6498fec20180b631b04d4ce5f5c81ec
Bill Swanson <bswanson58@gmail.com>
2015-02-20 14:40:22 -0600

    Updated the StorageFile associated with a sidecar file during file processing.

--- Noise.Core/FileProcessor/UpdateSidecarPipelineStep.cs
+++ Noise.Core/FileProcessor/UpdateSidecarPipelineStep.cs
@@ -4,11 +4,13 @@ using Noise.Infrastructure.Interfaces;
 
 namespace Noise.Core.FileProcessor {
 	internal class UpdateSidecarPipelineStep : BasePipelineStep {
-		private readonly ISidecarProvider	mSidecarProvider;
+		private readonly ISidecarProvider		mSidecarProvider;
+		private readonly IStorageFileProvider	mStorageFileProvider;
 
-		public UpdateSidecarPipelineStep( ISidecarProvider sidecarProvider ) :
+		public UpdateSidecarPipelineStep( ISidecarProvider sidecarProvider, IStorageFileProvider storageFileProvider ) :
 			base( ePipelineStep.UpdateSidecar ) {
 			mSidecarProvider = sidecarProvider;
+			mStorageFileProvider = storageFileProvider;
 		}
 
 		public override void ProcessStep( PipelineContext context ) {
@@ -16,8 +18,19 @@ namespace Noise.Core.FileProcessor {
 			Condition.Requires( context.StorageFile ).IsNotNull();
 
 			if( context.Artist != null ) {
-				mSidecarProvider.Add( context.Album != null ? new StorageSidecar( context.StorageFile.Name, context.Album )
-															: new StorageSidecar( context.StorageFile.Name, context.Artist ) );
+				var sidecar = context.Album != null ? new StorageSidecar( context.StorageFile.Name, context.Album )
+													: new StorageSidecar( context.StorageFile.Name, context.Artist );
+				mSidecarProvider.Add( sidecar );
+
+				using( var updater = mStorageFileProvider.GetFileForUpdate( context.StorageFile.DbId )) {
+					if( updater.Item != null ) {
+						updater.Item.MetaDataPointer = context.Album != null ? context.Album.DbId : context.Artist.DbId;
+						updater.Item.FileType = eFileType.Sidecar;
+						updater.Item.WasUpdated = false;
+
+						updater.Update();
+					}
+				}
 			}
 		}
 	}
