97bbf6b89fa2ba6fe684ca874b177cb1418e8dd4
Bill Swanson <bswanson58@gmail.com>
2015-11-23 09:18:03 -0600

    Moved the generated RefitStubs template file to the command area and copy it into each project during building.

---
+++ Common/RefitStubs.cs
@@ -0,0 +1,6 @@
+
+/* ******** Hey You! *********
+ *
+ * This is a generated file, and gets rewritten every time you build the
+ * project. If you want to edit it, you need to edit the mustache template
+ * in the Refit package */
