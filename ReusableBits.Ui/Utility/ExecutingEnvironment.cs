2706c74abd43f2798e9550093ee198bece6a611b
Bill Swanson <bswanson58@gmail.com>
2016-04-22 11:23:41 -0500

    Added support for high DPI monitors when insuring that windows are visible on a screen.

--- ReusableBits.Ui/Utility/ExecutingEnvironment.cs
+++ ReusableBits.Ui/Utility/ExecutingEnvironment.cs
@@ -1,38 +1,101 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using Point = System.Drawing.Point;
 
 namespace ReusableBits.Ui.Utility {
-	public class ExecutingEnvironment {
-		public static void ResizeWindowIntoVisibility( Window window ) {
-			var workArea = SystemParameters.WorkArea;
+    public class ExecutingEnvironment {
+        public static void ResizeWindowIntoVisibility( Window window ) {
+            var     graphics = Graphics.FromHwnd( new WindowInteropHelper( window ).Handle);
+            float   scaleX;
+            float   scaleY;
 
-			var screenLeft = SystemParameters.VirtualScreenLeft;
-			var screenRight = SystemParameters.VirtualScreenLeft + SystemParameters.VirtualScreenWidth;
-			var screenTop = Math.Max( SystemParameters.VirtualScreenTop, workArea.Top );
-			var screenBottom = Math.Min( workArea.Bottom, SystemParameters.VirtualScreenTop + SystemParameters.VirtualScreenHeight );
+            try {
+                scaleX = graphics.DpiX / 96.0f;
+                scaleY = graphics.DpiY / 96.0f;
+            }
+            finally {
+                graphics.Dispose();
+            }
 
-			window.Left = Math.Max( window.Left, screenLeft );
-			window.Left = Math.Min( window.Left, screenRight - 800 );
-			window.Top = Math.Max( window.Top, screenTop );
-			window.Top = Math.Min( window.Top, screenBottom - 600 );
+            var rect = new Rectangle((int)( window.Left * scaleX ), (int)( window.Top * scaleY ),
+                                     (int)( window.Width * scaleX ), (int)( window.Height * scaleY ));
 
-			window.Width = Math.Min( window.Width, screenRight - window.Left );
-			window.Height = Math.Min( window.Height, screenBottom - window.Top );
-		}
+            if(!IsOnScreen( rect, 1.0 )) {
+                var screen = Screen.FromRectangle( rect );
 
-		public static void MoveWindowIntoVisibility( Window window ) {
-			var workArea = SystemParameters.WorkArea;
+                if( rect.Right > screen.WorkingArea.Right ) {
+                    rect.Width = Math.Min( rect.Width, screen.WorkingArea.Width );
+                    rect.Location = new Point( screen.WorkingArea.Right - rect.Width ,rect.Top );
+                }
+                if( rect.Left < screen.WorkingArea.Left ) {
+                    rect.Width = Math.Min( rect.Width, screen.WorkingArea.Width );
+                    rect.Location = new Point( screen.WorkingArea.Left, rect.Top );
+                }
 
-			var screenLeft = SystemParameters.VirtualScreenLeft;
-			var screenRight = SystemParameters.VirtualScreenLeft + SystemParameters.VirtualScreenWidth;
-			var screenTop = Math.Max( SystemParameters.VirtualScreenTop, workArea.Top );
-			var screenBottom = Math.Min( workArea.Bottom, SystemParameters.VirtualScreenTop + SystemParameters.VirtualScreenHeight );
+                if( rect.Bottom > screen.WorkingArea.Bottom ) {
+                    rect.Height = Math.Min( rect.Height, screen.WorkingArea.Height );
+                    rect.Location = new Point( rect.Left, screen.WorkingArea.Bottom - rect.Height );
+                }
+                if( rect.Top < screen.WorkingArea.Top ) {
+                    rect.Height = Math.Min( rect.Height, screen.WorkingArea.Height );
+                    rect.Location = new Point( rect.Left, screen.WorkingArea.Top );
+                }
 
-			window.Left = Math.Max( window.Left, screenLeft );
-			window.Top = Math.Max( window.Top, screenTop );
+                window.Top = rect.Top / scaleY;
+                window.Height = rect.Height / scaleY;
+                window.Left = rect.Left / scaleX;
+                window.Width = rect.Width / scaleX;
+            }
+        }
 
-			window.Left = Math.Min( window.Left, screenRight - window.Width );
-			window.Top = Math.Min( window.Top, screenBottom - window.Height );
-		}
-	}
+        public static void MoveWindowIntoVisibility( Window window ) {
+            var     graphics = Graphics.FromHwnd( new WindowInteropHelper( window ).Handle);
+            float   scaleX;
+            float   scaleY;
+
+            try {
+                scaleX = graphics.DpiX / 96.0f;
+                scaleY = graphics.DpiY / 96.0f;
+            }
+            finally {
+                graphics.Dispose();
+            }
+
+            var rect = new Rectangle((int)( window.Left * scaleX ), (int)( window.Top * scaleY ),
+                                     (int)( window.Width * scaleX ), (int)( window.Height * scaleY ));
+
+            if(!IsOnScreen( rect, 0.8 )) {
+                var screen = Screen.FromRectangle( rect );
+
+                var left = Math.Max( rect.Left, screen.WorkingArea.Left );
+                var top = Math.Max( rect.Top, screen.WorkingArea.Top );
+
+                left = Math.Min( left, ( screen.WorkingArea.Left + screen.WorkingArea.Width ) - rect.Width );
+                top = Math.Min( top, ( screen.WorkingArea.Top + screen.WorkingArea.Height ) - rect.Height );
+
+                window.Left = left / scaleX;
+                window.Top = top / scaleY;
+            }
+        }
+
+        // Return True if at least the percent specified of the rectangle is shown on the total screen area of all monitors, otherwise return False.
+        private static bool IsOnScreen( Rectangle rect, double minimumPercentOnScreen ) {
+            double pixelsVisible = 0;
+
+            foreach( var screen in Screen.AllScreens ) {
+                var intersect = Rectangle.Intersect( rect, screen.WorkingArea );
+
+                if(( intersect.Width != 0 ) &&
+                   ( intersect.Height != 0 )) {
+                    pixelsVisible += ( intersect.Width * intersect.Height );
+                }
+            }
+
+            return pixelsVisible >= (( rect.Width * rect.Height ) * minimumPercentOnScreen );
+        }
+    }
 }
+
