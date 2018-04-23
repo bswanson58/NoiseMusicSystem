6f7eb1f2666c8378a08be8d834e8143194fe4a48
Bill Swanson <bswanson58@gmail.com>
2018-04-23 13:38:02 -0500

    Added a Color to SolidColorBrush converter.

---
+++ ReusableBits.Ui/ValueConverters/ColorToBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ReusableBits.Ui.ValueConverters {
+    public class ColorToBrushConverter : IValueConverter {
+        public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture ) {
+            if( null == value ) {
+                return null;
+            }
+
+            if( value is Color ) {
+                var color = (Color)value;
+
+                return new SolidColorBrush( color );
+            }
+
+            Type type = value.GetType();
+            throw new InvalidOperationException( "Unsupported type [" + type.Name + "]" );            
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture ) {
+            throw new NotImplementedException();
+        }
+    }
+}
