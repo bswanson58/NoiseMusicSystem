dfe085eabcf20819c2752cd32d1b9d1e43c4df0d
Bill Swanson <bswanson58@gmail.com>
2018-05-04 09:51:02 -0500

    Adjustment and styling for dialog boxes.

--- Noise.UI/Support/DialogWindow.xaml.cs
+++ Noise.UI/Support/DialogWindow.xaml.cs
@@ -1,10 +1,8 @@
-﻿using System.Windows;
-
-namespace Noise.UI.Support {
+﻿namespace Noise.UI.Support {
 	/// <summary>
 	/// Interaction logic for DialogWindow.xaml
 	/// </summary>
-	public partial class DialogWindow : Window {
+	public partial class DialogWindow {
 		public DialogWindow() {
 			InitializeComponent();
 		}
