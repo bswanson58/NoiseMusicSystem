75b8e56bdfdc4cb25bb66ef431ad6c7f1d60a3f4
Bill Swanson <bswanson58@gmail.com>
2019-08-15 13:27:10 -0500

    Added an exhausted strategy to replay the queue.

---
+++ Noise.Core/PlayStrategies/Exhausted/Suggesters/ReplayQueue.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.ObjectBuilder2;
+using Noise.Infrastructure.Dto;
+using Noise.Infrastructure.Interfaces;
+
+namespace Noise.Core.PlayStrategies.Exhausted.Suggesters {
+    class ReplayQueue : ExhaustedHandlerBase {
+        public ReplayQueue() : base( eTrackPlayHandlers.Replay, eTrackPlayStrategy.Suggester, "Replay Queue", "Replay the tracks in the play queue." ) { }
+
+        public override void SelectTrack( IExhaustedSelectionContext context ) {
+            if( context.PlayQueue.UnplayedTrackCount == 0 ) {
+                context.PlayQueue.PlayList.ForEach( 
+                    track => {
+                        if(!track.IsPlaying) {
+                            track.HasPlayed = false;
+                        }
+                    });
+            }
+        }
+    }
+}
