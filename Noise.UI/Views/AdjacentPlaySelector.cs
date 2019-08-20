2d27ba4ccafcf4f1f221cbfff60b81d73b3b4093
Bill Swanson <bswanson58@gmail.com>
2019-08-20 10:07:57 -0500

    Added images in the user interface for the adjacent play strategies.

---
+++ Noise.UI/Views/AdjacentPlaySelector.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Noise.Infrastructure.Dto;
+
+namespace Noise.UI.Views {
+    public class AdjacentPlaySelector : DataTemplateSelector {
+        public DataTemplate     PreviousTemplate { get; set; }
+        public DataTemplate     NextTemplate { get; set; }
+        public DataTemplate     BothTemplate { get; set; }
+        public DataTemplate     NeitherTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate( object item, DependencyObject container ) {
+            var retValue = NeitherTemplate;
+
+            if( item != null ) {
+                var playMode = (ePlayAdjacentStrategy)item;
+
+                switch( playMode ) {
+                    case ePlayAdjacentStrategy.None:
+                        retValue = NeitherTemplate;
+                        break;
+
+                    case ePlayAdjacentStrategy.PlayNext:
+                        retValue = NextTemplate;
+                        break;
+
+                    case ePlayAdjacentStrategy.PlayNextPrevious:
+                        retValue = BothTemplate;
+                        break;
+
+                    case ePlayAdjacentStrategy.PlayPrevious:
+                        retValue = PreviousTemplate;
+                        break;
+                }
+            }
+
+            return retValue;
+        }
+    }
+}
