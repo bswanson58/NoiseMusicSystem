b426967183b8c06f5d0b491b86f0bcacefafd2f0
Bill Swanson <bswanson58@gmail.com>
2019-10-28 12:36:41 -0500

    Moved the numeric text multitude value converter into the reusable UI assembly, updated it's functionality.

---
+++ ReusableBits.Ui/ValueConverters/NumericTextMultitudeConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+// from: https://thomasfreudenberg.com/archive/2017/01/21/presenting-byte-size-values-in-wpf/
+
+namespace ReusableBits.Ui.ValueConverters {
+	public class NumericTextMultitudeConverter : IValueConverter {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        private static extern long StrFormatByteSizeW(long qdw, [MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszBuf, int cchBuf);
+
+        public object Convert( object value, Type targetType, object parameter, CultureInfo culture ) {
+            var number = System.Convert.ToInt64( value );
+            var sb = new StringBuilder( 32 );
+            
+            StrFormatByteSizeW( number, sb, sb.Capacity );
+
+            return sb.ToString();
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture ) {
+            return DependencyProperty.UnsetValue;
+        }
+        /*
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture ) {
+			var lValue = System.Convert.ToInt64( value );
+			var formatValue = lValue;
+			var suffix = String.Empty;
+
+            if( Math.Abs( lValue ) > 999999999 ) {
+                suffix = "G";
+
+                formatValue = lValue / 1000000000;
+            }
+			else if( Math.Abs( lValue ) > 999999 ) {
+				suffix = "M";
+
+				formatValue = lValue / 1000000;
+			}
+            else if( Math.Abs( lValue ) > 999 ) {
+                suffix = "K";
+
+                formatValue = lValue / 1000;
+            }
+
+			return( $"{formatValue}{suffix}" );
+		}
+        */
+	}
+}
