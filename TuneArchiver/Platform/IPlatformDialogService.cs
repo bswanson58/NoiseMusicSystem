3a5fca07c2595aff9c368ad25cab5b28c6a43274
Bill Swanson <bswanson58@gmail.com>
2019-10-28 09:40:22 -0500

    Added support for platform dialogs.

---
+++ TuneArchiver/Platform/IPlatformDialogService.cs
@@ -0,0 +1,9 @@
+﻿namespace TuneArchiver.Platform {
+	public interface IDialogService {
+		bool?	OpenFileDialog( string title, string extensions, string filter, out string fileName );
+		bool?	SaveFileDialog( string title, string extensions, string filter, out string fileName );
+		bool?	SelectFolderDialog( string title, ref string path );
+
+		void	MessageDialog( string title, string message );
+	}
+}
