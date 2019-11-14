ae184570c30e705c2628c71e022f967555f625d8
Bill Swanson <bswanson58@gmail.com>
2019-11-14 12:03:09 -0600

    Added a behavior to scroll a list box to the last item.

---
+++ ArchiveLoader/Behaviours/ListBoxLastItemVisible.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ArchiveLoader.Behaviours {
+    public class ListBoxLastItemVisible {
+        public static object GetChangeTrigger( ListBox listView ) {
+            return( listView.GetValue( ChangeTriggerProperty ));
+        }
+
+        public static void SetChangeTrigger( ListView listView, object value ) {
+            listView.SetValue( ChangeTriggerProperty, value );
+        }
+
+        public static readonly DependencyProperty ChangeTriggerProperty =
+            DependencyProperty.RegisterAttached(
+                "ChangeTrigger",
+                typeof( object ),
+                typeof( ListBoxLastItemVisible ),
+                new UIPropertyMetadata( null, OnChangeTrigger ));
+
+        static void OnChangeTrigger( DependencyObject depObj, DependencyPropertyChangedEventArgs args ) {
+            if(( depObj is ListBox list ) &&
+               ( list.Items.Count > 0 )) {
+                list.ScrollIntoView( list.Items[list.Items.Count - 1]);
+            }
+        }
+    }
+}
