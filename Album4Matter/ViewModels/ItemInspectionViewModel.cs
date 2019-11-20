246de729a855d8e96b1cf455062cf5600c786e00
Bill Swanson <bswanson58@gmail.com>
2019-11-20 12:23:24 -0600

    Added reading of text files to the inspector.

--- Album4Matter/ViewModels/ItemInspectionViewModel.cs
+++ Album4Matter/ViewModels/ItemInspectionViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Reactive.Subjects;
 using Album4Matter.Dto;
 using Album4Matter.Interfaces;
@@ -6,7 +8,10 @@ using ReusableBits.Mvvm.ViewModelSupport;
 
 namespace Album4Matter.ViewModels {
     class ItemInspectionViewModel : AutomaticCommandBase, IItemInspectionViewModel {
+        private readonly string[]                       mTextFileExtensions = { ".txt", ".nfo" };
+
         private readonly Subject<InspectionItemUpdate>  mInspectionChangedSubject;
+        private readonly IPlatformLog                   mLog;
         private string                                  mSelectedText;
 
         public IObservable<InspectionItemUpdate>        InspectionItemChanged => mInspectionChangedSubject;
@@ -14,13 +19,15 @@ namespace Album4Matter.ViewModels {
         public  string                                  InspectionItemName { get; private set; }
         public  string                                  InspectionText { get; set; }
 
-        public ItemInspectionViewModel() {
+        public ItemInspectionViewModel( IPlatformLog log ) {
+            mLog = log;
             mInspectionChangedSubject = new Subject<InspectionItemUpdate>();
         }
 
         public void SetInspectionItem( SourceItem item ) {
             InspectionItemName = item.Name;
-            InspectionText = item.Name;
+
+            InspectItem( item );
 
             RaisePropertyChanged( () => InspectionItemName );
             RaisePropertyChanged( () => InspectionText );
@@ -37,8 +44,42 @@ namespace Album4Matter.ViewModels {
             }
         }
 
+        private void InspectItem( SourceItem item ) {
+            if( ItemIsTextFile( item )) {
+                InspectionText = LoadTextFile( item.FileName );
+            }
+            else {
+                InspectionText = item.Name;
+            }
+        }
+
+        private bool ItemIsTextFile( SourceItem item ) {
+            var retValue = false;
+
+            if( item is SourceFile ) {
+                var extension = Path.GetExtension( item.FileName );
+
+                retValue = mTextFileExtensions.Any( e => e.Equals( extension ));
+            }
+
+            return retValue;
+        }
+
+        private string LoadTextFile( string fileName ) {
+            var retValue = string.Empty;
+
+            try {
+                retValue = File.ReadAllText( fileName );
+            }
+            catch( Exception ex ) {
+                mLog.LogException( $"ItemInspector:LoadTextFile: '{fileName}'", ex );
+            }
+
+            return retValue;
+        }
+
         public void Execute_TextIsArtist() {
-            mInspectionChangedSubject.OnNext( new InspectionItemUpdate( InspectionItem.Artist, SelectedText ));
+            mInspectionChangedSubject.OnNext( new InspectionItemUpdate( InspectionItem.Artist, SelectedText.Trim()));
         }
 
         public bool CanExecute_TextIsArtist() {
@@ -46,7 +87,7 @@ namespace Album4Matter.ViewModels {
         }
 
         public void Execute_TextIsAlbum() {
-            mInspectionChangedSubject.OnNext( new InspectionItemUpdate( InspectionItem.Album, SelectedText ));
+            mInspectionChangedSubject.OnNext( new InspectionItemUpdate( InspectionItem.Album, SelectedText.Trim()));
         }
 
         public bool CanExecute_TextIsAlbum() {
@@ -54,7 +95,7 @@ namespace Album4Matter.ViewModels {
         }
 
         public void Execute_TextIsDate() {
-            mInspectionChangedSubject.OnNext( new InspectionItemUpdate( InspectionItem.Date, SelectedText ));
+            mInspectionChangedSubject.OnNext( new InspectionItemUpdate( InspectionItem.Date, SelectedText.Trim()));
         }
 
         public bool CanExecute_TextIsDate() {
