2a1268900ed949fc57f4b24a380c028e2ddb7bf2
Bill Swanson <bswanson58@gmail.com>
2019-11-27 08:57:07 -0600

    Added the status view and several support classes.

--- TuneRenamer/ApplicationModule.cs
+++ TuneRenamer/ApplicationModule.cs
@@ -1,5 +1,7 @@
-﻿using Prism.Ioc;
+﻿using Caliburn.Micro;
+using Prism.Ioc;
 using Prism.Modularity;
+using ReusableBits.Mvvm.VersionSpinner;
 using TuneRenamer.Interfaces;
 using TuneRenamer.Logging;
 using TuneRenamer.Platform;
@@ -7,10 +9,15 @@ using TuneRenamer.Platform;
 namespace TuneRenamer {
     class ApplicationModule : IModule {
         public void RegisterTypes( IContainerRegistry containerRegistry ) {
+            containerRegistry.RegisterSingleton<IEventAggregator, EventAggregator>();
+
             containerRegistry.Register<IEnvironment, OperatingEnvironment>();
             containerRegistry.Register<IFileWriter, JsonObjectWriter>();
             containerRegistry.Register<IPlatformLog, SeriLogAdapter>();
             containerRegistry.Register<IPreferences, PreferencesManager>();
+            containerRegistry.RegisterSingleton<IPlatformDialogService, PlatformDialogService>();
+
+            containerRegistry.Register<IVersionFormatter, VersionSpinnerViewModel>();
         }
 
         public void OnInitialized( IContainerProvider containerProvider ) {
---
+++ TuneRenamer/Views/StatusView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TuneRenamer.Views {
+    /// <summary>
+    /// Interaction logic for StatusView.xaml
+    /// </summary>
+    public partial class StatusView : UserControl {
+        public StatusView() {
+            InitializeComponent();
+        }
+    }
+}
