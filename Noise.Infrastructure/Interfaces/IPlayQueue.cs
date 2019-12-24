2906ab8a91fc445095ec245a8c5098ab8700a8c6
Bill Swanson <bswanson58@gmail.com>
2019-12-24 09:00:20 -0600

    Converted the play queue manager to be based on a DynamicData SourceList.

--- Noise.Infrastructure/Interfaces/IPlayQueue.cs
+++ Noise.Infrastructure/Interfaces/IPlayQueue.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using DynamicData;
 using Noise.Infrastructure.Dto;
 
 namespace Noise.Infrastructure.Interfaces {
@@ -53,6 +55,7 @@ namespace Noise.Infrastructure.Interfaces {
 		bool			DeletedPlayedTracks { get; set; }
 		int				MaximumPlayedTracks { get; set; }
 
-		IEnumerable<PlayQueueTrack>	PlayList { get; }
+		IEnumerable<PlayQueueTrack>				PlayList { get; }
+        IObservable<IChangeSet<PlayQueueTrack>>	PlayQueue { get; }
 	}
 }
