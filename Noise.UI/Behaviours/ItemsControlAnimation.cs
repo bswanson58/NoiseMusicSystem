5f10bbe35d4624d0e4d46b909f81974a73c4c89a
Bill Swanson <bswanson58@gmail.com>
2020-01-02 09:50:13 -0600

    ItemsControlAnimation code cleanup. Handled the source collection reset condition.

--- Noise.UI/Behaviours/ItemsControlAnimation.cs
+++ Noise.UI/Behaviours/ItemsControlAnimation.cs
@@ -27,10 +27,8 @@ using System.Windows.Media.Animation;
 namespace Noise.UI.Behaviours {
     public class ItemsControlAnimation {
         public static readonly DependencyProperty ItemsSourceProperty =
-        DependencyProperty.RegisterAttached("ItemsSource",
-                                            typeof(IList),
-                                            typeof(ItemsControlAnimation),
-                                            new UIPropertyMetadata(null, ItemsSourcePropertyChanged));
+            DependencyProperty.RegisterAttached( "ItemsSource", typeof( IList ), typeof( ItemsControlAnimation ), new UIPropertyMetadata( null, ItemsSourcePropertyChanged ));
+
         public static void SetItemsSource( DependencyObject element, IList value ) {
             element.SetValue( ItemsSourceProperty, value );
         }
@@ -38,82 +36,9 @@ namespace Noise.UI.Behaviours {
             return (IList)element.GetValue( ItemsSourceProperty );
         }
 
-        private static void ItemsSourcePropertyChanged( DependencyObject source, DependencyPropertyChangedEventArgs e ) {
-            ItemsControl itemsControl = source as ItemsControl;
-            IList itemsSource = e.NewValue as IList;
-            if ( itemsControl == null ) {
-                return;
-            }
-            if ( itemsSource == null ) {
-                itemsControl.ItemsSource = null;
-                return;
-            }
-
-            Type itemsSourceType = itemsSource.GetType();
-            Type listType = typeof(ObservableCollection<>).MakeGenericType(itemsSourceType.GetGenericArguments()[0]);
-            IList mirrorItemsSource = (IList)Activator.CreateInstance(listType);
-            itemsControl.SetBinding( ItemsControl.ItemsSourceProperty, new Binding { Source = mirrorItemsSource } );
-
-            foreach ( object item in itemsSource ) {
-                mirrorItemsSource.Add( item );
-            }
-            FadeInContainers( itemsControl, itemsSource );
-
-            (itemsSource as INotifyCollectionChanged).CollectionChanged +=
-                ( object sender, NotifyCollectionChangedEventArgs ne ) => {
-                    if ( ne.Action == NotifyCollectionChangedAction.Add ) {
-                        foreach ( object newItem in ne.NewItems ) {
-                            mirrorItemsSource.Add( newItem );
-                        }
-                        FadeInContainers( itemsControl, ne.NewItems );
-                    }
-                    else if ( ne.Action == NotifyCollectionChangedAction.Remove ) {
-                        foreach ( object oldItem in ne.OldItems ) {
-                            UIElement container = itemsControl.ItemContainerGenerator.ContainerFromItem(oldItem) as UIElement;
-                            Storyboard fadeOutAnimation = GetFadeOutAnimation(itemsControl);
-                            if ( container != null && fadeOutAnimation != null ) {
-                                Storyboard.SetTarget( fadeOutAnimation, container );
-
-                                EventHandler onAnimationCompleted = null;
-                                onAnimationCompleted = (( sender2, e2 ) => {
-                                    fadeOutAnimation.Completed -= onAnimationCompleted;
-                                    mirrorItemsSource.Remove( oldItem );
-                                });
-
-                                fadeOutAnimation.Completed += onAnimationCompleted;
-                                fadeOutAnimation.Begin();
-                            }
-                            else {
-                                mirrorItemsSource.Remove( oldItem );
-                            }
-                        }
-                    }
-                };
-        }
-
-        private static void FadeInContainers( ItemsControl itemsControl, IList newItems ) {
-            EventHandler statusChanged = null;
-            statusChanged = new EventHandler( delegate {
-                if ( itemsControl.ItemContainerGenerator.Status == GeneratorStatus.ContainersGenerated ) {
-                    itemsControl.ItemContainerGenerator.StatusChanged -= statusChanged;
-                    foreach ( object newItem in newItems ) {
-                        UIElement container = itemsControl.ItemContainerGenerator.ContainerFromItem(newItem) as UIElement;
-                        Storyboard fadeInAnimation = GetFadeInAnimation(itemsControl);
-                        if ( container != null && fadeInAnimation != null ) {
-                            Storyboard.SetTarget( fadeInAnimation, container );
-                            fadeInAnimation.Begin();
-                        }
-                    }
-                }
-            } );
-            itemsControl.ItemContainerGenerator.StatusChanged += statusChanged;
-        }
-
         public static readonly DependencyProperty FadeInAnimationProperty =
-        DependencyProperty.RegisterAttached("FadeInAnimation",
-                                            typeof(Storyboard),
-                                            typeof(ItemsControlAnimation),
-                                            new UIPropertyMetadata(null));
+            DependencyProperty.RegisterAttached( "FadeInAnimation", typeof( Storyboard ), typeof( ItemsControlAnimation ), new UIPropertyMetadata( null ));
+
         public static void SetFadeInAnimation( DependencyObject element, Storyboard value ) {
             element.SetValue( FadeInAnimationProperty, value );
         }
@@ -122,15 +47,112 @@ namespace Noise.UI.Behaviours {
         }
 
         public static readonly DependencyProperty FadeOutAnimationProperty =
-        DependencyProperty.RegisterAttached("FadeOutAnimation",
-                                            typeof(Storyboard),
-                                            typeof(ItemsControlAnimation),
-                                            new UIPropertyMetadata(null));
+            DependencyProperty.RegisterAttached( "FadeOutAnimation", typeof( Storyboard ), typeof( ItemsControlAnimation ), new UIPropertyMetadata( null ));
+
         public static void SetFadeOutAnimation( DependencyObject element, Storyboard value ) {
             element.SetValue( FadeOutAnimationProperty, value );
         }
         public static Storyboard GetFadeOutAnimation( DependencyObject element ) {
             return (Storyboard)element.GetValue( FadeOutAnimationProperty );
+        }
+
+        private static void ItemsSourcePropertyChanged( DependencyObject source, DependencyPropertyChangedEventArgs e ) {
+            var itemsControl = source as ItemsControl;
+            var itemsSource = e.NewValue as IList;
+
+            if ( itemsControl == null ) {
+                return;
+            }
+            if ( itemsSource == null ) {
+                itemsControl.ItemsSource = null;
+                return;
+            }
+
+            var itemsSourceType = itemsSource.GetType();
+            var listType = typeof(ObservableCollection<>).MakeGenericType( itemsSourceType.GetGenericArguments()[0]);
+            var mirrorItemsSource = Activator.CreateInstance( listType ) as IList;
+            var deleteList = Activator.CreateInstance( listType ) as IList;
+
+            if(( mirrorItemsSource != null ) &&
+               ( deleteList != null )) {
+                itemsControl.SetBinding( ItemsControl.ItemsSourceProperty, new Binding { Source = mirrorItemsSource } );
+
+                AddItems( itemsControl, itemsSource, mirrorItemsSource );
+
+                if( itemsSource is INotifyCollectionChanged notifySource ) {
+                    notifySource.CollectionChanged += ( sender, collectionChangedArgs ) => {
+                        switch( collectionChangedArgs.Action ) {
+                            case NotifyCollectionChangedAction.Add:
+                                AddItems( itemsControl, collectionChangedArgs.NewItems, mirrorItemsSource );
+                                break;
+
+                            case NotifyCollectionChangedAction.Remove:
+                                RemoveItems( itemsControl, mirrorItemsSource, collectionChangedArgs.OldItems );
+                                break;
+
+                            case NotifyCollectionChangedAction.Reset:
+                                deleteList.Clear();
+                                foreach( var item in mirrorItemsSource ) {
+                                    deleteList.Add( item );
+                                }
+                                RemoveItems( itemsControl, mirrorItemsSource, deleteList );
+                                AddItems( itemsControl, itemsSource, mirrorItemsSource );
+                                break;
+                        }
+                    };
+                }
+            }
+        }
+
+        private static void AddItems( ItemsControl itemsControl, IList items, IList mirrorItems ) {
+            foreach( var newItem in items ) {
+                mirrorItems.Add( newItem );
+            }
+
+            FadeInContainers( itemsControl, items );
+        }
+
+        private static void RemoveItems( ItemsControl itemsControl, IList mirrorItems, IList items ) {
+            foreach( var oldItem in items ) {
+                var container = itemsControl.ItemContainerGenerator.ContainerFromItem( oldItem ) as UIElement;
+                var fadeOutAnimation = GetFadeOutAnimation(itemsControl);
+
+                if(( container != null ) &&
+                   ( fadeOutAnimation != null )) {
+                    Storyboard.SetTarget( fadeOutAnimation, container );
+
+                    void OnAnimationCompleted( object sender2, EventArgs e2 ) {
+                        fadeOutAnimation.Completed -= OnAnimationCompleted;
+                        mirrorItems.Remove( oldItem );
+                    }
+
+                    fadeOutAnimation.Completed += OnAnimationCompleted;
+                    fadeOutAnimation.Begin();
+                }
+                else {
+                    mirrorItems.Remove( oldItem );
+                }
+            }
+        }
+
+        private static void FadeInContainers( ItemsControl itemsControl, IList newItems ) {
+            void StatusChanged( object sender, EventArgs e ) {
+                if( itemsControl.ItemContainerGenerator.Status == GeneratorStatus.ContainersGenerated ) {
+                    itemsControl.ItemContainerGenerator.StatusChanged -= StatusChanged;
+                    foreach( object newItem in newItems ) {
+                        var container = itemsControl.ItemContainerGenerator.ContainerFromItem( newItem ) as UIElement;
+                        var fadeInAnimation = GetFadeInAnimation( itemsControl );
+
+                        if( container != null &&
+                            fadeInAnimation != null ) {
+                            Storyboard.SetTarget( fadeInAnimation, container );
+                            fadeInAnimation.Begin();
+                        }
+                    }
+                }
+            }
+
+            itemsControl.ItemContainerGenerator.StatusChanged += StatusChanged;
         }
     }
 }
