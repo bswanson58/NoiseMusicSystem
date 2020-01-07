75746eedb6d5ed4d692d8da5d832fdf516a65682
Bill Swanson <bswanson58@gmail.com>
2020-01-07 12:45:30 -0600

    Created a tree like display for the playback related tracks view.

---
+++ Noise.UI/Dto/RelatedTrackNode.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using Caliburn.Micro;
+using Noise.Infrastructure.Dto;
+using ReusableBits.Mvvm.ViewModelSupport;
+
+namespace Noise.UI.Dto {
+    public class RelatedTrackNode : AutomaticCommandBase {
+        protected readonly Action<RelatedTrackNode> OnPlay;
+        private bool                                mIsExpanded;
+
+        public  DbArtist    Artist { get; }
+        public  DbAlbum     Album { get; }
+        public  DbTrack     Track { get; }
+        public	string		TrackName => Track.Name;
+        public  string      Key => TrackName;
+        public  string      AlbumName => $"{Artist.Name}/{Album.Name}";
+
+        public RelatedTrackNode( DbArtist artist, DbAlbum album, DbTrack track, Action<RelatedTrackNode> onPlay ) {
+            Artist = artist;
+            Album = album;
+            Track = track;
+
+            OnPlay = onPlay;
+        }
+
+        public bool IsExpanded {
+            get => mIsExpanded;
+            set {
+                mIsExpanded = value;
+
+                RaisePropertyChanged( () => IsExpanded );
+            }
+        }
+
+        public void Execute_Play() {
+            OnPlay?.Invoke( this );
+        }
+    }
+
+    public class RelatedTrackParent : RelatedTrackNode {
+        public	string		                            FirstAlbumName => MultipleTracks ? IsExpanded ? "Album List:" : $" (on {Tracks.Count} albums - expand to view list)" : AlbumName;
+        public  string                                  ParentName => Track.Name;
+        public	BindableCollection<RelatedTrackNode>    Tracks { get; }
+        public  bool                                    IsPlayable => Tracks.Count == 0;
+        public	bool		                            MultipleTracks => Tracks.Count > 0;
+
+        public RelatedTrackParent( DbArtist artist, DbAlbum album, DbTrack track, Action<RelatedTrackNode> onPlay ) :
+            base( artist, album, track, onPlay ) {
+            Tracks = new BindableCollection<RelatedTrackNode>();
+        }
+
+        public void AddAlbum( DbArtist artist, DbAlbum album, DbTrack track ) {
+            if(!Tracks.Any()) {
+                Tracks.Add( new RelatedTrackNode( Artist, Album, Track, OnPlay ));
+            }
+
+            Tracks.Add( new RelatedTrackNode( artist, album, track, OnPlay ));
+//            Tracks.OrderBy( a => a.AlbumName );
+
+            RaisePropertyChanged( () => IsPlayable );
+            RaisePropertyChanged( () => MultipleTracks );
+            RaisePropertyChanged( () => FirstAlbumName );
+        }
+    }
+}
--- Noise.UI/ViewModels/PlaybackRelatedViewModel.cs
+++ Noise.UI/ViewModels/PlaybackRelatedViewModel.cs
@@ -1,27 +1,27 @@
 ﻿using System;
+using System.Linq;
 using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Caliburn.Micro;
 using DynamicData;
-using DynamicData.Binding;
 using Noise.Infrastructure;
 using Noise.Infrastructure.Dto;
 using Noise.Infrastructure.Interfaces;
-using Noise.UI.Adapters;
+using Noise.UI.Dto;
 using Noise.UI.Interfaces;
 using ReactiveUI;
 
 namespace Noise.UI.ViewModels {
     class PlaybackRelatedViewModel : ReactiveObject, IDisposable {
-        private readonly ITrackProvider                         mTrackProvider;
-        private readonly ISearchProvider						mSearchProvider;
-        private readonly IPlayCommand							mPlayCommand;
-        private readonly IEventAggregator						mEventAggregator;
-        private readonly IDisposable                            mSubscriptions;
-        private readonly ReactiveCommand<PlayingItem, Unit>     mStartSearch;
+        private readonly ITrackProvider                     mTrackProvider;
+        private readonly ISearchProvider					mSearchProvider;
+        private readonly IPlayCommand						mPlayCommand;
+        private readonly IEventAggregator					mEventAggregator;
+        private readonly IDisposable                        mSubscriptions;
+        private readonly ReactiveCommand<PlayingItem, Unit> mStartSearch;
 
-        public	ObservableCollectionExtended<SearchViewNode>	SearchResults { get; }
+        public	BindableCollection<RelatedTrackParent>      Tracks { get; }
 
         public PlaybackRelatedViewModel( ISelectionState selectionState, ISearchProvider searchProvider, ITrackProvider trackProvider, IPlayCommand playCommand,
                                          IEventAggregator eventAggregator ) {
@@ -30,14 +30,11 @@ namespace Noise.UI.ViewModels {
             mPlayCommand = playCommand;
             mEventAggregator = eventAggregator;
 
-            SearchResults = new ObservableCollectionExtended<SearchViewNode>();
+            Tracks = new BindableCollection<RelatedTrackParent>();
 
             var searchResultsSubscription = 
                 mSearchProvider.SearchResults
-                    .Transform( r => new SearchViewNode( r, OnPlay ))
-                    .Filter( n => n.CanPlay )
-                    .ObserveOnDispatcher()
-                    .Bind( SearchResults )
+                    .Do( AddSearchItem )
                     .Subscribe();
 
             mStartSearch = ReactiveCommand.CreateFromObservable<PlayingItem, Unit>( item => OnStartSearch( item ).SubscribeOn( RxApp.TaskpoolScheduler ));
@@ -47,6 +44,33 @@ namespace Noise.UI.ViewModels {
             mEventAggregator.Subscribe( this );
 
             mSubscriptions = new CompositeDisposable( selectionStateSubscription, searchResultsSubscription );
+        }
+
+        private void AddSearchItem( IChangeSet<SearchResultItem> items ) {
+            foreach( var change in items ) {
+                if( change.Reason == ListChangeReason.Clear ) {
+                    Tracks.Clear();
+                }
+                else if( change.Reason == ListChangeReason.Add ) {
+                    AddSearchItem( change.Item.Current );
+                }
+                else if( change.Reason == ListChangeReason.AddRange ) {
+                    foreach( var  item in change.Range ) {
+                        AddSearchItem( item );
+                    }
+                }
+            }
+        }
+
+        private void AddSearchItem( SearchResultItem item ) {
+            var parent = Tracks.FirstOrDefault( i => i.Key.Equals( item.Track.Name ));
+
+            if( parent != null ) {
+                parent.AddAlbum( item.Artist, item.Album, item.Track );
+            }
+            else {
+                Tracks.Add( new RelatedTrackParent( item.Artist, item.Album, item.Track, OnPlay ));
+            }
         }
 
         private void OnTrackStarted( PlayingItem playingItem ) {
@@ -67,7 +91,7 @@ namespace Noise.UI.ViewModels {
             });
         }
 
-        private void OnPlay( SearchViewNode node ) { 
+        private void OnPlay( RelatedTrackNode node ) { 
             if( node.Track != null ) {
                 mPlayCommand.Play( node.Track );
             }
