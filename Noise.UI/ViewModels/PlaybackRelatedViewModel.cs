7798eb2b598833f6570bcb32cc767dc5258770e3
Bill Swanson <bswanson58@gmail.com>
2020-01-07 10:08:23 -0600

    Basic view populated with search results of the track name.

--- Noise.UI/ViewModels/PlaybackRelatedViewModel.cs
+++ Noise.UI/ViewModels/PlaybackRelatedViewModel.cs
@@ -1,10 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Caliburn.Micro;
+using DynamicData;
+using DynamicData.Binding;
+using Noise.Infrastructure;
+using Noise.Infrastructure.Dto;
+using Noise.Infrastructure.Interfaces;
+using Noise.UI.Adapters;
+using Noise.UI.Interfaces;
+using ReactiveUI;
 
 namespace Noise.UI.ViewModels {
-    class PlaybackRelatedViewModel {
+    class PlaybackRelatedViewModel : ReactiveObject, IDisposable {
+        private readonly ITrackProvider                         mTrackProvider;
+        private readonly ISearchProvider						mSearchProvider;
+        private readonly IPlayCommand							mPlayCommand;
+        private readonly IEventAggregator						mEventAggregator;
+        private readonly IDisposable                            mSubscriptions;
+        private readonly ReactiveCommand<PlayingItem, Unit>     mStartSearch;
+
+        public	ObservableCollectionExtended<SearchViewNode>	SearchResults { get; }
+
+        public PlaybackRelatedViewModel( ISelectionState selectionState, ISearchProvider searchProvider, ITrackProvider trackProvider, IPlayCommand playCommand,
+                                         IEventAggregator eventAggregator ) {
+            mSearchProvider = searchProvider;
+            mTrackProvider = trackProvider;
+            mPlayCommand = playCommand;
+            mEventAggregator = eventAggregator;
+
+            SearchResults = new ObservableCollectionExtended<SearchViewNode>();
+
+            var searchResultsSubscription = 
+                mSearchProvider.SearchResults
+                    .Transform( r => new SearchViewNode( r, OnPlay ))
+                    .Filter( n => n.CanPlay )
+                    .ObserveOnDispatcher()
+                    .Bind( SearchResults )
+                    .Subscribe();
+
+            mStartSearch = ReactiveCommand.CreateFromObservable<PlayingItem, Unit>( item => OnStartSearch( item ).SubscribeOn( RxApp.TaskpoolScheduler ));
+
+            var selectionStateSubscription = selectionState.PlayingTrackChanged.Subscribe( OnTrackStarted );
+
+            mEventAggregator.Subscribe( this );
+
+            mSubscriptions = new CompositeDisposable( selectionStateSubscription, searchResultsSubscription );
+        }
+
+        private void OnTrackStarted( PlayingItem playingItem ) {
+            mStartSearch.Execute( playingItem );
+        }
+
+        private IObservable<Unit> OnStartSearch( PlayingItem item ) {
+            return Observable.Start( () => {
+                if( item.Track != Constants.cDatabaseNullOid ) {
+                    var track = mTrackProvider.GetTrack( item.Track );
+
+                    if( track != null ) {
+                        mSearchProvider.StartSearch( eSearchItemType.Track , track.Name );
+                    }
+                }
+
+                return Unit.Default;
+            });
+        }
+
+        private void OnPlay( SearchViewNode node ) { 
+            if( node.Track != null ) {
+                mPlayCommand.Play( node.Track );
+            }
+        }
+
+        public void Dispose() {
+            mSubscriptions?.Dispose();
+
+            mEventAggregator.Unsubscribe( this );
+        }
     }
 }
