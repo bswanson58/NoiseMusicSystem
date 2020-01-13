1191146d8ffb43f1073d78074a8172d484e59fba
Bill Swanson <bswanson58@gmail.com>
2020-01-13 12:29:23 -0600

    Added an application resource dictionary to satisfy design time needs.

---
+++ Noise.UI/Resources/DesignTimeResources.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Noise.UI.Resources {
+    /// <summary>
+    /// Interaction logic for DesignTimeResources.xaml
+    /// </summary>
+    public partial class DesignTimeResources {
+        public DesignTimeResources() {
+            InitializeComponent();
+        }
+    }
+}
