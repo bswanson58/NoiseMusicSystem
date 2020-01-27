7321b3e856f98dafec9943419b819970685f68fc
Bill Swanson <bswanson58@gmail.com>
2020-01-27 14:58:30 -0600

    Created the body of the rated tracks view model.

---
+++ Noise.UI/Dto/UiTrackAlbum.cs
@@ -0,0 +1,16 @@
+﻿using Noise.Infrastructure.Dto;
+
+namespace Noise.UI.Dto {
+    public class UiTrackAlbum {
+        private readonly DbAlbum    mAlbum;
+        private readonly DbTrack    mTrack;
+
+        public  string              TrackName => mTrack.Name;
+        public  int                 SortRating => mTrack.IsFavorite ? 6 : mTrack.Rating;
+
+        public UiTrackAlbum( DbAlbum album, DbTrack track ) {
+            mAlbum = album;
+            mTrack = track;
+        }
+    }
+}
--- Noise.UI/ViewModels/RatedTracksViewModel.cs
+++ Noise.UI/ViewModels/RatedTracksViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading;
+using System.Windows.Data;
 using Caliburn.Micro;
 using Microsoft.Practices.Prism;
 using Noise.Infrastructure;
+using Noise.Infrastructure.Dto;
 using Noise.Infrastructure.Interfaces;
 using Noise.Infrastructure.Support;
 using Noise.UI.Dto;
@@ -21,13 +24,13 @@ namespace Noise.UI.ViewModels {
         private readonly IAlbumProvider		mAlbumProvider;
         private readonly ITrackProvider		mTrackProvider;
         private readonly IPlayCommand		mPlayCommand;
-        private readonly BindableCollection<UiArtistTrackNode>	mTrackList;
+        private readonly BindableCollection<UiTrackAlbum>	mTrackList;
         private ICollectionView				mTrackView;
         private	bool						mIsActive;
-        private TaskHandler<IEnumerable<UiArtistTrackNode>>	mUpdateTask;
-        private CancellationTokenSource						mCancellationTokenSource;
+        private TaskHandler<IEnumerable<UiTrackAlbum>>      mUpdateTask;
+        private CancellationTokenSource					    mCancellationTokenSource;
 
-//        public	bool									IsListFiltered => !String.IsNullOrWhiteSpace( FilterText );
+        public	bool									IsListFiltered => !String.IsNullOrWhiteSpace( FilterText );
         public	event EventHandler						IsActiveChanged = delegate { };
 
         public RatedTracksViewModel( IEventAggregator eventAggregator, ISelectionState selectionState, IPlayCommand playCommand,
@@ -41,7 +44,7 @@ namespace Noise.UI.ViewModels {
 
             mEventAggregator.Subscribe( this );
 
-            mTrackList = new BindableCollection<UiArtistTrackNode>();
+            mTrackList = new BindableCollection<UiTrackAlbum>();
             TracksValid = false;
 
 //            mSelectionState.CurrentArtistChanged.Subscribe( OnArtistChanged );
@@ -54,11 +57,11 @@ namespace Noise.UI.ViewModels {
 
                 if( mIsActive ) {
                     if( mSelectionState.CurrentArtist != null ) {
-//                        UpdateTrackList( mSelectionState.CurrentArtist );
+                        UpdateTrackList( mSelectionState.CurrentArtist );
                     }
                 }
                 else {
-//                    CancelRetrievalTask();
+                    CancelRetrievalTask();
                     mTrackList.Clear();
                 }
 
@@ -74,6 +77,113 @@ namespace Noise.UI.ViewModels {
         public void Handle( Events.DatabaseClosing args ) {
             mTrackList.Clear();
             TracksValid = false;
+        }
+
+        public ICollectionView TrackList {
+            get{ 
+                if( mTrackView == null ) {
+                    mTrackView = CollectionViewSource.GetDefaultView( mTrackList );
+
+                    mTrackView.Filter += OnTrackFilter;
+                }
+
+                return mTrackView;
+            }
+        }
+
+        public string FilterText {
+            get { return( Get(() => FilterText )); }
+            set {
+                Set(() => FilterText, value );
+
+                mTrackView?.Refresh();
+                RaisePropertyChanged( () => IsListFiltered );
+            }
+        }
+
+        private bool OnTrackFilter( object node ) {
+            var retValue = true;
+
+            if((!string.IsNullOrWhiteSpace( FilterText )) &&
+               ( node is UiArtistTrackNode trackNode )) {
+                if( trackNode.TrackName.IndexOf( FilterText, StringComparison.OrdinalIgnoreCase ) == -1 ) {
+                    retValue = false;
+                }
+            }
+
+            return retValue;
+        }
+
+		internal TaskHandler<IEnumerable<UiTrackAlbum>>  UpdateTask {
+			get {
+				if( mUpdateTask == null ) {
+					Execute.OnUIThread( () => mUpdateTask = new TaskHandler<IEnumerable<UiTrackAlbum>>());
+				}
+
+				return( mUpdateTask );
+			}
+			set => mUpdateTask = value;
+        }
+
+		private CancellationToken GenerateCanellationToken() {
+			mCancellationTokenSource = new CancellationTokenSource();
+
+			return( mCancellationTokenSource.Token );
+		}
+
+		private void CancelRetrievalTask() {
+			if( mCancellationTokenSource != null ) {
+				mCancellationTokenSource.Cancel();
+				mCancellationTokenSource = null;
+			}
+		}
+
+		private void UpdateTrackList( DbArtist artist ) {
+			CancelRetrievalTask();
+			TracksValid = false;
+			FilterText = String.Empty;
+
+			var cancellationToken = GenerateCanellationToken();
+
+			if(( artist != null ) &&
+			   ( mIsActive )) {
+				UpdateTask.StartTask( () => BuildTrackList( artist, cancellationToken ), 
+									  SetTrackList,
+									  ex => mLog.LogException( $"UpdateTrackList for {artist}", ex ),
+									  cancellationToken );
+			}
+		}
+
+        private IEnumerable<UiTrackAlbum> BuildTrackList( DbArtist forArtist, CancellationToken cancellationToken ) {
+            var retValue = new List<UiTrackAlbum>();
+
+            using( var trackList = mTrackProvider.GetRatedTracks( forArtist )) {
+                foreach( var track in trackList.List ) {
+                    if(!cancellationToken.IsCancellationRequested ) {
+                        var album = mAlbumProvider.GetAlbum( track.Album );
+
+                        if( album != null ) {
+                            retValue.Add( new UiTrackAlbum( album, track ));
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+
+            return retValue;
+        }
+
+        private void SetTrackList( IEnumerable<UiTrackAlbum> list ) {
+            mTrackList.Clear();
+            mTrackList.AddRange( from node in list orderby node.SortRating descending, node.TrackName select node );
+
+            TracksValid = true;
+        }
+
+        private void OnTrackPlay( long trackId ) {
+            mPlayCommand.Play( mTrackProvider.GetTrack( trackId ));
         }
 
         public void Dispose() {
