04f1a8fe70a81ea17f69d5990c80f3a662c08333
Bill Swanson <bswanson58@gmail.com>
2020-02-14 09:38:22 -0600

    Hid the status bar when the window is maximized.

--- MilkBottle/ViewModels/ShellViewModel.cs
+++ MilkBottle/ViewModels/ShellViewModel.cs
@@ -1,3 +1,24 @@
-﻿namespace MilkBottle.ViewModels {
-    public class ShellViewModel { }
+﻿using System.Windows;
+using ReusableBits.Mvvm.ViewModelSupport;
+
+namespace MilkBottle.ViewModels {
+    public class ShellViewModel : PropertyChangeBase {
+        private WindowState mCurrentWindowState;
+
+        public  bool        DisplayStatus { get; private set; }
+
+        public ShellViewModel() {
+            DisplayStatus = true;
+        }
+
+        public WindowState CurrentWindowState {
+            get => mCurrentWindowState;
+            set {
+                mCurrentWindowState = value;
+
+                DisplayStatus = mCurrentWindowState != WindowState.Maximized;
+                RaisePropertyChanged( () => DisplayStatus );
+            }
+        }   
+    } 
 }
