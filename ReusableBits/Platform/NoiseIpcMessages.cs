0fd08d5b8dcfc763328f5c9b5c06dd1690cc7a49
Bill Swanson <bswanson58@gmail.com>
2020-02-21 09:21:56 -0600

    Added an IPC handler to ReusableBits

---
+++ ReusableBits/Platform/NoiseIpcMessages.cs
@@ -0,0 +1,18 @@
+﻿namespace ReusableBits.Platform {
+    public static class NoiseIpcSubject {
+        public const string cCompanionApplication = "companionApplication";
+        public const string cActivateApplication = "activateApplication";
+    }
+
+    public class CompanionApplication {
+        public  string  Name { get; }
+        public  string  Icon { get; }
+
+        public CompanionApplication( string name, string icon ) {
+            Name = name;
+            Icon = icon;
+        }
+    }
+
+    public class ActivateApplication { }
+}
