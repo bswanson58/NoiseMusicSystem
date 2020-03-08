0d75a0be50690fc9bfc422bafff95c5bedaf61f6
Bill Swanson <bswanson58@gmail.com>
2020-03-08 13:13:19 -0500

    Created the view and view model for the tag editor.

---
+++ MilkBottle/ViewModels/TagEditViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using MilkBottle.Entities;
+using MilkBottle.Interfaces;
+using Prism.Commands;
+
+namespace MilkBottle.ViewModels {
+    class TagEditViewModel {
+        private readonly ITagProvider   mTagProvider;
+
+        public  ObservableCollection<PresetTag> Tags { get; }
+        public  ObservableCollection<Preset>    TaggedPresets {  get; }
+
+        public  DelegateCommand                 NewTag { get; }
+
+        public TagEditViewModel( ITagProvider tagProvider ) {
+            mTagProvider = tagProvider;
+
+            Tags = new ObservableCollection<PresetTag>();
+            TaggedPresets = new ObservableCollection<Preset>();
+
+            NewTag = new DelegateCommand( OnNewTag );
+
+            LoadTags();
+        }
+
+        private void LoadTags() {
+            Tags.Clear();
+
+            mTagProvider.SelectTags( list => Tags.AddRange( from t in list orderby t.Name select t ));
+        }
+
+        private void OnNewTag() {
+
+        }
+    }
+}
