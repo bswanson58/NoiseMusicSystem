c795b0aeb727a5ed487eca9db6e0e9d2aebd920b
Bill Swanson <bswanson58@gmail.com>
2020-03-13 13:41:40 -0500

    Added a basic status view to the reviewer panel.

---
+++ MilkBottle/ViewModels/ReviewStatusViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MilkBottle.Entities;
+using MilkBottle.Interfaces;
+using ReusableBits.Mvvm.ViewModelSupport;
+
+namespace MilkBottle.ViewModels {
+    class ReviewStatusViewModel : PropertyChangeBase, IDisposable {
+        private IDisposable                 mPresetSubscription;
+
+        public  string                      PresetName { get; private set; }
+
+        public ReviewStatusViewModel( IPresetController presetController ) {
+            mPresetSubscription = presetController.CurrentPreset.Subscribe( OnPresetChanged );
+        }
+
+        private void OnPresetChanged( Preset preset ) {
+            PresetName = preset != null ? preset.Name : String.Empty;
+
+            RaisePropertyChanged( () => PresetName );
+        }
+
+        public void Dispose() {
+            mPresetSubscription?.Dispose();
+            mPresetSubscription = null;
+        }
+    }
+}
