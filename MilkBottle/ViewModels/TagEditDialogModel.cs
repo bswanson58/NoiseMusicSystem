038fa3759431ce62a96548bf17fcdf27d00e5522
Bill Swanson <bswanson58@gmail.com>
2020-03-21 14:26:28 -0500

    Added the ability to create a new tag from the tag associate dialog.

--- MilkBottle/ViewModels/TagEditDialogModel.cs
+++ MilkBottle/ViewModels/TagEditDialogModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Windows.Documents;
 using MilkBottle.Dto;
 using MilkBottle.Entities;
 using MilkBottle.Interfaces;
@@ -15,6 +14,7 @@ namespace MilkBottle.ViewModels {
 
         private readonly ITagProvider       mTagProvider;
         private readonly IPlatformLog       mLog;
+        private readonly IDialogService     mDialogService;
         private Preset                      mPreset;
 
         public  String                      Name { get; set; }
@@ -26,11 +26,13 @@ namespace MilkBottle.ViewModels {
 
         public  DelegateCommand             Ok { get; }
         public  DelegateCommand             Cancel { get; }
+        public  DelegateCommand             NewTag { get; }
 
         public  event Action<IDialogResult> RequestClose;
 
-        public TagEditDialogModel( ITagProvider tagProvider, IPlatformLog log ) {
+        public TagEditDialogModel( ITagProvider tagProvider, IDialogService dialogService, IPlatformLog log ) {
             mTagProvider = tagProvider;
+            mDialogService = dialogService;
             mLog = log;
 
             Title = "Associate Tags";
@@ -38,6 +40,7 @@ namespace MilkBottle.ViewModels {
 
             Ok = new DelegateCommand( OnOk );
             Cancel = new DelegateCommand( OnCancel );
+            NewTag = new DelegateCommand( OnNewTag );
         }
 
         public bool CanCloseDialog() {
@@ -47,9 +50,13 @@ namespace MilkBottle.ViewModels {
         public void OnDialogOpened( IDialogParameters parameters ) {
             mPreset = parameters.GetValue<Preset>( cPresetParameter );
 
+            UpdatePresetState();
+        }
+
+        private void UpdatePresetState() {
             Tags.Clear();
             mTagProvider.SelectTags( list => Tags.AddRange( from t in list orderby t.Name select new UiTag( t, OnTagChanged, null, null )))
-                            .IfLeft( ex => LogException( "OnDialogOpened", ex ) );
+                .IfLeft( ex => LogException( "OnDialogOpened", ex ) );
 
             foreach( var tag in Tags ) {
                 tag.SetSelectedState( mPreset.Tags.FirstOrDefault( t => t.Id.Equals( tag.Tag.Id )) != null );
@@ -64,6 +71,21 @@ namespace MilkBottle.ViewModels {
         private void OnTagChanged( UiTag tag ) {
             mPreset = mPreset.WithTagState( tag.Tag, tag.IsSelected );
         }
+
+        private void OnNewTag() {
+            mDialogService.ShowDialog( "NewTagDialog", null, OnNewTagResult );
+        }
+
+        private void OnNewTagResult( IDialogResult result ) {
+            if( result.Result == ButtonResult.OK ) {
+                var newTag = new PresetTag( result.Parameters.GetValue<string>( NewTagDialogModel.cTagNameParameter ));
+
+                mTagProvider.Insert( newTag );
+
+                UpdatePresetState();
+            }
+        }
+
 
         public void OnOk() {
             mPreset = mPreset.WithFavorite( IsFavorite );
