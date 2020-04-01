245295371ffdd4cc3ed48a8680ff3afa366d8f22
Bill Swanson <bswanson58@gmail.com>
2020-04-01 11:31:27 -0500

    Eliminated compiler warning.

--- MilkBottle/Models/PresetTimers/InfiniteDurationTimer.cs
+++ MilkBottle/Models/PresetTimers/InfiniteDurationTimer.cs
@@ -3,7 +3,7 @@ using MilkBottle.Interfaces;
 
 namespace MilkBottle.Models.PresetTimers {
     class InfiniteDurationTimer : IPresetTimer {
-        public event EventHandler PresetTimeElapsed;
+        public event EventHandler PresetTimeElapsed = delegate { };
 
         public void StartTimer() { }
         public void StopTimer() { }
