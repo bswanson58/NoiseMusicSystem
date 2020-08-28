2ed0cdf9982b9dd50078f21bc0dc42228b76eb65
Bill Swanson <bswanson58@gmail.com>
2020-08-28 13:33:52 -0500

    Added a view to select the current zone.

---
+++ MilkBottle/LightPipe/ViewModels/ZoneSelectionViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using LightPipe.Dto;
+using LightPipe.Interfaces;
+using ReusableBits.Mvvm.ViewModelSupport;
+
+namespace LightPipe.ViewModels {
+    class ZoneSelectionViewModel : PropertyChangeBase {
+        private readonly IZoneManager   mZoneManager;
+        private ZoneGroup               mCurrentZone;
+
+        public  ObservableCollection<ZoneGroup> Zones { get; }
+        public  List<ZoneDefinition>            ZoneList => CurrentZone?.Zones;
+
+        public ZoneSelectionViewModel( IZoneManager zoneManager ) {
+            mZoneManager = zoneManager;
+
+            Zones = new ObservableCollection<ZoneGroup>();
+
+            LoadZones();
+        }
+
+        public ZoneGroup CurrentZone {
+            get => mCurrentZone;
+            set {
+                mCurrentZone = value;
+
+                RaisePropertyChanged( () => CurrentZone );
+            }
+        }
+
+        private void LoadZones() {
+            Zones.Clear();
+            Zones.AddRange( mZoneManager.GetZones());
+
+            var currentGroup = mZoneManager.GetCurrentGroup();
+
+            CurrentZone = Zones.FirstOrDefault( z => z.GroupId.Equals( currentGroup.GroupId )) ??
+                          Zones.FirstOrDefault();
+        }
+    }
+}
