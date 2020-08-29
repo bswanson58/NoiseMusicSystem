7a0323694df3ac6aff217a5f61449261a4778606
Bill Swanson <bswanson58@gmail.com>
2020-08-29 13:59:58 -0500

    Added a LightPipeControlView with the LightPipeStatusView embedded in it.

---
+++ MilkBottle/ViewModels/LightPipeControlViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MilkBottle.Interfaces;
+using ReusableBits.Mvvm.ViewModelSupport;
+
+namespace MilkBottle.ViewModels {
+    class LightPipeControlViewModel : PropertyChangeBase {
+        private readonly ILightPipePump     mLightPipePump;
+
+        public LightPipeControlViewModel( ILightPipePump pump ) {
+            mLightPipePump = pump;
+        }
+    }
+}
