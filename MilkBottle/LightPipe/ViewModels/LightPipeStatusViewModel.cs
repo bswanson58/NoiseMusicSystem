262b4e7939709b888616d69eb7d51d3556c0f8ed
Bill Swanson <bswanson58@gmail.com>
2020-08-31 11:31:30 -0500

    Cleared the LightPipe status view when the zone in changed, highlighted high processing times.

--- MilkBottle/LightPipe/ViewModels/LightPipeStatusViewModel.cs
+++ MilkBottle/LightPipe/ViewModels/LightPipeStatusViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Media;
+using Caliburn.Micro;
 using LightPipe.Dto;
 using LightPipe.Interfaces;
 using ReusableBits.Mvvm.ViewModelSupport;
@@ -21,37 +22,59 @@ namespace LightPipe.ViewModels {
         }
     }
 
-    public class LightPipeStatusViewModel : PropertyChangeBase, IDisposable {
+    public class LightPipeStatusViewModel : PropertyChangeBase, IDisposable, IHandle<MilkBottle.Infrastructure.Events.CurrentZoneChanged> {
+        private const int                           cProcessingLimit = 60;
+
         private readonly IImageProcessor            mImageProcessor;
+        private readonly IEventAggregator           mEventAggregator;
         private IDisposable                         mImageProcessSubscription;
+        private bool                                mClearZones;
 
         public  ObservableCollection<ZoneInfo>      ZoneSummaries { get; }
         public  string                              ElapsedTime { get; private set; }
+        public  bool                                HighProcessingTime { get; private set; }
 
-        public LightPipeStatusViewModel( IImageProcessor imageProcessor ) {
+        public LightPipeStatusViewModel( IImageProcessor imageProcessor, IEventAggregator eventAggregator ) {
             mImageProcessor = imageProcessor;
+            mEventAggregator = eventAggregator;
 
             ZoneSummaries = new ObservableCollection<ZoneInfo>();
 
             mImageProcessSubscription = mImageProcessor.ZoneUpdate.Subscribe( OnZoneUpdated );
+            mEventAggregator.Subscribe( this );
+        }
+
+        public void Handle( MilkBottle.Infrastructure.Events.CurrentZoneChanged args ) {
+            mClearZones = true;
         }
 
         private void OnZoneUpdated( ZoneSummary summary ) {
-            var existing = ZoneSummaries.FirstOrDefault( z => z.ZoneId.Equals( summary.ZoneId ));
+            if( mClearZones ) {
+                ZoneSummaries.Clear();
+                mClearZones = false;
+            }
+            else {
+                var existing = ZoneSummaries.FirstOrDefault( z => z.ZoneId.Equals( summary.ZoneId ));
 
-            if( existing != null ) {
-                ZoneSummaries.Remove( existing );
+                if( existing != null ) {
+                    ZoneSummaries.Remove( existing );
+                }
+
+                ZoneSummaries.Add( new ZoneInfo( summary, summary.FindMeanColors( 4 )));
             }
 
-            ZoneSummaries.Add( new ZoneInfo( summary, summary.FindMeanColors( 4 )));
+            ElapsedTime = $"{mImageProcessor.ElapsedTime}";
+            HighProcessingTime = mImageProcessor.ElapsedTime > cProcessingLimit;
 
-            ElapsedTime = $"{mImageProcessor.ElapsedTime} ms";
             RaisePropertyChanged( () => ElapsedTime );
+            RaisePropertyChanged( () => HighProcessingTime );
         }
 
         public void Dispose() {
             mImageProcessSubscription?.Dispose();
             mImageProcessSubscription = null;
+
+            mEventAggregator.Unsubscribe( this );
         }
     }
 }
