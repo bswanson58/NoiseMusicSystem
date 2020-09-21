c317e513562cb1bafccd205d8a76ac7bfaa1dd5a
Bill Swanson <bswanson58@gmail.com>
2020-09-21 10:59:43 -0500

    Handled mouse wheel events in the WindowsFormsHost and used the event to close the active preset view.

---
+++ MilkBottle/Behaviors/FormsHostEventRouting.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms.Integration;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+using ReusableBits.Ui.Utility;
+using Application = System.Windows.Application;
+using Control = System.Windows.Forms.Control;
+using MouseEventArgs = System.Windows.Forms.MouseEventArgs;
+
+namespace MilkBottle.Behaviors {
+    public class FormsHostEventRouting : Behavior<WindowsFormsHost> {
+        public static readonly DependencyProperty RouteMouseWheelEventsProperty = DependencyProperty.Register(
+            "RouteMouseWheelEvents",
+            typeof( bool ),
+            typeof( FormsHostEventRouting ),
+            new PropertyMetadata( null ) );
+
+        public bool RouteMouseWheelEvents {
+            get => (bool)GetValue( RouteMouseWheelEventsProperty );
+            set => SetValue( RouteMouseWheelEventsProperty, value );
+        }
+
+        public static readonly DependencyProperty RouteToElementProperty = DependencyProperty.Register(
+            "RouteToElement",
+            typeof( FrameworkElement ),
+            typeof( FormsHostEventRouting ),
+            new PropertyMetadata( null ) );
+
+        public FrameworkElement RouteToElement {
+            get => GetValue( RouteToElementProperty ) as FrameworkElement;
+            set => SetValue( RouteToElementProperty, value );
+        }
+
+        public static readonly DependencyProperty RouteToTagProperty = DependencyProperty.Register(
+            "RouteToTag",
+            typeof( String ),
+            typeof( FormsHostEventRouting ),
+            new PropertyMetadata( null ) );
+
+        public String RouteToTag {
+            get => (string)GetValue( RouteToTagProperty );
+            set => SetValue( RouteToTagProperty, value );
+        }
+
+        protected override void OnAttached() {
+            base.OnAttached();
+
+            AssociatedObject.ChildChanged += OnChildChanged;
+        }
+
+        protected override void OnDetaching() {
+            AssociatedObject.ChildChanged -= OnChildChanged;
+
+            base.OnDetaching();
+        }
+
+        private void OnChildChanged( object sender, ChildChangedEventArgs args ) {
+            if( args.PreviousChild is Control control ) {
+                control.MouseWheel -= OnMouseWheel;
+            }
+
+            if( AssociatedObject.Child != null ) {
+                AssociatedObject.Child.MouseWheel += OnMouseWheel;
+            }
+        }
+
+        private void OnMouseWheel( object sender, MouseEventArgs args ) {
+            var wheelEvent = new MouseWheelEventArgs( Mouse.PrimaryDevice, DateTime.Now.Millisecond, args.Delta ) { RoutedEvent = UIElement.MouseWheelEvent, Source = sender };
+
+            if( RouteToElement != null ) {
+                RouteToElement.RaiseEvent( wheelEvent );
+            }
+            else if( !String.IsNullOrWhiteSpace( RouteToTag ) ) {
+                var element = Application.Current.MainWindow.FindChildren<FrameworkElement>().FirstOrDefault( c => c.Tag?.Equals( RouteToTag ) == true );
+
+                element?.RaiseEvent( wheelEvent );
+            }
+            else {
+                AssociatedObject.RaiseEvent( wheelEvent );
+            }
+        }
+    }
+}
---
+++ MilkBottle/Behaviors/RedirectRoutedEventBehavior.cs
@@ -0,0 +1,77 @@
+﻿using System.Reflection;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+
+namespace MilkBottle.Behaviors {
+    public class RedirectRoutedEventBehavior : Behavior<UIElement> {
+        public static readonly DependencyProperty RedirectTargetProperty =
+            DependencyProperty.Register( "RedirectTarget", typeof( UIElement ),
+                typeof( RedirectRoutedEventBehavior ),
+                new PropertyMetadata( null ));
+
+        public static readonly DependencyProperty RoutedEventProperty =
+            DependencyProperty.Register( "RoutedEvent", typeof( RoutedEvent ), typeof( RedirectRoutedEventBehavior ),
+                new PropertyMetadata( null, OnRoutedEventChanged ) );
+
+        public UIElement RedirectTarget {
+            get => (UIElement)GetValue( RedirectTargetProperty );
+            set => SetValue( RedirectTargetProperty, value );
+        }
+
+        public RoutedEvent RoutedEvent {
+            get => (RoutedEvent)GetValue( RoutedEventProperty );
+            set => SetValue( RoutedEventProperty, value );
+        }
+
+        private static MethodInfo MemberwiseCloneMethod { get; }
+            = typeof( object )
+                .GetMethod( "MemberwiseClone", BindingFlags.NonPublic | BindingFlags.Instance );
+
+        private static void OnRoutedEventChanged( DependencyObject d, DependencyPropertyChangedEventArgs e ) {
+            ((RedirectRoutedEventBehavior)d).OnRoutedEventChanged( (RoutedEvent)e.OldValue, (RoutedEvent)e.NewValue );
+        }
+
+        private void OnRoutedEventChanged( RoutedEvent oldValue, RoutedEvent newValue ) {
+            if( AssociatedObject == null ) {
+                return;
+            }
+
+            if( oldValue != null ) {
+                AssociatedObject.RemoveHandler( oldValue, new RoutedEventHandler( EventHandler ));
+            }
+
+            if( newValue != null ) {
+                AssociatedObject.AddHandler( newValue, new RoutedEventHandler( EventHandler ));
+            }
+        }
+
+        protected override void OnAttached() {
+            base.OnAttached();
+            if( RoutedEvent != null ) {
+                AssociatedObject.AddHandler( RoutedEvent, new RoutedEventHandler( EventHandler ));
+            }
+        }
+
+        protected override void OnDetaching() {
+            if( RoutedEvent != null ) {
+                AssociatedObject.RemoveHandler( RoutedEvent, new RoutedEventHandler( EventHandler ));
+            }
+
+            base.OnDetaching();
+        }
+
+        private static RoutedEventArgs CloneEvent( RoutedEventArgs e ) {
+            return (RoutedEventArgs)MemberwiseCloneMethod.Invoke( e, null );
+        }
+
+        private void EventHandler( object sender, RoutedEventArgs e ) {
+            var newEvent = CloneEvent( e );
+            e.Handled = true;
+
+            if( RedirectTarget != null ) {
+                newEvent.Source = RedirectTarget;
+                RedirectTarget.RaiseEvent( newEvent );
+            }
+        }
+    }
+}
