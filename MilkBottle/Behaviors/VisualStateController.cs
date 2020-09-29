1c2380fea0b9bfe524d875365d57b4f51870c4c1
Bill Swanson <bswanson58@gmail.com>
2020-09-29 12:20:21 -0500

    Added a simple track information overlay.

---
+++ MilkBottle/Behaviors/VisualStateController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace MilkBottle.Behaviors {
+    public class VisualStateController : Behavior<Control> {
+        public static readonly DependencyProperty DefaultStateProperty = DependencyProperty.Register(
+            "DefaultState",
+            typeof( string ),
+            typeof( VisualStateController ),
+            new PropertyMetadata( null ));
+
+        public string DefaultState {
+            get => (string)GetValue( DefaultStateProperty );
+            set => SetValue( DefaultStateProperty, value );
+        }
+
+        public static readonly DependencyProperty VisualStateProperty = DependencyProperty.Register(
+            "VisualState",
+            typeof( string ),
+            typeof( VisualStateController ),
+            new PropertyMetadata( OnTriggerState ));
+
+        public static void OnTriggerState( DependencyObject sender, DependencyPropertyChangedEventArgs args ) {
+            if( sender is VisualStateController manager ) {
+                manager.GoToState();
+            }
+        }
+
+        public string VisualState {
+            get => (string)GetValue( VisualStateProperty );
+            set => SetValue( VisualStateProperty, value );
+        }
+
+        private void GoToState() {
+            if( AssociatedObject != null ) {
+                if((!String.IsNullOrWhiteSpace( DefaultState )) &&
+                   (!VisualState.Equals( DefaultState ))) {
+                    VisualStateManager.GoToState( AssociatedObject, DefaultState, true );
+                }
+
+                VisualStateManager.GoToState( AssociatedObject, StartUntil( VisualState, '|' ), true );
+            }
+        }
+
+        private static string StartUntil( string input, char stopChar ) {
+            var retValue = input;
+
+            if(!String.IsNullOrWhiteSpace( input )) {
+                retValue = String.Concat( input.TakeWhile( c => c != stopChar ));
+            }
+
+            return retValue;
+        }
+    }
+}
--- MilkBottle/ViewModels/InfoOverlayViewModel.cs
+++ MilkBottle/ViewModels/InfoOverlayViewModel.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using MilkBottle.Interfaces;
 using Prism.Commands;
 using ReusableBits.Mvvm.ViewModelSupport;
@@ -8,20 +6,11 @@ using ReusableBits.Platform;
 
 namespace MilkBottle.ViewModels {
     class InfoOverlayViewModel : PropertyChangeBase, IDisposable {
-        private const string                cArtistElement = "_artistName";
-        private const string                cAlbumElement = "_albumName";
-        private const string                cTrackElement = "_trackName";
-
-        private readonly Subject<string>    mArtistSubject;
-        private readonly Subject<string>    mAlbumSubject;
-        private readonly Subject<string>    mTrackSubject;
+        private const string                cInactiveState = "_inactiveState";
+        private const string                cDisplayTrackInformation = "_displayTrackInformation";
         private IDisposable                 mPlaybackSubscription;
-        private IDisposable                 mArtistSubscription;
-        private IDisposable                 mAlbumSubscription;
-        private IDisposable                 mTrackSubscription;
 
-        public  string                      AnimationTrigger { get; private set; }
-        public  string                      TargetElement { get; private set; }
+        public  string                      VisualState { get; private set; }
 
         public  string                      ArtistName { get; private set; }
         public  string                      AlbumName { get; private set; }
@@ -32,78 +21,41 @@ namespace MilkBottle.ViewModels {
         public InfoOverlayViewModel( IIpcManager ipcManager ) {
             Animate = new DelegateCommand( OnAnimate );
 
-            mArtistSubject = new Subject<string>();
-            mArtistSubscription = mArtistSubject.Delay( TimeSpan.FromMilliseconds( 500 )).ObserveOnDispatcher().Subscribe( OnArtist );
-            mAlbumSubject = new Subject<string>();
-            mAlbumSubscription = mAlbumSubject.Delay( TimeSpan.FromMilliseconds( 7000 )).ObserveOnDispatcher().Subscribe( OnAlbum );
-            mTrackSubject = new Subject<string>();
-            mTrackSubscription = mTrackSubject.Delay( TimeSpan.FromMilliseconds( 12000 )).ObserveOnDispatcher().Subscribe( OnTrack );
-
-            ArtistName = String.Empty;
-            AlbumName = String.Empty;
-            TrackName = String.Empty;
-            AnimationTrigger = String.Empty;
+            ArtistName = "The Rolling Stones"; //String.Empty;
+            AlbumName = "Sticky Fingers"; // String.Empty;
+            TrackName = "You Can't Always Get What You Want"; // String.Empty;
+            VisualState = cInactiveState;
 
             mPlaybackSubscription = ipcManager.OnPlaybackEvent.Subscribe( OnPlaybackEvent );
         }
 
         private void OnAnimate() {
-            mArtistSubject.OnNext( "The Rolling Stones" );
-            mAlbumSubject.OnNext( "Sticky Fingers" );
-            mTrackSubject.OnNext( "Bitch" );
+            TriggerDisplay();
         }
 
         private void OnPlaybackEvent( PlaybackEvent args ) {
-            mArtistSubject.OnNext( args.ArtistName );
-            mAlbumSubject.OnNext( args.AlbumName );
-            mTrackSubject.OnNext( args.TrackName );
+            if(!String.IsNullOrWhiteSpace( args?.TrackName )) {
+                ArtistName = args.ArtistName;
+                AlbumName = args.AlbumName;
+                TrackName = args.TrackName;
+
+                RaisePropertyChanged( () => ArtistName );
+                RaisePropertyChanged( () => AlbumName );
+                RaisePropertyChanged( () => TrackName );
+
+                TriggerDisplay();
+            }
         }
 
-        private void OnArtist( string artist ) {
-            ArtistName = artist;
+        private void TriggerDisplay() {
+            VisualState = cDisplayTrackInformation + '|' + DateTime.Now.Millisecond;
 
-            TargetElement = cArtistElement;
-            AnimationTrigger = artist;
-
-            RaisePropertyChanged( () => ArtistName );
-            RaisePropertyChanged( () => TargetElement );
-            RaisePropertyChanged( () => AnimationTrigger );
-        }
-
-        private void OnAlbum( string album ) {
-            AlbumName = album;
-
-            TargetElement = cAlbumElement;
-            AnimationTrigger = album;
-
-            RaisePropertyChanged( () => AlbumName );
-            RaisePropertyChanged( () => TargetElement );
-            RaisePropertyChanged( () => AnimationTrigger );
-        }
-
-        private void OnTrack( string track ) {
-            TrackName = track;
-
-            TargetElement = cTrackElement;
-            AnimationTrigger = track;
-
-            RaisePropertyChanged( () => TrackName );
-            RaisePropertyChanged( () => TargetElement );
-            RaisePropertyChanged( () => AnimationTrigger );
+            RaisePropertyChanged( () => VisualState );
         }
 
         public void Dispose() {
             mPlaybackSubscription?.Dispose();
             mPlaybackSubscription = null;
-
-            mArtistSubscription?.Dispose();
-            mArtistSubscription = null;
-
-            mAlbumSubscription?.Dispose();
-            mAlbumSubscription = null;
-
-            mTrackSubscription?.Dispose();
-            mTrackSubscription = null;
         }
     }
 }
