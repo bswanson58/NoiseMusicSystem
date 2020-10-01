eaff4409b637a043c603d411ae49a72fa25819bc
Bill Swanson <bswanson58@gmail.com>
2020-10-01 16:03:22 -0500

    Cleaned up the jitter in the LightPipe status view.

--- MilkBottle/LightPipe/ViewModels/LightPipeStatusViewModel.cs
+++ MilkBottle/LightPipe/ViewModels/LightPipeStatusViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Reactive.Linq;
+using System.Windows.Data;
 using System.Windows.Media;
 using Caliburn.Micro;
 using LightPipe.Dto;
@@ -13,34 +15,40 @@ namespace LightPipe.ViewModels {
         private readonly ZoneSummary    mZoneSummary;
 
         public  string                  ZoneId => mZoneSummary.ZoneId;
-        public  List<ColorBin>          Colors => mZoneSummary.Colors;
         public  List<Color>             MeanColors { get; }
 
         public ZoneInfo( ZoneSummary summary, List<Color> meanColors ) {
             mZoneSummary = summary;
             MeanColors = meanColors;
+
+            while( MeanColors.Count < 4 ) {
+                MeanColors.Add( Colors.Black );
+            }
         }
     }
 
     public class LightPipeStatusViewModel : PropertyChangeBase, IDisposable, IHandle<MilkBottle.Infrastructure.Events.CurrentZoneChanged> {
-        private const int                           cProcessingLimit = 60;
+        private const int                               cProcessingLimit = 60;
 
-        private readonly IImageProcessor            mImageProcessor;
-        private readonly IEventAggregator           mEventAggregator;
-        private IDisposable                         mImageProcessSubscription;
-        private bool                                mClearZones;
+        private readonly IImageProcessor                mImageProcessor;
+        private readonly IEventAggregator               mEventAggregator;
+        private readonly BindableCollection<ZoneInfo>   mZoneSummaries;
+        private IDisposable                             mImageProcessSubscription;
+        private bool                                    mClearZones;
 
-        public  ObservableCollection<ZoneInfo>      ZoneSummaries { get; }
-        public  string                              ElapsedTime { get; private set; }
-        public  bool                                HighProcessingTime { get; private set; }
+        public  ICollectionView                         ZoneSummaries { get; }
+        public  string                                  ElapsedTime { get; private set; }
+        public  bool                                    HighProcessingTime { get; private set; }
 
         public LightPipeStatusViewModel( IImageProcessor imageProcessor, IEventAggregator eventAggregator ) {
             mImageProcessor = imageProcessor;
             mEventAggregator = eventAggregator;
 
-            ZoneSummaries = new ObservableCollection<ZoneInfo>();
+            mZoneSummaries = new BindableCollection<ZoneInfo>();
+            ZoneSummaries = CollectionViewSource.GetDefaultView( mZoneSummaries );
+            ZoneSummaries.SortDescriptions.Add( new SortDescription( nameof( ZoneInfo.ZoneId ), ListSortDirection.Ascending ));
 
-            mImageProcessSubscription = mImageProcessor.ZoneUpdate.Subscribe( OnZoneUpdated );
+            mImageProcessSubscription = mImageProcessor.ZoneUpdate.ObserveOnDispatcher().Subscribe( OnZoneUpdated );
             mEventAggregator.Subscribe( this );
         }
 
@@ -50,17 +58,20 @@ namespace LightPipe.ViewModels {
 
         private void OnZoneUpdated( ZoneSummary summary ) {
             if( mClearZones ) {
-                ZoneSummaries.Clear();
+                mZoneSummaries.Clear();
                 mClearZones = false;
             }
             else {
-                var existing = ZoneSummaries.FirstOrDefault( z => z.ZoneId.Equals( summary.ZoneId ));
+                var existing = mZoneSummaries.FirstOrDefault( z => z.ZoneId.Equals( summary.ZoneId ));
 
+                mZoneSummaries.IsNotifying = false;
                 if( existing != null ) {
-                    ZoneSummaries.Remove( existing );
+                    mZoneSummaries.Remove( existing );
                 }
 
-                ZoneSummaries.Add( new ZoneInfo( summary, summary.FindMeanColors( 4 )));
+                mZoneSummaries.Add( new ZoneInfo( summary, summary.FindMeanColors( 4 )));
+                mZoneSummaries.IsNotifying = true;
+                mZoneSummaries.Refresh();
             }
 
             ElapsedTime = $"{mImageProcessor.ElapsedTime}";
