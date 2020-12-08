0b8157d299e5def20332b8ad1f3972126ec52546
Bill Swanson <bswanson58@gmail.com>
2020-12-08 13:39:21 -0600

    Added handlers for unhandled exceptions.

--- Noise.RemoteClient.Android/MainActivity.cs
+++ Noise.RemoteClient.Android/MainActivity.cs
@@ -1,33 +1,45 @@
-﻿using Android.App;
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Noise.RemoteClient.Droid.Support;
 using Prism;
 using Prism.Ioc;
 
-namespace Noise.RemoteClient.Droid
-{
+namespace Noise.RemoteClient.Droid {
     [Activity(Theme = "@style/MainTheme",
               ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity {
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity {
         protected override void OnCreate(Bundle savedInstanceState) {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
 
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException; 
+            
+            Xamarin.Forms.Forms.Init( this, savedInstanceState );
             LoadApplication(new App(new AndroidInitializer()));
         }
 
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults) {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults) {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-    }
 
+        private static void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs) {
+            new Exception( "TaskSchedulerOnUnobservedTaskException", unobservedTaskExceptionEventArgs.Exception ).LogUnhandledException();
+        }  
+ 
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs) {
+            new Exception( "CurrentDomainOnUnhandledException", unhandledExceptionEventArgs.ExceptionObject as Exception ).LogUnhandledException();
+        }  
+    } 
+    
     public class AndroidInitializer : IPlatformInitializer {
         public void RegisterTypes( IContainerRegistry containerRegistry ) { }
     }
 }
-
---
+++ Noise.RemoteClient.Android/Support/ExceptionFileWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Noise.RemoteClient.Droid.Support {
+    public static class ExceptionFileWriter {
+        static string FilePath {
+            get {
+                const string errorFilename = "Fatal.log";
+                var libraryPath = Environment.GetFolderPath( Environment.SpecialFolder.Personal );
+                
+                return Path.Combine( libraryPath, errorFilename );
+            }
+        }
+
+        public static void LogUnhandledException( this Exception exception ) {
+            try {
+                var errorMessage =
+                    $"Time: {DateTime.Now}\r\nError: Unhandled Exception\r\n{( string.IsNullOrEmpty( exception.StackTrace ) ? exception.ToString() : exception.StackTrace )}\n\n";
+                File.WriteAllText( FilePath, errorMessage );
+            }
+            catch( Exception ) {
+                // just suppress any error logging exceptions
+            }
+        }
+    }
+}
