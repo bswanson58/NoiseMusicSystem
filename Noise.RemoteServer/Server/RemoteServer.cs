8050e842ee5a113493df9d928ca5f1f6587c4e4f
Bill Swanson <bswanson58@gmail.com>
2020-12-11 12:49:56 -0600

    Added exception handling in the Remote Server.

--- Noise.RemoteServer/Server/RemoteServer.cs
+++ Noise.RemoteServer/Server/RemoteServer.cs
@@ -1,5 +1,7 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using Grpc.Core;
+using Noise.Infrastructure.Interfaces;
 using Noise.Infrastructure.RemoteHost;
 using Noise.RemoteServer.Interfaces;
 using Noise.RemoteServer.Protocol;
@@ -11,14 +13,16 @@ namespace Noise.RemoteServer.Server {
         private readonly HostInformationService     mHostInformationService;
         private readonly RemoteHostConfiguration    mRemoteHostConfiguration;
         private readonly IRemoteServiceFactory      mServiceFactory;
+        private readonly INoiseLog                  mLog;
         private Grpc.Core.Server	                mNoiseServer;
 
         public RemoteServer( IDiscoveryService discoveryService, HostInformationService hostInformationService,
-                             IRemoteServiceFactory serviceFactory, RemoteHostConfiguration remoteHostConfiguration ) {
+                             IRemoteServiceFactory serviceFactory, RemoteHostConfiguration remoteHostConfiguration, INoiseLog log ) {
             mDiscoveryService = discoveryService;
             mHostInformationService = hostInformationService;
             mServiceFactory = serviceFactory;
             mRemoteHostConfiguration = remoteHostConfiguration;
+            mLog = log;
         }
 
         public void OpenRemoteServer() {
@@ -30,26 +34,41 @@ namespace Noise.RemoteServer.Server {
         private void StartRpcServer() {
             StopRpcServer();
 
-            mNoiseServer = mServiceFactory.HostServer;
+            try {
+                mNoiseServer = mServiceFactory.HostServer;
 
-            mNoiseServer.Services.Add( HostInformation.BindService( mHostInformationService ));
-            mNoiseServer.Services.Add( ArtistInformation.BindService( mServiceFactory.ArtistInformationService ));
-            mNoiseServer.Services.Add( AlbumInformation.BindService( mServiceFactory.AlbumInformationService ));
-            mNoiseServer.Services.Add( TrackInformation.BindService( mServiceFactory.TrackInformationService ));
-            mNoiseServer.Ports.Add( new ServerPort( Dns.GetHostName(), mRemoteHostConfiguration.HostPort, ServerCredentials.Insecure ));
-            mNoiseServer.Start();
+                mNoiseServer.Services.Add( HostInformation.BindService( mHostInformationService ));
+                mNoiseServer.Services.Add( ArtistInformation.BindService( mServiceFactory.ArtistInformationService ));
+                mNoiseServer.Services.Add( AlbumInformation.BindService( mServiceFactory.AlbumInformationService ));
+                mNoiseServer.Services.Add( TrackInformation.BindService( mServiceFactory.TrackInformationService ));
+                mNoiseServer.Ports.Add( new ServerPort( Dns.GetHostName(), mRemoteHostConfiguration.HostPort, ServerCredentials.Insecure ));
+                mNoiseServer.Start();
+            }
+            catch( Exception ex ) {
+                mLog.LogException( "StartRpcServer", ex );
+            }
         }
 
         public void CloseRemoteServer() {
-            mDiscoveryService.StopDiscoveryService();
-            mHostInformationService.StopHostStatusResponder();
+            try {
+                mDiscoveryService.StopDiscoveryService();
+                mHostInformationService.StopHostStatusResponder();
 
-            StopRpcServer();
+                StopRpcServer();
+            }
+            catch( Exception ex ) {
+                mLog.LogException( "CloseRemoteServer", ex );
+            }
         }
 
         private async void StopRpcServer() {
             if( mNoiseServer != null ) {
-                await mNoiseServer.ShutdownAsync();
+                try {
+                    await mNoiseServer.ShutdownAsync();
+                }
+                catch( Exception ex ) {
+                    mLog.LogException( "StopRpcServer", ex );
+                }
 
                 mNoiseServer = null;
             }
