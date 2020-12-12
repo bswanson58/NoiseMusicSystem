7e967b3d825d3426a32690ee58bf9a70f391a654
Bill Swanson <bswanson58@gmail.com>
2020-12-12 17:11:49 -0600

    Added indications of playing, hasPlayed, and isStrategyQueued to the queue list view.

--- Noise.RemoteClient/Dto/UiQueuedTrack.cs
+++ Noise.RemoteClient/Dto/UiQueuedTrack.cs
@@ -1,11 +1,17 @@
-﻿using Noise.RemoteServer.Protocol;
+﻿using System;
+using Noise.RemoteServer.Protocol;
+using Prism.Mvvm;
 
 namespace Noise.RemoteClient.Dto {
-    class UiQueuedTrack {
+    class UiQueuedTrack : BindableBase {
         public  QueueTrackInfo  Track { get; }
         public  string          TrackName => Track.TrackName;
         public  string          AlbumName => Track.AlbumName;
         public  string          ArtistName => Track.ArtistName;
+        public  Int32           TrackDuration => Track.Duration;
+        public  bool            IsPlaying => Track.IsPlaying;
+        public  bool            IsStrategyQueued => Track.IsStrategyQueued;
+        public  bool            HasPlayed => Track.HasPlayed;
 
         public UiQueuedTrack( QueueTrackInfo track ) {
             Track = track;
---
+++ Noise.RemoteClient/ValueConverters/TimeSpanConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Noise.RemoteClient.ValueConverters {
+	public class TimeSpanConverter : IValueConverter {
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture ) {
+			var retValue = "";
+
+			if( value is TimeSpan timeSpan ) {
+                if( Math.Abs( timeSpan.Hours ) > 0 ) {
+					retValue = $"{timeSpan.Hours}:{Math.Abs( timeSpan.Minutes ):D2}:{Math.Abs( timeSpan.Seconds ):D2}";
+				}
+				else {
+					if( Math.Abs( timeSpan.Minutes ) > 0 ) {
+						retValue = $"{timeSpan.Minutes}:{Math.Abs( timeSpan.Seconds ):D2}";
+					}
+					else {
+						if( timeSpan.Seconds >= 0 ) {
+							retValue = $"0:{Math.Abs( timeSpan.Seconds ):D2}";
+						}
+						else {
+							retValue = $"-0:{Math.Abs( timeSpan.Seconds ):D2}";
+						}
+					}
+				}
+			}
+			return retValue;
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture ) {
+			return null;
+		}
+	}
+}
