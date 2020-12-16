46069415f3151125faefe904f6aa2d025368dec6
Bill Swanson <bswanson58@gmail.com>
2020-12-16 11:44:56 -0600

    Added favorite and rating indicators to the album list.

--- Noise.RemoteClient/Dto/UiAlbum.cs
+++ Noise.RemoteClient/Dto/UiAlbum.cs
@@ -11,6 +11,9 @@ namespace Noise.RemoteClient.Dto {
         public  Int32                       TrackCount => Album.TrackCount;
         public  Int32                       PublishedYear => Album.PublishedYear;
         public  bool                        HasPublishedYear => Album.PublishedYear > 0;
+        public  bool                        IsFavorite => Album.IsFavorite;
+        public  Int32                       Rating => Album.Rating;
+        public  bool                        HasRating => Rating != 0;
 
         public  DelegateCommand             Play { get; }
 
@@ -19,6 +22,19 @@ namespace Noise.RemoteClient.Dto {
             mPlayAction = onPlay;
 
             Play = new DelegateCommand( OnPlay );
+        }
+
+        public string RatingSource {
+            get {
+                var retValue = "0_Star";
+
+                if(( Rating > 0 ) &&
+                   ( Rating < 6 )) {
+                    retValue = $"{Rating:D1}_Star";
+                }
+
+                return retValue;
+            }
         }
 
         private void OnPlay() {
