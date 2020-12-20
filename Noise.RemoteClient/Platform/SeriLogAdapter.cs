42e9d88e0fdbb75db24397cfae2f930575770bee
Bill Swanson <bswanson58@gmail.com>
2020-12-20 14:14:29 -0600

    Attempted to place error log file on external stroage.

--- Noise.RemoteClient/Platform/SeriLogAdapter.cs
+++ Noise.RemoteClient/Platform/SeriLogAdapter.cs
@@ -2,20 +2,19 @@
 using System.IO;
 using Noise.RemoteClient.Interfaces;
 using Serilog;
-using Xamarin.Essentials;
 
 namespace Noise.RemoteClient.Platform {
     class SeriLogAdapter : IPlatformLog {
         private readonly ILogger	mLog;
 
         public SeriLogAdapter() {
-//            var logFile = Path.Combine( environment.LogFileDirectory(), environment.ApplicationName() + " log - {Date}.log" );
+            string filePath = Path.Combine( "/storage/emulated/0/Android/data", "Noise", "Logs", "error.log" );
 
             mLog = new LoggerConfiguration()
                 .MinimumLevel.Verbose()
                 .Enrich.FromLogContext()
                 .WriteTo.File(
-                    Path.Combine(FileSystem.AppDataDirectory, "Logs", "Log.log"),
+                    filePath, // Path.Combine( FileSystem.AppDataDirectory, "Logs", "Log.log" ),
                     rollingInterval: RollingInterval.Day,
                     outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}] {Message:lj} ({SourceContext}) {Exception}{NewLine}" )
                 .CreateLogger();
