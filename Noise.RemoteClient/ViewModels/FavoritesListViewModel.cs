247b3ab14c47f0cbffa76d469af7ff97b31e2919
Bill Swanson <bswanson58@gmail.com>
2020-12-20 16:43:41 -0600

    Added playing state to the favorites list.

--- Noise.RemoteClient/ViewModels/FavoritesListViewModel.cs
+++ Noise.RemoteClient/ViewModels/FavoritesListViewModel.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Noise.RemoteClient.Dto;
 using Noise.RemoteClient.Interfaces;
+using Xamarin.Forms.Internals;
 
 namespace Noise.RemoteClient.ViewModels {
     class FavoritesListViewModel : ListBase<UiTrack> {
         private readonly ITrackProvider     mTrackProvider;
+        private PlayingState                mPlayingState;
+        private IDisposable                 mPlayingStateSubscription;
 
-        public FavoritesListViewModel( ITrackProvider trackProvider, IQueuePlayProvider queuePlayProvider, IHostInformationProvider hostInformationProvider ) :
+        public FavoritesListViewModel( ITrackProvider trackProvider, IQueuePlayProvider queuePlayProvider, IHostInformationProvider hostInformationProvider,
+                                       IClientState clientState ) :
             base( queuePlayProvider, hostInformationProvider ) {
             mTrackProvider = trackProvider;
 
             InitializeLibrarySubscription();
+            mPlayingStateSubscription = clientState.CurrentlyPlaying.Subscribe( OnPlayingState );
         }
 
         protected override void OnLibraryStatusChanged( LibraryStatus status ) {
@@ -21,16 +27,30 @@ namespace Noise.RemoteClient.ViewModels {
             }
         }
 
+        private void OnPlayingState( PlayingState state ) {
+            mPlayingState = state;
+
+            DisplayList.ForEach( f => f.SetIsPlaying( mPlayingState ));
+        }
+
         protected override async Task<IEnumerable<UiTrack>> RetrieveList() {
             IEnumerable<UiTrack> retValue = new List<UiTrack>();
 
             var list = await mTrackProvider.GetFavoriteTracks();
 
             if( list?.Success == true ) {
-                retValue = from track in list.TrackList orderby track.TrackName, track.ArtistName, track.AlbumName select new UiTrack( track, OnPlay );
+                retValue = from track in list.TrackList orderby track.TrackName, track.ArtistName, track.AlbumName 
+                            select new UiTrack( track, OnPlay, mPlayingState );
             }
 
             return retValue;
+        }
+
+        public override void Dispose() {
+            mPlayingStateSubscription?.Dispose();
+            mPlayingStateSubscription = null;
+
+            base.Dispose();
         }
     }
 }
